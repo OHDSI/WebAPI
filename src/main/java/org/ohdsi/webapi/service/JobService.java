6cf97d0bfe962a7ce37f4aec3ee59e309d77766b
Alex Franken <alfranke@regenstrief.org>
2015-02-05 22:00:24 -0500

    Remove Regenstrief copyright remnant from eclipse IDE config

--- src/main/java/org/ohdsi/webapi/service/JobService.java
+++ src/main/java/org/ohdsi/webapi/service/JobService.java
@@ -1,15 +1,3 @@
-/**
- * The contents of this file are subject to the Regenstrief Public License
- * Version 1.0 (the "License"); you may not use this file except in compliance with the License.
- * Please contact Regenstrief Institute if you would like to obtain a copy of the license.
- *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
- *
- * Copyright (C) Regenstrief Institute.  All Rights Reserved.
- */
 package org.ohdsi.webapi.service;
 
 import javax.ws.rs.GET;

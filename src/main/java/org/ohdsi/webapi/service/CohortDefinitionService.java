2a8fe2a6f46e05a1cc5452ada877beab3bb39163
Charity Hilton <cahilton@regenstrief.org>
2015-02-10 16:54:05 -0500

    Switch cohort_definition to use cohort_schema

--- src/main/java/org/ohdsi/webapi/service/CohortDefinitionService.java
+++ src/main/java/org/ohdsi/webapi/service/CohortDefinitionService.java
@@ -84,7 +84,7 @@ public class CohortDefinitionService extends AbstractDaoService {
   public List<CohortDefinition> getCohortDefinitionList() {
       
       String sql = ResourceHelper.GetResourceAsString("/resources/cohortdefinition/sql/getCohortDefinitions.sql");
-      sql = SqlRender.renderSql(sql, new String[] { "CDM_schema" }, new String[] { getCdmSchema() });
+      sql = SqlRender.renderSql(sql, new String[] { "cohort_schema" }, new String[] { this.getCohortSchema() });
       sql = SqlTranslate.translateSql(sql, getSourceDialect(), getDialect());
       
       return getJdbcTemplate().query(sql, this.cohortDefinitionMapper);
@@ -100,8 +100,8 @@ public class CohortDefinitionService extends AbstractDaoService {
   public CohortDefinition getCohortDefinition(@PathParam("id") final int id) {
       
       String sql_statement = ResourceHelper.GetResourceAsString("/resources/cohortdefinition/sql/getCohortDefinitionsById.sql");
-      sql_statement = SqlRender.renderSql(sql_statement, new String[] { "id", "CDM_schema" },
-          new String[] { String.valueOf(id), getCdmSchema() });
+      sql_statement = SqlRender.renderSql(sql_statement, new String[] { "id", "cohort_schema" },
+          new String[] { String.valueOf(id), this.getCohortSchema() });
       sql_statement = SqlTranslate.translateSql(sql_statement, getSourceDialect(), getDialect());
       
       CohortDefinition def = null;

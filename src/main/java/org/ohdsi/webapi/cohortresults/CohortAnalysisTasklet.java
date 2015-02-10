cb6f47bf72970f0eb1e1f0d6bb0de43826ed3b58
Charity Hilton <cahilton@regenstrief.org>
2015-02-10 10:06:32 -0500

    standardize concepts comment out execute

--- src/main/java/org/ohdsi/webapi/cohortresults/CohortAnalysisTasklet.java
+++ src/main/java/org/ohdsi/webapi/cohortresults/CohortAnalysisTasklet.java
@@ -1,5 +1,7 @@
 package org.ohdsi.webapi.cohortresults;
 
+import jersey.repackaged.com.google.common.base.Splitter;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.batch.core.StepContribution;
@@ -7,6 +9,7 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.util.StringUtils;
 
 public class CohortAnalysisTasklet implements Tasklet {
 	
@@ -26,8 +29,28 @@ public class CohortAnalysisTasklet implements Tasklet {
 	@Override
 	public RepeatStatus execute(StepContribution contribution,
 			ChunkContext chunkContext) throws Exception {
+		/*
 		log.info(String.format("Executing Cohort Analysis Job for \n:%s", task.toString()));
-		this.jdbcTemplate.execute(sql);
+		Iterable<String> statements = Splitter.on(';').trimResults().split(sql);
+		boolean hasError = false;
+		// TODO all in one transaction
+		for (String stmt : statements) {
+			if (StringUtils.isEmpty(stmt)) {
+				continue;
+			}
+			if (hasError) {
+				log.info(String.format("unable to continue, unable to execute \n%s", stmt));
+				break;
+			}
+			try {
+				this.jdbcTemplate.execute(stmt);
+			} catch (Exception e) {
+				log.error(String.format("error executing %s", stmt), e);
+				hasError = true;
+			}
+		}
+		
+		*/
 		return RepeatStatus.FINISHED;
 	}
 

693d5c55589ab521d351e291d395243131c0e86b
Alex Franken <alfranke@regenstrief.org>
2015-02-19 14:05:08 -0500

    Ensure JpaTransactionManager is configured w/ Spring Batch. Change to non-pooling DataSource - hopefully a temporary change but should solve some immediate issues with oracle, temp tables, and locking

--- src/main/java/org/ohdsi/webapi/DataAccessConfig.java
+++ src/main/java/org/ohdsi/webapi/DataAccessConfig.java
@@ -3,14 +3,13 @@ package org.ohdsi.webapi;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
-import org.apache.tomcat.jdbc.pool.PoolConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -31,48 +30,55 @@ public class DataAccessConfig {
     
     @Bean
     @Primary
-    @ConfigurationProperties(prefix = "datasource")
     public DataSource primaryDataSource() {
         
-        try {
-            final PoolConfiguration pc = new org.apache.tomcat.jdbc.pool.PoolProperties();
-            pc.setDriverClassName(this.env.getRequiredProperty("datasource.driverClassName"));
-            pc.setUrl(this.env.getRequiredProperty("datasource.url"));
-            pc.setUsername(this.env.getRequiredProperty("datasource.username"));
-            pc.setPassword(this.env.getRequiredProperty("datasource.password"));
-            pc.setDefaultAutoCommit(false);
-            
-            // TODO remaining datasource pool props
-            
-            return new org.apache.tomcat.jdbc.pool.DataSource(pc);
-        } catch (final Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Bean
-    public EntityManagerFactory entityManagerFactory() {
-
-      HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-      vendorAdapter.setGenerateDdl(false);
-      vendorAdapter.setShowSql(Boolean.getBoolean(this.env.getRequiredProperty("spring.jpa.show-sql")));
-
-      LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-      factory.setJpaVendorAdapter(vendorAdapter);
-      factory.setPackagesToScan("org.ohdsi.webapi");
-      factory.setDataSource(primaryDataSource());
-      factory.afterPropertiesSet();
-
-      return factory.getObject();
+        String driver = this.env.getRequiredProperty("datasource.driverClassName");
+        String url = this.env.getRequiredProperty("datasource.url");
+        String user = this.env.getRequiredProperty("datasource.username");
+        String pass = this.env.getRequiredProperty("datasource.password");
+        String autoCommit = "false";
+        
+        //pooling - currently issues with (at least) oracle with use of temp tables and "on commit preserve rows" instead of "on commit delete rows";
+        //http://forums.ohdsi.org/t/transaction-vs-session-scope-for-global-temp-tables-statements/333/2
+        /*final PoolConfiguration pc = new org.apache.tomcat.jdbc.pool.PoolProperties();
+        pc.setDriverClassName(driver);
+        pc.setUrl(url);
+        pc.setUsername(user);
+        pc.setPassword(pass);
+        pc.setDefaultAutoCommit(Boolean.valueOf(autoCommit));*/
+        
+        //non-pooling
+        DataSource ds = new DriverManagerDataSource(url, user, pass);
+        //note autocommit defaults vary across vendors. use provided @Autowired TransactionTemplate
+        
+        return ds;
+        //return new org.apache.tomcat.jdbc.pool.DataSource(pc);        
     }
     
     @Bean
-    @Primary//This is needed so that JpaTransactionManager is used for autowiring, instead of DataSourceTransactionManager
-    public PlatformTransactionManager jpaTransactionManager(){//EntityManagerFactory entityManagerFactory) {
-
-      JpaTransactionManager txManager = new JpaTransactionManager();
-      txManager.setEntityManagerFactory(entityManagerFactory());
-      return txManager;
+    public EntityManagerFactory entityManagerFactory() {
+        
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(false);
+        vendorAdapter.setShowSql(Boolean.getBoolean(this.env.getRequiredProperty("spring.jpa.show-sql")));
+        
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("org.ohdsi.webapi");
+        factory.setDataSource(primaryDataSource());
+        factory.afterPropertiesSet();
+        
+        return factory.getObject();
+    }
+    
+    @Bean
+    @Primary
+    //This is needed so that JpaTransactionManager is used for autowiring, instead of DataSourceTransactionManager
+    public PlatformTransactionManager jpaTransactionManager() {//EntityManagerFactory entityManagerFactory) {
+    
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory());
+        return txManager;
     }
     
     @Bean

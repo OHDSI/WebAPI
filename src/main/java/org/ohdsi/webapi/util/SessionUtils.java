de279c4aa64152b0c79836873a032cd0bff7e95e
Alex Franken <alfranke@regenstrief.org>
2015-03-09 23:19:34 -0400

    Use SqlTranslate to generate random since there is some additional logic involved (e.g. non-numeric initial character)

--- src/main/java/org/ohdsi/webapi/util/SessionUtils.java
+++ src/main/java/org/ohdsi/webapi/util/SessionUtils.java
@@ -1,13 +1,13 @@
 package org.ohdsi.webapi.util;
 
-import org.apache.commons.lang3.RandomStringUtils;
-
+import org.ohdsi.sql.SqlTranslate;
 
 /**
  *
  */
 public final class SessionUtils {
-    public static final String sessionId(){
-        return RandomStringUtils.randomAlphanumeric(10);
+    
+    public static final String sessionId() {
+        return SqlTranslate.generateSessionId();
     }
 }

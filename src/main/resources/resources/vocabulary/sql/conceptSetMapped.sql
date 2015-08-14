32be10c44e02eb7d26ad16d7202b54c8a420cdda
Frank DeFalco <fdefalco@ohdsi.org>
2015-08-14 14:30:04 -0400

    vocabulary service query improvements

--- src/main/resources/resources/vocabulary/sql/conceptSetMapped.sql
+++ src/main/resources/resources/vocabulary/sql/conceptSetMapped.sql
@@ -3,4 +3,4 @@ FROM
 (
   @conceptsetQuery
 ) C
-join @cdm_database_schema.concept_relationship cr on C.concept_id = cr.concept_id_1 and cr.relationship_id = 'Mapped from'
+join @cdm_database_schema.concept_relationship cr on C.concept_id = cr.concept_id_1 and cr.relationship_id = 'Mapped from' and cr.invalid_reason IS NULL

dac2d456412b917ca7614cb188e3a352fbbd588f
GlebMalikov <gleb.malikov@firstlinesoftware.com>
2015-09-11 15:29:05 +0300

    Basic authentication full.

--- src/main/java/org/ohdsi/webapi/ShiroConfiguration.java
+++ src/main/java/org/ohdsi/webapi/ShiroConfiguration.java
@@ -1,19 +1,25 @@
 package org.ohdsi.webapi;
 
-import org.apache.shiro.authc.credential.DefaultPasswordService;
-import org.apache.shiro.authc.credential.PasswordMatcher;
 import org.apache.shiro.authc.credential.SimpleCredentialsMatcher;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.filter.authc.AnonymousFilter;
+import org.apache.shiro.web.filter.authc.BasicHttpAuthenticationFilter;
+import org.apache.shiro.web.filter.authc.UserFilter;
+import org.apache.shiro.web.filter.authz.RolesAuthorizationFilter;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.mgt.WebSecurityManager;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.ohdsi.webapi.shiro.SampleRealm;
-import org.ohdsi.webapi.shiro.SampleShiroWaffleRealm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.servlet.*;
+import javax.servlet.Filter;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by GMalikov on 20.08.2015.
@@ -24,15 +30,19 @@ public class ShiroConfiguration {
 
     @Bean
     public ShiroFilterFactoryBean shiroFilter(){
-        ShiroFilterFactoryBean factoryBean = new ShiroFilterFactoryBean();
-        factoryBean.setSecurityManager(securityManager());
-        return factoryBean;
+        ShiroFilterFactoryBean shiroFilter = new ShiroFilterFactoryBean();
+        shiroFilter.setSecurityManager(securityManager());
+        shiroFilter.setLoginUrl("/test/login");
+        Map<String, String> filterChain = new HashMap<>();
+        filterChain.put("/test/info", "authcBasic");
+        shiroFilter.setFilterChainDefinitionMap(filterChain);
+        return shiroFilter;
     }
 
     @Bean(name = "securityManager")
     public DefaultWebSecurityManager securityManager(){
         final DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
-        securityManager.setRealm(realm());
+        securityManager.setRealm(simpleAuthRealm());
         return securityManager;
     }
 
@@ -43,21 +53,21 @@ public class ShiroConfiguration {
         return sessionManager;
     }
 
+    @Bean(name = "simpleAuthRealm")
+    @DependsOn("lifecycleBeanPostProcessor")
+    public SampleRealm simpleAuthRealm(){
+        final SampleRealm simpleAuthRealm = new SampleRealm();
+        simpleAuthRealm.setCredentialsMatcher(new SimpleCredentialsMatcher());
+        return simpleAuthRealm;
+    }
+
 //    @Bean(name = "realm")
 //    @DependsOn("lifecycleBeanPostProcessor")
-//    public SampleRealm realm(){
-//        final SampleRealm realm = new SampleRealm();
+//    public SampleShiroWaffleRealm realm(){
+//        final SampleShiroWaffleRealm realm = new SampleShiroWaffleRealm();
 //        realm.setCredentialsMatcher(new SimpleCredentialsMatcher());
 //        return realm;
 //    }
-
-    @Bean(name = "realm")
-    @DependsOn("lifecycleBeanPostProcessor")
-    public SampleShiroWaffleRealm realm(){
-        final SampleShiroWaffleRealm realm = new SampleShiroWaffleRealm();
-        realm.setCredentialsMatcher(new SimpleCredentialsMatcher());
-        return realm;
-    }
 
     @Bean
     public LifecycleBeanPostProcessor lifecycleBeanPostProcessor(){

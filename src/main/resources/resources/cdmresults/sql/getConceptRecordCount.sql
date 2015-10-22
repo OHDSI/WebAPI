3ad33641bfb82a5a9515bdc3870431e03489d671
Patrick Ryan <ryan@ohdsi.org>
2015-10-22 16:25:16 -0400

    Update getConceptRecordCount.sql
    added measurement counts

--- src/main/resources/resources/cdmresults/sql/getConceptRecordCount.sql
+++ src/main/resources/resources/cdmresults/sql/getConceptRecordCount.sql
@@ -5,12 +5,12 @@ with concepts as (
 ), counts as (
 	select stratum_1 concept_id, max(count_value) agg_count_value
 	from @resultTableQualifier.achilles_results
-	where analysis_id in (2, 4, 5, 201, 301, 401, 501, 505, 601, 701, 801, 901, 1001,1201)
+	where analysis_id in (2, 4, 5, 201, 301, 401, 501, 505, 601, 701, 801, 901, 1001,1201,1801)
 	group by stratum_1
 	union
 	select stratum_2 as concept_id, sum(count_value) as agg_count_value
 	from @resultTableQualifier.achilles_results
-	where analysis_id in (405, 605, 705, 805, 807)
+	where analysis_id in (405, 605, 705, 805, 807, 1805, 1807)
 	group by stratum_2
 )
 select concepts.ancestor_id concept_id, isnull(max(c1.agg_count_value),0) record_count, isnull(sum(c2.agg_count_value),0) descendant_record_count

a29a567c2f361bbc92a22f7445c39c70aa59d44b
Chris Knoll <cknoll@ohdsi.org>
2016-05-11 09:03:32 -0400

    Add coalesce to death cause_concept_id.
    Fixes #101

--- src/main/resources/resources/cohortdefinition/sql/death.sql
+++ src/main/resources/resources/cohortdefinition/sql/death.sql
@@ -1,4 +1,4 @@
-select C.person_id, C.death_date as start_date, DATEADD(d,1,C.death_date) as end_date, C.cause_concept_id as TARGET_CONCEPT_ID
+select C.person_id, C.death_date as start_date, DATEADD(d,1,C.death_date) as end_date, coalesce(C.cause_concept_id,0) as TARGET_CONCEPT_ID
 from 
 (
   select d.*

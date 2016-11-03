4b882a4985497af2c12ce16a3f894d92993eab84
Gennadiy Anisimov <gennadiy.anisimov@arcadia.spb.ru>
2016-11-03 10:21:39 +0300

    Add migration to rename 'system' user to 'anonymous'.

---
+++ src/main/resources/db/migration/sqlserver/V1.0.5.0__rename_system_user_to_anonymous.sql
@@ -0,0 +1,6 @@
+UPDATE cohort_definition SET created_by = 'anonymous' WHERE created_by = 'system';
+UPDATE cohort_definition SET modified_by = 'anonymous' WHERE modified_by = 'system';
+UPDATE feasibility_study SET created_by = 'anonymous' WHERE created_by = 'system';
+UPDATE feasibility_study SET modified_by = 'anonymous' WHERE modified_by = 'system';
+UPDATE ir_analysis SET created_by = 'anonymous' WHERE created_by = 'system';
+UPDATE ir_analysis SET modified_by = 'anonymous' WHERE modified_by = 'system';

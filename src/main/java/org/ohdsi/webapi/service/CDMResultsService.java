1e521c3eb5eca768c198527f357214ff516690d1
Mark Velez <mav7014@nyp.org>
2016-11-22 14:54:49 -0500

    Remove obsoleted code from CDMResultsService (activates observation reports)

--- src/main/java/org/ohdsi/webapi/service/CDMResultsService.java
+++ src/main/java/org/ohdsi/webapi/service/CDMResultsService.java
@@ -41,34 +41,6 @@ public class CDMResultsService extends AbstractDaoService {
         queryRunner = new CDMResultsAnalysisRunner(this.getSourceDialect()/*, this.visualizationDataRepository*/);
     }
 
-    @Path("{conceptId}/monthlyConditionOccurrencePrevalence")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public MonthlyPrevalence getMonthlyConditionOccurrencePrevalence(@PathParam("sourceKey") String sourceKey, @PathParam("conceptId") String conceptId) {
-        try {
-            Source source = getSourceRepository().findBySourceKey(sourceKey);
-            String tableQualifier = source.getTableQualifier(SourceDaimon.DaimonType.Results);
-
-            String sql_statement = ResourceHelper.GetResourceAsString("/resources/cdmresults/sql/getMonthlyConditionOccurrencePrevalence.sql");
-            sql_statement = SqlRender.renderSql(sql_statement, new String[]{"OHDSI_schema", "conceptId"}, new String[]{tableQualifier, conceptId});
-            sql_statement = SqlTranslate.translateSql(sql_statement, "sql server", source.getSourceDialect());
-
-            return getSourceJdbcTemplate(source).query(sql_statement, new ResultSetExtractor<MonthlyPrevalence>() {
-                @Override
-                public MonthlyPrevalence extractData(ResultSet rs) throws SQLException, DataAccessException {
-                    MonthlyPrevalence result = new MonthlyPrevalence();
-                    while (rs.next()) {
-                        result.monthKey.add(rs.getString(1));
-                        result.prevalence.add(rs.getFloat(2));
-                    }
-                    return result;
-                }
-            });
-        } catch (Exception exception) {
-            throw new RuntimeException("Error retrieving monthly condition occurrence prevalence statistics." + exception.getMessage());
-        }
-    }
-
     private final RowMapper<SimpleEntry<Long, Long[]>> rowMapper = new RowMapper<SimpleEntry<Long, Long[]>>() {
         @Override
         public SimpleEntry<Long, Long[]> mapRow(final ResultSet resultSet, final int arg1) throws SQLException {
@@ -214,33 +186,6 @@ public class CDMResultsService extends AbstractDaoService {
     }
 
     /**
-     * Queries for observations report for the given sourceKey
-     *
-     * @return CDMObservation
-     */
-    @GET
-    @Path("observation")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<CDMObservation> getObservations(@PathParam("sourceKey") final String sourceKey, @DefaultValue("false") @QueryParam("refresh") boolean refresh) {
-        List<CDMObservation> cdmObservationPeriods = null;
-        final String key = CDMResultsAnalysisRunner.OBSERVATION;
-        Source source = getSourceRepository().findBySourceKey(sourceKey);
-//        AchillesVisualizationData data = /*refresh ?*/ null /*: this.visualizationDataRepository.findByCohortDefinitionIdAndSourceIdAndVisualizationKey(source.getSourceId(), key)*/;
-
-        if (refresh /*|| data == null*/) {
-            cdmObservationPeriods = this.queryRunner.getObservationResults(this.getSourceJdbcTemplate(source), source, true);
-        } else {
-            try {
-//                procedureSummary = mapper.readValue(data.getData(), CDMProcedureSummary.class);
-            } catch (Exception e) {
-                log.error(e);
-            }
-        }
-
-        return cdmObservationPeriods;
-    }
-
-    /**
      * Queries for data density report for the given sourceKey
      *
      * @return CDMDataDensity
@@ -268,162 +213,9 @@ public class CDMResultsService extends AbstractDaoService {
     }
 
     /**
-     * Queries for condition report for the given sourceKey
+     * Queries for treemap results
      *
-     * @return CDMConditionTree
-     */
-    @GET
-    @Path("condition_treemap")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<CDMCondition> getConditionTreeMap(@PathParam("sourceKey") final String sourceKey, @DefaultValue("false") @QueryParam("refresh") boolean refresh) {
-        List<CDMCondition> cdmConditions = null;
-        final String key = CDMResultsAnalysisRunner.CONDITION;
-        Source source = getSourceRepository().findBySourceKey(sourceKey);
-//        AchillesVisualizationData data = /*refresh ?*/ null /*: this.visualizationDataRepository.findByCohortDefinitionIdAndSourceIdAndVisualizationKey(source.getSourceId(), key)*/;
-
-        if (refresh /*|| data == null*/) {
-            cdmConditions = this.queryRunner.getCondition(this.getSourceJdbcTemplate(source), source, true);
-        } else {
-            try {
-//                procedureSummary = mapper.readValue(data.getData(), CDMProcedureSummary.class);
-            } catch (Exception e) {
-                log.error(e);
-            }
-        }
-
-        return cdmConditions;
-    }
-
-    /**
-     * Queries for condition era report for the given sourceKey
-     *
-     * @return CDMConditionEra
-     */
-    @GET
-    @Path("conditionera_treemap")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<CDMConditionEra> getConditionEraTreeMap(@PathParam("sourceKey") final String sourceKey, @DefaultValue("false") @QueryParam("refresh") boolean refresh) {
-        List<CDMConditionEra> cdmConditionEras = null;
-        final String key = CDMResultsAnalysisRunner.CONDITIONERA;
-        Source source = getSourceRepository().findBySourceKey(sourceKey);
-//        AchillesVisualizationData data = /*refresh ?*/ null /*: this.visualizationDataRepository.findByCohortDefinitionIdAndSourceIdAndVisualizationKey(source.getSourceId(), key)*/;
-
-        if (refresh /*|| data == null*/) {
-            cdmConditionEras = this.queryRunner.getConditionEras(this.getSourceJdbcTemplate(source), source, true);
-        } else {
-            try {
-//                procedureSummary = mapper.readValue(data.getData(), CDMProcedureSummary.class);
-            } catch (Exception e) {
-                log.error(e);
-            }
-        }
-
-        return cdmConditionEras;
-    }
-
-
-    @Path("drugeratreemap")
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public List<DrugPrevalence> getDrugEraTreemap(@PathParam("sourceKey") String sourceKey, String[] identifiers) {
-        Source source = getSourceRepository().findBySourceKey(sourceKey);
-        String tableQualifier = source.getTableQualifier(SourceDaimon.DaimonType.Results);
-        String vocabularyTableQualifier = source.getTableQualifier(SourceDaimon.DaimonType.Vocabulary);
-
-        for (int i = 0; i < identifiers.length; i++) {
-            identifiers[i] = "'" + identifiers[i] + "'";
-        }
-
-        String identifierList = StringUtils.join(identifiers, ",");
-        String sql_statement = ResourceHelper.GetResourceAsString("/resources/cdmresults/sql/getDrugEraTreemap.sql");
-        sql_statement = SqlRender.renderSql(sql_statement, new String[]{"ohdsi_database_schema", "vocabulary_database_schema", "conceptList"}, new String[]{tableQualifier, vocabularyTableQualifier, identifierList});
-        sql_statement = SqlTranslate.translateSql(sql_statement, "sql server", source.getSourceDialect());
-
-
-        List<Map<String, Object>> rows = getSourceJdbcTemplate(source).queryForList(sql_statement);
-        List<DrugPrevalence> listOfResults = new ArrayList<DrugPrevalence>();
-        for (Map rs : rows) {
-            DrugPrevalence d = new DrugPrevalence();
-            d.conceptId = Long.valueOf(String.valueOf(rs.get("concept_id")));
-            d.conceptPath = String.valueOf(rs.get("concept_path"));
-            d.lengthOfEra = Float.valueOf(String.valueOf(rs.get("length_of_era")));
-            d.numPersons = Long.valueOf(String.valueOf(rs.get("num_persons")));
-            d.percentPersons = Float.valueOf(String.valueOf(rs.get("percent_persons")));
-            listOfResults.add(d);
-        }
-
-        return listOfResults;
-    }
-
-    @Path("{conceptId}/drugeraprevalence")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<DrugEraPrevalence> getDrugEraPrevalenceByGenderAgeYear(@PathParam("sourceKey") String
-                                                                               sourceKey, @PathParam("conceptId") String conceptId) {
-        Source source = getSourceRepository().findBySourceKey(sourceKey);
-        String tableQualifier = source.getTableQualifier(SourceDaimon.DaimonType.Results);
-        String vocabularyTableQualifier = source.getTableQualifier(SourceDaimon.DaimonType.Vocabulary);
-
-        String sql_statement = ResourceHelper.GetResourceAsString("/resources/cdmresults/sql/getDrugEraPrevalenceByGenderAgeYear.sql");
-        sql_statement = SqlRender.renderSql(sql_statement, new String[]{"ohdsi_database_schema", "vocabulary_database_schema", "conceptId"}, new String[]{tableQualifier, vocabularyTableQualifier, conceptId});
-        sql_statement = SqlTranslate.translateSql(sql_statement, "sql server", source.getSourceDialect());
-
-
-        List<Map<String, Object>> rows = getSourceJdbcTemplate(source).queryForList(sql_statement);
-        List<DrugEraPrevalence> listOfResults = new ArrayList<DrugEraPrevalence>();
-        for (Map rs : rows) {
-            DrugEraPrevalence d = new DrugEraPrevalence();
-            d.conceptId = Long.valueOf(String.valueOf(rs.get("concept_id")));
-            d.trellisName = String.valueOf(rs.get("trellis_name"));
-            d.seriesName = String.valueOf(rs.get("series_name"));
-            d.xCalendarYear = Long.valueOf(String.valueOf(rs.get("x_calendar_year")));
-            d.yPrevalence1000Pp = Float.valueOf(String.valueOf(rs.get("y_prevalence_1000pp")));
-            listOfResults.add(d);
-        }
-
-        return listOfResults;
-    }
-
-    @Path("conditionoccurrencetreemap")
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public List<ConditionOccurrenceTreemapNode> getConditionOccurrenceTreemap(@PathParam("sourceKey") String
-                                                                                      sourceKey, String[] identifiers) {
-        Source source = getSourceRepository().findBySourceKey(sourceKey);
-        String tableQualifier = source.getTableQualifier(SourceDaimon.DaimonType.Results);
-        String cdmTableQualifier = source.getTableQualifier(SourceDaimon.DaimonType.CDM);
-
-        for (int i = 0; i < identifiers.length; i++) {
-            identifiers[i] = "'" + identifiers[i] + "'";
-        }
-
-        String identifierList = StringUtils.join(identifiers, ",");
-        String sql_statement = ResourceHelper.GetResourceAsString("/resources/cdmresults/sql/getConditionOccurrenceTreemap.sql");
-        sql_statement = SqlRender.renderSql(sql_statement, new String[]{"ohdsi_database_schema", "cdm_database_schema", "conceptIdList"}, new String[]{tableQualifier, cdmTableQualifier, identifierList});
-        sql_statement = SqlTranslate.translateSql(sql_statement, "sql server", source.getSourceDialect());
-
-
-        List<Map<String, Object>> rows = getSourceJdbcTemplate(source).queryForList(sql_statement);
-        List<ConditionOccurrenceTreemapNode> listOfResults = new ArrayList<ConditionOccurrenceTreemapNode>();
-        for (Map rs : rows) {
-            ConditionOccurrenceTreemapNode c = new ConditionOccurrenceTreemapNode();
-            c.conceptId = Long.valueOf(String.valueOf(rs.get("concept_id")));
-            c.conceptPath = String.valueOf(rs.get("concept_path"));
-            c.numPersons = Long.valueOf(String.valueOf(rs.get("num_persons")));
-            c.percentPersons = Float.valueOf(String.valueOf(rs.get("percent_persons")));
-            c.recordsPerPerson = Float.valueOf(String.valueOf(rs.get("records_per_person")));
-            listOfResults.add(c);
-        }
-
-        return listOfResults;
-    }
-
-    /**
-     * Queries for measurement treemap results
-     *
-     * @return List<HierarchicalConceptRecord>
+     * @return List<ArrayNode>
      */
     @GET
     @Path("/{domain}/")
@@ -435,6 +227,11 @@ public class CDMResultsService extends AbstractDaoService {
         return queryRunner.getTreemap(this.getSourceJdbcTemplate(source), domain, source);
     }
 
+    /**
+     * Queries for drilldown results
+     *
+     * @return List<ArrayNode>
+     */
     @GET
     @Path("/{domain}/{conceptId}")
     @Produces(MediaType.APPLICATION_JSON)

fe8e88ce2323613432a177b62cf827dda8c7f785
Anthony Sena <asena5@its.jnj.com>
2017-06-25 22:50:47 -0400

    Fix get distinct outcomes

--- src/main/resources/resources/study/sql/queryDashboardOutcomes.sql
+++ src/main/resources/resources/study/sql/queryDashboardOutcomes.sql
@@ -6,5 +6,5 @@ FROM @study_results_schema.dashboard d
 INNER JOIN @study_results_schema.cohort_definition o ON d.outcome_cohort_definition_id = o.cohort_definition_id
 WHERE d.target_cohort_definition_id @cohort_list_equality
   AND d.study_id = @study_id
-ORDER BY o.cohort_definition_name
+ORDER BY o.short_name
 ;

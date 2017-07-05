2716424cdb3b83c3f81aa4ace2431ad67ecb5528
Tom White <tom@cloudera.com>
2017-07-05 15:28:29 +0100

    Use CONCAT rather than +.

--- src/main/resources/resources/cdmresults/sql/report/measurement/treemap.sql
+++ src/main/resources/resources/cdmresults/sql/report/measurement/treemap.sql
@@ -1,9 +1,10 @@
 SELECT
   concept_hierarchy.concept_id                          AS "conceptId",
-  isNull(concept_hierarchy.level3_concept_name, 'NA')
-  + '||' + isNull(concept_hierarchy.level2_concept_name, 'NA')
-  + '||' + isNull(concept_hierarchy.level1_concept_name, 'NA')
-  + '||' + isNull(concept_hierarchy.concept_name, 'NA') AS "conceptPath",
+  CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(
+    isNull(concept_hierarchy.level3_concept_name, 'NA'), '||'),
+    isNull(concept_hierarchy.level2_concept_name, 'NA')), '||'),
+    isNull(concept_hierarchy.level1_concept_name, 'NA')), '||'),
+    isNull(concept_hierarchy.concept_name, 'NA'))       AS "conceptPath",
   ar1.count_value                                       AS "numPersons",
   1.0 * ar1.count_value / denom.count_value             AS "percentPersons",
   1.0 * ar2.count_value / ar1.count_value               AS "recordsPerPerson"

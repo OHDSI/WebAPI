16330d1fc0f04aa61dd30d3ef177af51b075b12b
Gowtham Rao <gowthamrao@users.noreply.github.com>
2017-08-22 13:47:19 -0400

    added json property for era constructor
    OHDSI/WebAPI#217
    OHDSI/Atlas#414
    OHDSI/Atlas#375

--- src/main/java/org/ohdsi/webapi/cohortdefinition/CohortExpression.java
+++ src/main/java/org/ohdsi/webapi/cohortdefinition/CohortExpression.java
@@ -45,4 +45,6 @@ public class CohortExpression {
   @JsonProperty("CensoringCriteria")
   public Criteria[] censoringCriteria;
   
+  @JsonProperty("EraConstructor")
+  public EraConstructor eraConstructor;
 }

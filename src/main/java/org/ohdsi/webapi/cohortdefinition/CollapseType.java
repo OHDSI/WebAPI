38139bb3c08f1e6c43de86e4cea603a29d7c9954
Gowtham Rao <gowthamrao@users.noreply.github.com>
2017-08-22 13:48:43 -0400

    init commit of collapse type for era constructor
    OHDSI/WebAPI#217
    OHDSI/Atlas#414
    OHDSI/Atlas#375

---
+++ src/main/java/org/ohdsi/webapi/cohortdefinition/CollapseType.java
@@ -0,0 +1,8 @@
+package org.ohdsi.webapi.cohortdefinition;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CollapseType {
+	@JsonProperty("CollapseType")
+	public String collapseType = "Era";
+}

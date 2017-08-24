e219a44d2878a6fee78a9ec745440700dc4781a2
Gowtham Rao <gowthamrao@users.noreply.github.com>
2017-08-24 15:38:57 -0400

    change geteraconstructorquery to collapseconstructorquery, modify query builder to match

--- src/main/java/org/ohdsi/webapi/cohortdefinition/CohortExpressionQueryBuilder.java
+++ src/main/java/org/ohdsi/webapi/cohortdefinition/CohortExpressionQueryBuilder.java
@@ -268,7 +268,7 @@ public class CohortExpressionQueryBuilder implements IGetCriteriaSqlDispatcher, 
     return query;
   }
   
-  public String getEraConstructorQuery(EraConstructorSettings eraConstructorSettings) {
+  public String getCollapseConstructorQuery(CollapseSettings collapseSettings) {
 	String query = ERA_CONSTRUCTOR_TEMPLATE;
 	
 	query = StringUtils.replace(query, "@eraGroup", "person_id");
@@ -335,10 +335,10 @@ public class CohortExpressionQueryBuilder implements IGetCriteriaSqlDispatcher, 
       
     resultSql = StringUtils.replace(resultSql, "@censoringInserts", getCensoringEventsQuery(expression.censoringCriteria));
     
-    resultSql = StringUtils.replace(resultSql, "@eraConstructor", getEraConstructorQuery(expression.eraConstructorSettings));
+    resultSql = StringUtils.replace(resultSql, "@collapseConstructor", getCollapseConstructorQuery(expression.collapseSettings));
 	
 	// table from which to records are read and inserted into the db
-    resultSql = StringUtils.replace(resultSql, "@output_table", "#era_constructor_output");
+    resultSql = StringUtils.replace(resultSql, "@output_table", "#collapse_constructor_output");
 	
     if (options != null)
     {

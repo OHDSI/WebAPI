1b52e7a503dc27e05aba04beccb0ae13df78726e
Alexandr Ryabokon <alexandr.ryabokon@firstlinesoftware.com>
2017-08-28 11:23:44 +0300

    Moved the latest CohortQueryBuilder changes to circe

--- src/main/java/org/ohdsi/webapi/cohortdefinition/CollapseType.java
+++
@@ -1,8 +0,0 @@
-package org.ohdsi.webapi.cohortdefinition;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public class CollapseType {
-	@JsonProperty("CollapseType")
-	public String collapseType = "Era";
-}

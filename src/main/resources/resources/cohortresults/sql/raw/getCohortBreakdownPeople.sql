d378f9a3fc6747b9dcaf0017f5e8875963698146
Gowtham Rao <gowthamrao@users.noreply.github.com>
2017-09-07 15:48:58 -0400

    Changing formatting to ohdsi style
    Changing formatting

--- src/main/resources/resources/cohortresults/sql/raw/getCohortBreakdownPeople.sql
+++ src/main/resources/resources/cohortresults/sql/raw/getCohortBreakdownPeople.sql
@@ -1,39 +1,32 @@
 with breakdown (subject_id, cohort_start_date, cohort_end_date, gender,age,conditions,drugs) as (
     select subject_id, cohort_start_date, cohort_end_date, gender, age, conditions, drugs
     from
-        (
-			select subject_id, cohort_start_date, cohort_end_date,
-					gc.concept_name as gender,
-					cast(floor((year(cohort_start_date) - year_of_birth) / 10) * 10 as varchar(5)) + '-' +
-						cast(floor((year(cohort_start_date) - year_of_birth) / 10 + 1) * 10 - 1 as varchar(5)) as age,
-					conditions.conditions,
-					drugs.drugs
-			from
-				@resultsTableQualifier.cohort c
-			join
-				(
-					select person_id,
-						round(count(*), cast(- floor(log10(abs(count(*) + 0.01))) as int)) conditions
-					from @tableQualifier.condition_occurrence co
-					group by person_id
-				) as conditions
-			on c.subject_id = conditions.person_id
-			join
-				(
-					select person_id,
-						round(count(*), cast(- floor(log10(abs(count(*) + 0.01))) as int)) drugs
-					from @tableQualifier.drug_exposure de
-					group by person_id
-				) as drugs
-			on c.subject_id = drugs.person_id
-			join 
-				@tableQualifier.person p 
-			on c.subject_id = p.person_id
-			join
-				@tableQualifier.concept gc
-			on p.gender_concept_id = gc.concept_id
-			where cohort_definition_id = @cohortDefinitionId
-		) as f		
+        (select subject_id, cohort_start_date, cohort_end_date,
+				gc.concept_name as gender,
+				cast(floor((year(cohort_start_date) - year_of_birth) / 10) * 10 as varchar(5)) + '-' +
+					cast(floor((year(cohort_start_date) - year_of_birth) / 10 + 1) * 10 - 1 as varchar(5)) as age,
+				conditions.conditions,
+				drugs.drugs
+		from @resultsTableQualifier.cohort c
+		join
+			(select person_id,
+					round(count(*),
+					cast(- floor(log10(abs(count(*) + 0.01))) as int)) conditions
+			from @tableQualifier.condition_occurrence co
+			group by person_id
+			) as conditions
+		on c.subject_id = conditions.person_id
+		join
+			(select person_id,
+					round(count(*), cast(- floor(log10(abs(count(*) + 0.01))) as int)) drugs
+			from @tableQualifier.drug_exposure de
+			group by person_id
+			) as drugs
+		on c.subject_id = drugs.person_id
+		join @tableQualifier.person p on c.subject_id = p.person_id
+		join @tableQualifier.concept gc on p.gender_concept_id = gc.concept_id
+		where cohort_definition_id = @cohortDefinitionId
+		) as cohort_people		
     /*whereclause*/
 )
 select * from
@@ -42,4 +35,3 @@ select * from
  from breakdown
 ) withrows
 where row_limit <= @rows/@groups
-

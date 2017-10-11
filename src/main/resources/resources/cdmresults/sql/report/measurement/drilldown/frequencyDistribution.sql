dfae5919ff33f97ee871454cff82e5ce5e039ff1
Anthony Sena <asena5@its.jnj.com>
2017-10-11 17:15:25 -0400

    Measurement conversion to snake case; fixes for condition_era and procedure

--- src/main/resources/resources/cdmresults/sql/report/measurement/drilldown/frequencyDistribution.sql
+++ src/main/resources/resources/cdmresults/sql/report/measurement/drilldown/frequencyDistribution.sql
@@ -1,8 +1,8 @@
 SELECT 
-	c1.concept_id								AS "conceptId",
-	c1.concept_name								AS "conceptName",
-	CAST(ROUND((100.0*num.count_value / denom.count_value), 0) AS INT) AS "yNumPersons",
-	num.stratum_2								AS "xCount"
+	c1.concept_id								AS concept_id,
+	c1.concept_name								AS concept_name",
+	CAST(ROUND((100.0*num.count_value / denom.count_value), 0) AS INT) AS y_num_persons,
+	num.stratum_2								AS x_Count
 FROM 
 	(SELECT count_value FROM @results_database_schema.ACHILLES_results WHERE analysis_id = 1) denom,
 	(SELECT CAST(stratum_1 AS INT) stratum_1, CAST(stratum_2 AS INT) stratum_2, count_value 
--- src/main/resources/resources/cdmresults/sql/report/procedure/drilldown/prevalenceByMonth.sql
+++ src/main/resources/resources/cdmresults/sql/report/procedure/drilldown/prevalenceByMonth.sql
@@ -3,7 +3,7 @@ SELECT
   c1.concept_name                                              AS concept_name,
   num.stratum_2                                                AS x_calendar_month,
   -- calendar year, note, there could be blanks
-  round(1000 * (1.0 * num.count_value / denom.count_value), 5) AS yPrevalence1000Pp --prevalence, per 1000 persons
+  round(1000 * (1.0 * num.count_value / denom.count_value), 5) AS y_prevalence_1000_pp --prevalence, per 1000 persons
 FROM
   (SELECT *
    FROM @results_database_schema.ACHILLES_results WHERE analysis_id = 602) num

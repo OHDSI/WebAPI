50f0558f33ea3074a00d1edaff36ac69288bbb4b
Maria Pozhidaeva <maria.pozhidaeva@odysseusinc.com>
2017-12-14 15:55:11 +0300

    licenses

--- src/main/java/org/ohdsi/webapi/AuthDataSource.java
+++ src/main/java/org/ohdsi/webapi/AuthDataSource.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Authors: Mikhail Mironov
+ * Authors: Mikhail Mironov, Vitaly Koulakov
  *
  */
 package org.ohdsi.webapi;

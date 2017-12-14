2f915da80db409300a052e19867919c6197b4c2c
Maria Pozhidaeva <maria.pozhidaeva@odysseusinc.com>
2017-12-14 19:35:03 +0300

    improvements of migrations

--- src/main/resources/db/migration/sqlserver/V1.0.11.0__data-cohortanalysis-permission.sql
+++ src/main/resources/db/migration/sqlserver/V1.0.11.0__data-cohortanalysis-permission.sql
@@ -1,7 +1,10 @@
 CREATE SEQUENCE ${ohdsiSchema}.sec_permission_id_seq START WITH 500;
+ALTER TABLE ${ohdsiSchema}.sec_permission ADD CONSTRAINT permission_unique UNIQUE (value);
 
 insert into ${ohdsiSchema}.sec_permission (id, value, description)
 values (NEXT VALUE FOR ${ohdsiSchema}.sec_permission_id_seq, 'cohortanalysis:post', 'Create Cohort analyses');
+
+ALTER TABLE ${ohdsiSchema}.sec_role_permission ADD CONSTRAINT role_permission_unique UNIQUE (role_id, permission_id);
 
 insert into ${ohdsiSchema}.sec_role_permission(role_id, permission_id)
 SELECT sr.id, sp.id

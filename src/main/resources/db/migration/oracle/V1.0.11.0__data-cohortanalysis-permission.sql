d3551afaca63862ee60bfa06b9f4e78e463a3619
chen-regen <cwen@regenstrief.org>
2018-02-21 16:40:12 -0500

    Fixing security stuff for Oracle =>
    - AuthDataSource: comment out setSchema, it's thowring exceptions on
    startup
    - application.properties: put placeholder for hikari properties (so they
    can be overwritten by pom)
    - pom default values for hikari properties
    - bunch of SEC_ROLE_PERMISSION_SEQUENCE sequence fixes for generating ID
    - remove "if exists" from remove_password script

--- src/main/resources/db/migration/oracle/V1.0.11.0__data-cohortanalysis-permission.sql
+++ src/main/resources/db/migration/oracle/V1.0.11.0__data-cohortanalysis-permission.sql
@@ -6,9 +6,9 @@ values (${ohdsiSchema}.sec_permission_id_seq.nextval, 'cohortanalysis:post', 'Cr
 
 ALTER TABLE ${ohdsiSchema}.sec_role_permission ADD CONSTRAINT role_permission_unique UNIQUE (role_id, permission_id);
 
-insert into ${ohdsiSchema}.sec_role_permission(role_id, permission_id)
-SELECT sr.id, sp.id
+insert into ${ohdsiSchema}.sec_role_permission(id, role_id, permission_id)
+SELECT ${ohdsiSchema}.SEC_ROLE_PERMISSION_SEQUENCE.nextval, sr.id, sp.id
     FROM ${ohdsiSchema}.sec_permission sp,
       ${ohdsiSchema}.sec_role sr
-    WHERE sp."value" = 'cohortanalysis:post'
+    WHERE sp.value = 'cohortanalysis:post'
       AND sr.name IN ('admin', 'cohort creator', 'Atlas users');

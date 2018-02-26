23f0c0ec34b7f6c60f14ccdc612aab2a625d1407
Anton Gackovka <anton.gackovka@odysseusinc.com>
2018-02-26 15:01:09 +0300

    ATLP-169-netezza-support reverted code

--- src/main/java/org/ohdsi/webapi/JerseyConfig.java
+++ src/main/java/org/ohdsi/webapi/JerseyConfig.java
@@ -3,30 +3,6 @@ package org.ohdsi.webapi;
 import org.glassfish.jersey.message.GZipEncoder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.filter.EncodingFilter;
-import org.ohdsi.webapi.service.ActivityService;
-import org.ohdsi.webapi.service.CDMResultsService;
-import org.ohdsi.webapi.service.CohortAnalysisService;
-import org.ohdsi.webapi.service.CohortDefinitionService;
-import org.ohdsi.webapi.service.CohortResultsService;
-import org.ohdsi.webapi.service.CohortService;
-import org.ohdsi.webapi.service.ComparativeCohortAnalysisService;
-import org.ohdsi.webapi.service.ConceptSetService;
-import org.ohdsi.webapi.service.DDLService;
-import org.ohdsi.webapi.service.EvidenceService;
-import org.ohdsi.webapi.service.FeasibilityService;
-import org.ohdsi.webapi.service.FeatureExtractionService;
-import org.ohdsi.webapi.service.IRAnalysisService;
-import org.ohdsi.webapi.service.InfoService;
-import org.ohdsi.webapi.service.JobService;
-import org.ohdsi.webapi.service.PatientLevelPredictionService;
-import org.ohdsi.webapi.service.PersonService;
-import org.ohdsi.webapi.service.RSBProxyService;
-import org.ohdsi.webapi.service.SourceService;
-import org.ohdsi.webapi.service.SparqlService;
-import org.ohdsi.webapi.service.SqlRenderService;
-import org.ohdsi.webapi.service.TherapyPathResultsService;
-import org.ohdsi.webapi.service.UserService;
-import org.ohdsi.webapi.service.VocabularyService;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -50,30 +26,6 @@ public class JerseyConfig extends ResourceConfig implements InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception {
         packages(this.rootPackage);
-        register(ActivityService.class);
-        register(CDMResultsService.class);
-        register(CohortAnalysisService.class);
-        register(CohortDefinitionService.class);
-        register(CohortResultsService.class);
-        register(CohortService.class);
-        register(ComparativeCohortAnalysisService.class);
-        register(ConceptSetService.class);
-        register(EvidenceService.class);
-        register(FeasibilityService.class);
-        register(InfoService.class);
-        register(IRAnalysisService.class);
-        register(JobService.class);
-        register(PersonService.class);
-        register(RSBProxyService.class);
-        register(PatientLevelPredictionService.class);
-        register(SourceService.class);
-        register(SparqlService.class);
-        register(SqlRenderService.class);
-        register(DDLService.class);
-        register(TherapyPathResultsService.class);
-        register(UserService.class);
-        register(VocabularyService.class);
-        register(FeatureExtractionService.class);
     }
     
 }

ff0604ec2a23cd3958a00ca841d0466dc7dd647b
Anton Gackovka <anton.gackovka@odysseusinc.com>
2018-02-27 18:52:14 +0300

    ATLP-167-merge-ple-plp deleted timestamp fix

--- src/main/resources/db/migration/postgresql/V2.2.0.201802131406466__timestamps-with-tz.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE ${ohdsiSchema}.cohort_definition ALTER COLUMN created_date TYPE TIMESTAMP(3) WITH TIME ZONE;
-ALTER TABLE ${ohdsiSchema}.cohort_definition ALTER COLUMN modified_date TYPE TIMESTAMP(3) WITH TIME ZONE;
-
-ALTER TABLE ${ohdsiSchema}.ir_analysis ALTER COLUMN created_date TYPE TIMESTAMP(3) WITH TIME ZONE;
-ALTER TABLE ${ohdsiSchema}.ir_analysis ALTER COLUMN modified_date TYPE TIMESTAMP(3) WITH TIME ZONE;
-
-ALTER TABLE ${ohdsiSchema}.plp ALTER COLUMN created TYPE TIMESTAMP(3) WITH TIME ZONE;
-ALTER TABLE ${ohdsiSchema}.plp ALTER COLUMN modified TYPE TIMESTAMP(3) WITH TIME ZONE;

b71e663c56206b9a0260b718ba0abef38da3c349
Anton Gackovka <agackovka@gmail.com>
2018-03-12 19:34:47 +0300

    Add Netezza support (#334)
    * ATLP-169-netezza-support

--- src/main/java/org/ohdsi/webapi/DataAccessConfig.java
+++ src/main/java/org/ohdsi/webapi/DataAccessConfig.java
@@ -63,7 +63,7 @@ public class DataAccessConfig {
         //note autocommit defaults vary across vendors. use provided @Autowired TransactionTemplate
 
         String[] supportedDrivers;
-        supportedDrivers = new String[]{"org.postgresql.Driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver", "oracle.jdbc.driver.OracleDriver", "com.amazon.redshift.jdbc.Driver", "com.cloudera.impala.jdbc4.Driver", "net.starschema.clouddb.jdbc.BQDriver"};
+        supportedDrivers = new String[]{"org.postgresql.Driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver", "oracle.jdbc.driver.OracleDriver", "com.amazon.redshift.jdbc.Driver", "com.cloudera.impala.jdbc4.Driver", "net.starschema.clouddb.jdbc.BQDriver", "org.netezza.Driver"};
         for (String driverName : supportedDrivers) {
             try {
                 Class.forName(driverName);

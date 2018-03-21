376ce931c2884f325e09941569d3b8312790eedb
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2018-03-21 16:03:10 +0300

    Revert: dockerize

--- src/main/resources/db/migration/oracle/V1.0.10.3__data-vocabulary-permissions.sql
+++
@@ -1,8 +0,0 @@
-insert into ${ohdsiSchema}.sec_permission(id, value, description) values(113, 'vocabulary:*:*:get', 'Vocabulary services');
-insert into ${ohdsiSchema}.sec_permission(id, value, description) values(114, 'vocabulary:*:*:post', 'Vocabulary services');
-insert into ${ohdsiSchema}.sec_permission(id, value, description) values(115, 'vocabulary:*:*:*:get', 'Vocabulary services');
-insert into ${ohdsiSchema}.sec_permission(id, value, description) values(116, 'vocabulary:*:*:*:post', 'Vocabulary services');
-insert into ${ohdsiSchema}.sec_role_permission(id, role_id, permission_id) values(sec_role_permission_sequence.NEXTVAL, 10, 113);
-insert into ${ohdsiSchema}.sec_role_permission(id, role_id, permission_id) values(sec_role_permission_sequence.NEXTVAL, 10, 114);
-insert into ${ohdsiSchema}.sec_role_permission(id, role_id, permission_id) values(sec_role_permission_sequence.NEXTVAL, 10, 115);
-insert into ${ohdsiSchema}.sec_role_permission(id, role_id, permission_id) values(sec_role_permission_sequence.NEXTVAL, 10, 116);

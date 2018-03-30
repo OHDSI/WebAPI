a3afc2ed2c3d518f604b48c00c056ec7cd8799cb
Pavel Grafkin <pavgra@gmail.com>
2018-03-30 11:36:33 +0300

    SQL optimizations

--- src/main/resources/resources/cohortresults/sql/healthcareutilization/getVisitConceptsForAnalysis.sql
+++ src/main/resources/resources/cohortresults/sql/healthcareutilization/getVisitConceptsForAnalysis.sql
@@ -1,4 +1,7 @@
-select distinct concept_id, concept_name 
-from @results_schema.heracles_results_dist 
-join @vocabulary_schema.concept on cast(concept_id as varchar(19)) = stratum_2
+with filtered_heracles_results_dist as (
+    select * from @results_schema.heracles_results_dist where stratum_2 <> ''
+)
+select distinct concept_id, concept_name
+from filtered_heracles_results_dist
+join @vocabulary_schema.concept on concept_id = cast(stratum_2 as INTEGER)
 where analysis_id = @analysis_id and cohort_definition_id = @cohort_definition_id

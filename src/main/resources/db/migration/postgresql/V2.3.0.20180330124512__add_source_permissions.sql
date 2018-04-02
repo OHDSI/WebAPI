813de7a1ad7c5a3f474ee77aa60ff47e06700a14
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2018-04-02 11:43:15 +0300

    migrations collapsed id to sequences nextval changed

--- src/main/resources/db/migration/postgresql/V2.3.0.20180330124512__add_source_permissions.sql
+++ src/main/resources/db/migration/postgresql/V2.3.0.20180330124512__add_source_permissions.sql
@@ -1,8 +1,34 @@
-INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description) VALUES(300, 'source:post', 'Create source');
-INSERT INTO ${ohdsiSchema}.sec_role_permission(role_id, permission_id) VALUES(1, 300);
+INSERT INTO ${ohdsiSchema}.sec_permission (id, value, description)
+  SELECT
+    nextval('${ohdsiSchema}.sec_permission_id_seq'),
+    'source:*:daimons:*:set-priority:post',
+    'Set priority daimons';
 
-INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description) VALUES(301, 'source:*:put', 'Edit source');
-INSERT INTO ${ohdsiSchema}.sec_role_permission (role_id, permission_id) VALUES (1, 301);
+INSERT INTO ${ohdsiSchema}.sec_role_permission (role_id, permission_id)
+  SELECT
+    sr.id,
+    sp.id
+  FROM ${ohdsiSchema}.sec_permission sp,
+    ${ohdsiSchema}.sec_role sr
+  WHERE sp."value" = 'source:*:daimons:*:set-priority:post'
+        AND sr.name IN ('admin');
 
-INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description) VALUES(302, 'source:*:delete', 'Delete source');
-INSERT INTO ${ohdsiSchema}.sec_role_permission(role_id, permission_id) VALUES(1, 302);
+INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description) VALUES(nextval('${ohdsiSchema}.sec_permission_id_seq'), 'source:post', 'Create source');
+INSERT INTO ${ohdsiSchema}.sec_role_permission
+  SELECT sr.id, sp.id
+  FROM ${ohdsiSchema}.sec_permission sp,
+    ${ohdsiSchema}.sec_role sr
+  WHERE sp."value" = 'source:post' AND sr.name IN ('admin');
+
+INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description) VALUES(nextval('${ohdsiSchema}.sec_permission_id_seq'), 'source:*:put', 'Edit source');
+INSERT INTO ${ohdsiSchema}.sec_role_permission (role_id, permission_id)
+  SELECT sr.id, sp.id
+  FROM ${ohdsiSchema}.sec_permission sp,
+    ${ohdsiSchema}.sec_role sr
+  WHERE sp."value" = 'source:*:put' AND sr.name IN ('admin');
+
+INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description) VALUES(nextval('${ohdsiSchema}.sec_permission_id_seq'), 'source:*:delete', 'Delete source');
+INSERT INTO ${ohdsiSchema}.sec_role_permission(role_id, permission_id)
+  SELECT sr.id, sp.id
+  FROM ${ohdsiSchema}.sec_role sr
+  WHERE sp."value" = 'source:*:delete' AND sr.name IN ('admin');

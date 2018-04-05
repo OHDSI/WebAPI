323004101945f151c3fffd570cae1d958308b75c
Anthony Sena <asena5@its.jnj.com>
2018-04-05 16:19:53 -0400

    Cleanup inconsistent sequence

--- src/main/resources/db/migration/postgresql/V2.2.5.20180215105415__separate-sequences.sql
+++ src/main/resources/db/migration/postgresql/V2.2.5.20180215105415__separate-sequences.sql
@@ -12,3 +12,8 @@ SELECT setval('${ohdsiSchema}.ir_analysis_sequence', coalesce(max(id), 1)) FROM 
 
 CREATE SEQUENCE ${ohdsiSchema}.plp_sequence;
 SELECT setval('${ohdsiSchema}.plp_sequence', coalesce(max(plp_id), 1)) FROM ${ohdsiSchema}.plp;
+
+CREATE SEQUENCE ${ohdsiSchema}.negative_controls_sequence;
+SELECT setval('${ohdsiSchema}.negative_controls_sequence', coalesce(max(id), 1)) FROM ${ohdsiSchema}.concept_set_negative_controls;
+ALTER TABLE ${ohdsiSchema}.CONCEPT_SET_NEGATIVE_CONTROLS ALTER COLUMN id SET DEFAULT nextval('${ohdsiSchema}.negative_controls_sequence');
+DROP SEQUENCE IF EXISTS ${ohdsiSchema}.CONCEPT_SET_NEGATIVE_CONTROLS_SEQUENCE;

6a5a0f57a34727e2013d23028cf0fe1f50cfa9e0
Anthony Sena <asena5@its.jnj.com>
2018-04-05 13:45:31 -0400

    Fixes #377 again

--- src/main/resources/db/migration/postgresql/V2.2.5.20180215105415__separate-sequences.sql
+++ src/main/resources/db/migration/postgresql/V2.2.5.20180215105415__separate-sequences.sql
@@ -4,15 +4,6 @@ SELECT setval('${ohdsiSchema}.cca_sequence', coalesce(max(cca_id), 1)) FROM ${oh
 CREATE SEQUENCE ${ohdsiSchema}.cohort_definition_sequence;
 SELECT setval('${ohdsiSchema}.cohort_definition_sequence', coalesce(max(id), 1)) FROM ${ohdsiSchema}.cohort_definition;
 
--- concept_set_sequence already exists - no need to create
-SELECT setval('${ohdsiSchema}.concept_set_sequence', coalesce(max(concept_set_id), 1)) FROM ${ohdsiSchema}.concept_set;
-
--- concept_set_item_sequence already exists - no need to create
-SELECT setval('${ohdsiSchema}.concept_set_item_sequence', coalesce(max(concept_set_item_id), 1)) FROM ${ohdsiSchema}.concept_set_item;
-
--- negative_controls_sequence already exists - no need to create
-SELECT setval('${ohdsiSchema}.CONCEPT_SET_NEGATIVE_CONTROLS_SEQUENCE', coalesce(max(id), 1)) FROM ${ohdsiSchema}.concept_set_negative_controls;
-
 CREATE SEQUENCE ${ohdsiSchema}.feasibility_study_sequence;
 SELECT setval('${ohdsiSchema}.feasibility_study_sequence', coalesce(max(id), 1)) FROM ${ohdsiSchema}.feasibility_study;
 

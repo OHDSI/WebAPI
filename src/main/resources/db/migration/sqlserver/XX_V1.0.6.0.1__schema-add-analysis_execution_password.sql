6cec038a0130d820728427f9ab570858e1f63b4b
Chris Knoll <cknoll@ohdsi.org>
2018-04-26 17:52:12 -0400

    Addressing Arachne Exectuion Engine issues
    -Renamed migration script to disable it.
    -Added required properties to pom.xml.

--- src/main/resources/db/migration/sqlserver/XX_V1.0.6.0.1__schema-add-analysis_execution_password.sql
+++ src/main/resources/db/migration/sqlserver/XX_V1.0.6.0.1__schema-add-analysis_execution_password.sql
@@ -1,26 +1,26 @@
-CREATE TABLE IF NOT EXISTS ${ohdsiSchema}.cca_execution_ext (
+CREATE TABLE ${ohdsiSchema}.cca_execution_ext (
   cca_execution_id INTEGER NOT NULL,
-  update_password  VARCHAR
+  update_password  VARCHAR(255)
 );
 
-ALTER TABLE IF EXISTS ${ohdsiSchema}.input_files
-  DROP CONSTRAINT IF EXISTS fk_sof_cca_execution;
+ALTER TABLE ${ohdsiSchema}.input_files
+  DROP CONSTRAINT fk_sof_cca_execution;
 
-ALTER TABLE IF EXISTS ${ohdsiSchema}.output_files
-  DROP CONSTRAINT IF EXISTS fk_sif_cca_execution;
+ALTER TABLE ${ohdsiSchema}.output_files
+  DROP CONSTRAINT fk_sif_cca_execution;
 
 ALTER TABLE ${ohdsiSchema}.cca_execution
-  DROP CONSTRAINT IF EXISTS cca_execution_pk;
+  DROP CONSTRAINT cca_execution_pk;
 
 ALTER TABLE ${ohdsiSchema}.cca_execution
   ADD CONSTRAINT cca_execution_pk PRIMARY KEY (cca_execution_id);
 
-CREATE TABLE IF NOT EXISTS ${ohdsiSchema}.input_files
+CREATE TABLE ${ohdsiSchema}.input_files
 (
   id               BIGINT,
   cca_execution_id BIGINT                 NOT NULL,
-  file_contents    BYTEA,
-  file_name        CHARACTER VARYING(255) NOT NULL,
+  file_contents    VARBINARY(max),
+  file_name        VARCHAR(255) NOT NULL,
   CONSTRAINT input_files_pkey PRIMARY KEY (id)
 );
 
@@ -29,12 +29,12 @@ ALTER TABLE ${ohdsiSchema}.input_files
   REFERENCES ${ohdsiSchema}.cca_execution (cca_execution_id)
   ON UPDATE NO ACTION ON DELETE CASCADE;
 
-CREATE TABLE IF NOT EXISTS ${ohdsiSchema}.output_files
+CREATE TABLE ${ohdsiSchema}.output_files
 (
   id               BIGINT,
   cca_execution_id BIGINT                 NOT NULL,
-  file_contents    BYTEA,
-  file_name        CHARACTER VARYING(255) NOT NULL,
+  file_contents    VARBINARY(max),
+  file_name        VARCHAR(255) NOT NULL,
   CONSTRAINT output_files_pkey PRIMARY KEY (id)
 );
 

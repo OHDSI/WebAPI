7f7339b0617c9bd2a82220bcdce9e81c57b7c350
Pavel Grafkin <pavgra@gmail.com>
2018-04-26 16:18:46 -0400

    Performance improvements for Visits and Drugs reports at Postgres

--- src/main/resources/resources/cohortresults/sql/healthcareutilization/getDrugTypes.sql
+++ src/main/resources/resources/cohortresults/sql/healthcareutilization/getDrugTypes.sql
@@ -1,8 +1,5 @@
-with filtered_heracles_results as (
-    select * from @results_schema.heracles_results where stratum_3 <> ''
-)
 select distinct concept_id, concept_name
-from filtered_heracles_results
-join @vocabulary_schema.concept on concept_id = cast(stratum_3 as INTEGER)
+from @results_schema.heracles_results
+join @vocabulary_schema.concept on concept_id = cast((CASE WHEN stratum_3 <> '' THEN stratum_3 ELSE '0' END) as INTEGER)
 where analysis_id in (@analysis_id_list) and cohort_definition_id = @cohort_definition_id and (stratum_2 = CAST(@drug_concept_id AS VARCHAR(255)) or CAST(@drug_concept_id AS VARCHAR(255)) is null)
 ;

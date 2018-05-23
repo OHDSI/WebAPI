d28509e7f6e3ee21c9885edaf887bd1a6a434aa4
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2018-05-23 23:34:22 +0300

    Not finished Cohorts / IRs should be invalidated on startup (#438)

---
+++ src/main/java/org/ohdsi/webapi/IExecutionInfo.java
@@ -0,0 +1,16 @@
+package org.ohdsi.webapi;
+
+import java.util.Date;
+
+public interface IExecutionInfo<T extends IExecutionInfo> {
+    Date getStartTime();
+    Integer getExecutionDuration();
+    GenerationStatus getStatus();
+    boolean getIsValid();
+    String getMessage();
+    T setStartTime(Date startTime);
+    T setExecutionDuration(Integer executionDuration);
+    T setStatus(GenerationStatus status);
+    T setIsValid(boolean valid);
+    T setMessage(String message);
+}

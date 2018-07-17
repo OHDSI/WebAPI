09da5f87c2f3a4586429259e169b625a16694a57
Pavel Grafkin <pavgra@gmail.com>
2018-07-17 17:57:22 -0400

    Fix Heracles execution in Impala (#509)
    * Fixes for Heracles calculation and results retrieval in Impala
    * Specified explicit varchar limits for integer-based fields
    * Fix CTE inserts for PDW.

--- src/main/resources/resources/cohortresults/sql/measurement/byConcept/sqlValuesRelativeToNorm.sql
+++ src/main/resources/resources/cohortresults/sql/measurement/byConcept/sqlValuesRelativeToNorm.sql
@@ -1,7 +1,7 @@
 select c1.concept_id as observation_concept_id, 
 	c1.concept_name as observation_concept_name,
 	c2.concept_id as concept_id,
-	c2.concept_name + ': ' + hr1.stratum_3 as concept_name, 
+	CONCAT(c2.concept_name, ': ', hr1.stratum_3) as concept_name,
 	hr1.count_value as count_value
 from @ohdsi_database_schema.heracles_results hr1
 	inner join @cdm_database_schema.concept c1 on hr1.stratum_1 = CAST(c1.concept_id as VARCHAR)

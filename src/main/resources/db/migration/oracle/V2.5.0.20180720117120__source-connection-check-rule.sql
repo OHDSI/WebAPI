bf9d1a41ba7eed6eb1e4a5e4d3f68f910384ad88
Anton Gackovka <anton.gackovka@odysseusinc.com>
2018-07-23 19:09:02 +0300

    ATLP-444. Fixed sequence value retrieving

--- src/main/resources/db/migration/oracle/V2.5.0.20180720117120__source-connection-check-rule.sql
+++ src/main/resources/db/migration/oracle/V2.5.0.20180720117120__source-connection-check-rule.sql
@@ -1,4 +1,4 @@
-INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description) VALUES(${ohdsiSchema}.sec_permission_id_seq, 'source:connection:*:get', 'Check source connection');
+INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description) VALUES(${ohdsiSchema}.sec_permission_id_seq.nextval, 'source:connection:*:get', 'Check source connection');
 INSERT INTO ${ohdsiSchema}.sec_role_permission(role_id, permission_id)
   SELECT sr.id, sp.id
   FROM ${ohdsiSchema}.sec_permission sp, ${ohdsiSchema}.sec_role sr

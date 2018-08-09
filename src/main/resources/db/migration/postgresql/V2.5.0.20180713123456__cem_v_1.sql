9924dbe09b851c5847e390f2d056bcbcf9045066
Chris Knoll <cknoll@ohdsi.org>
2018-08-09 13:15:23 -0400

    Removed IF EXISTS checks.

--- src/main/resources/db/migration/postgresql/V2.5.0.20180713123456__cem_v_1.sql
+++ src/main/resources/db/migration/postgresql/V2.5.0.20180713123456__cem_v_1.sql
@@ -1,7 +1,7 @@
 DELETE FROM ${ohdsiSchema}.CONCEPT_SET_GENERATION_INFO;
 
-DROP TABLE IF EXISTS ${ohdsiSchema}.CONCEPT_SET_NEGATIVE_CONTROLS;
-DROP SEQUENCE IF EXISTS ${ohdsiSchema}.negative_controls_sequence;
+DROP TABLE ${ohdsiSchema}.CONCEPT_SET_NEGATIVE_CONTROLS;
+DROP SEQUENCE ${ohdsiSchema}.negative_controls_sequence;
 CREATE SEQUENCE ${ohdsiSchema}.negative_controls_sequence;
 CREATE TABLE ${ohdsiSchema}.CONCEPT_SET_NEGATIVE_CONTROLS (
     id INTEGER NOT NULL DEFAULT NEXTVAL('negative_controls_sequence'),

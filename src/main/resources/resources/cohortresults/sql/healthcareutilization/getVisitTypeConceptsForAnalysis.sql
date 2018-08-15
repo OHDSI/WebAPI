32ef5b5e6201c2d99ededf2a4819819545d67df6
Pavel Grafkin <pavgra@gmail.com>
2018-08-15 14:41:56 -0400

    Removed dummy "No matching concept" option in filters for Cost & Util reports (cherry picked from commit 41fde0a)

--- src/main/resources/resources/cohortresults/sql/healthcareutilization/getVisitTypeConceptsForAnalysis.sql
+++ src/main/resources/resources/cohortresults/sql/healthcareutilization/getVisitTypeConceptsForAnalysis.sql
@@ -1,4 +1,4 @@
 select distinct concept_id, concept_name
 from @results_schema.heracles_results_dist
-join @vocabulary_schema.concept on concept_id = cast((CASE WHEN stratum_3 <> '' THEN stratum_3 ELSE '0' END) as INTEGER)
-where analysis_id = @analysis_id and cohort_definition_id = @cohort_definition_id
+join @vocabulary_schema.concept on concept_id = cast(stratum_3 as INTEGER)
+where analysis_id = @analysis_id and cohort_definition_id = @cohort_definition_id and stratum_3 <> ''

7cb5b2baeddbd46ca1fa6069a8021cd09d486fe5
Chris Knoll <cknoll@ohdsi.org>
2018-08-18 19:57:47 -0400

    Fixing oracle and sqlserver migrations.

--- src/main/resources/db/migration/sqlserver/V2.5.0.20180817154116__add-extra-import-permissions.sql
+++ src/main/resources/db/migration/sqlserver/V2.5.0.20180817154116__add-extra-import-permissions.sql
@@ -2,7 +2,7 @@ INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description)
   SELECT NEXT VALUE FOR ${ohdsiSchema}.sec_permission_id_seq,
         'user:import:*:test:get', 'Check LDAP/AD connection';
 
-INSERT INTO ${ohdsiSchema}.sec_role_permission (role_id, permission_id)
-  SELECT sr.id, sp.id
+INSERT INTO ${ohdsiSchema}.sec_role_permission (id, role_id, permission_id)
+  SELECT NEXT VALUE FOR ${ohdsiSchema}.SEC_ROLE_PERMISSION_SEQUENCE, sr.id, sp.id
   FROM ${ohdsiSchema}.sec_permission sp, ${ohdsiSchema}.sec_role sr
   WHERE sp."value" = 'user:import:*:test:get' AND sr.name IN ('admin');

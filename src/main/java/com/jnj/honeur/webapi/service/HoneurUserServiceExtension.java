e0eba9243c3584574ae59df9cc23a7273efacc00
Sander Bylemans <sbylemans@gmail.com>
2018-09-19 10:24:29 +0200

    Only enable empty user list on central side

--- src/main/java/com/jnj/honeur/webapi/service/HoneurUserServiceExtension.java
+++ src/main/java/com/jnj/honeur/webapi/service/HoneurUserServiceExtension.java
@@ -1,30 +1,24 @@
 package com.jnj.honeur.webapi.service;
 
 import com.jnj.honeur.security.SecurityUtils2;
-import com.jnj.honeur.webapi.hss.StorageServiceClient;
-import com.jnj.honeur.webapi.hssserviceuser.HSSServiceUserEntity;
-import com.jnj.honeur.webapi.hssserviceuser.HSSServiceUserRepository;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.ohdsi.webapi.service.UserService;
 import org.ohdsi.webapi.shiro.Entities.PermissionEntity;
 import org.ohdsi.webapi.shiro.PermissionManager;
-import org.ohdsi.webapi.shiro.management.Security;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 @Path("/")
 @Component
-@ConditionalOnProperty(name = "datasource.honeur.enabled", havingValue = "true")
+@ConditionalOnExpression("${datasource.honeur.enabled} and ${webapi.central}")
 public class HoneurUserServiceExtension {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HoneurUserServiceExtension.class);
@@ -47,7 +41,7 @@ public class HoneurUserServiceExtension {
     @GET
     @Path("user/permission")
     @Produces(MediaType.APPLICATION_JSON)
-    public Iterable<String> getPermissions(@HeaderParam("Authorization") String token) throws Exception{
+    public Iterable<String> getPermissions(@HeaderParam("Authorization") String token) throws Exception {
         String login = SecurityUtils2.getSubject(token.replace("Bearer ", ""));
         AuthorizationInfo authorizationInfo = this.authorizer.getAuthorizationInfo(login);
         Collection<String> permissionEntities = authorizationInfo.getStringPermissions();

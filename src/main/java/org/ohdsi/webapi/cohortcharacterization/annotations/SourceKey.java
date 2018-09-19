21ee7f13f87e91403f03de8d7f4d6f54e8ffd3ed
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2018-09-19 17:55:55 +0300

    cleanup comments

--- src/main/java/org/ohdsi/webapi/cohortcharacterization/annotations/SourceKey.java
+++ src/main/java/org/ohdsi/webapi/cohortcharacterization/annotations/SourceKey.java
@@ -5,10 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * @author vkoulakov
- * @since 9/17/18.
- */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface SourceKey {

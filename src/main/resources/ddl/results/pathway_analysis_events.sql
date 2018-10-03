606b737869a0393954b4f443e443f67a3b6f5e13
Chris Knoll <cknoll@ohdsi.org>
2018-10-03 10:49:28 -0400

    Split pathway analysis DDL into individual table DDL.

--- src/main/resources/ddl/results/pathway_analysis_events.sql
+++ src/main/resources/ddl/results/pathway_analysis_events.sql
@@ -9,11 +9,3 @@ CREATE TABLE @results_schema.pathway_analysis_events
 	cohort_start_date DATETIME NOT NULL,
 	cohort_end_date DATETIME NOT NULL
 );
-
-CREATE TABLE @results_schema.pathway_analysis_stats
-(
-  pathway_analysis_generation_id BIGINT NOT NULL,
-  target_cohort_id INTEGER NOT NULL,
-  target_cohort_count INTEGER NOT NULL,
-  pathways_count INTEGER NOT NULL
-);

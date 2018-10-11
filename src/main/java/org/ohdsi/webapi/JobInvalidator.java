b5008420d9b5058799c0587f68a989e6f34d172d
Anastasiia Klochkova <anastasiia.klochkova@odysseusinc.com>
2018-10-11 16:11:00 +0300

    ATLP-533. Made job invalidation transactional

--- src/main/java/org/ohdsi/webapi/JobInvalidator.java
+++ src/main/java/org/ohdsi/webapi/JobInvalidator.java
@@ -6,25 +6,39 @@ import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.annotation.PostConstruct;
+import java.util.Calendar;
 
 @Component
 public class JobInvalidator {
 
     @Autowired
-    JobExplorer jobExplorer;
+    private JobExplorer jobExplorer;
     @Autowired
-    JobRepository jobRepository;
+    private JobRepository jobRepository;
+    @Autowired
+    private PlatformTransactionManager transactionManager;
 
     @PostConstruct
-    private void invalidateGenerations(){
-        jobExplorer.getJobNames().forEach(
-                name -> jobExplorer.findRunningJobExecutions(name)
-                        .forEach(job -> {
-                            job.setStatus(BatchStatus.FAILED);
-                            job.setExitStatus(new ExitStatus(ExitStatus.FAILED.getExitCode(), "Invalidated by system"));
-                            jobRepository.update(job);
-                        }));
+    private void invalidateGenerations() {
+        TransactionTemplate template = new TransactionTemplate(transactionManager);
+        template.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                jobExplorer.getJobNames().forEach(
+                        name -> jobExplorer.findRunningJobExecutions(name)
+                                .forEach(job -> {
+                                    job.setStatus(BatchStatus.FAILED);
+                                    job.setExitStatus(new ExitStatus(ExitStatus.FAILED.getExitCode(), "Invalidated by system"));
+                                    job.setEndTime(Calendar.getInstance().getTime());
+                                    jobRepository.update(job);
+                                }));
+            }
+        });
     }
 }

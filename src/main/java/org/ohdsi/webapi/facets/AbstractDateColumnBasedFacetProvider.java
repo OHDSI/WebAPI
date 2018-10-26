49e0bf8ad0304d7cd0d621331c38f973a5d44262
VAntonov <valery.antonov@firstlinesoftware.com>
2018-10-26 12:56:26 +0300

    filter by several dates

--- src/main/java/org/ohdsi/webapi/facets/AbstractDateColumnBasedFacetProvider.java
+++ src/main/java/org/ohdsi/webapi/facets/AbstractDateColumnBasedFacetProvider.java
@@ -23,37 +23,42 @@ public abstract class AbstractDateColumnBasedFacetProvider extends AbstractColum
     protected abstract String getField();
 
     @Override
-    @SuppressWarnings("unchecked")
     public Predicate createPredicate(List<FilterItem> items, CriteriaBuilder cb, Root root) {
         assert items != null && !items.isEmpty();
-        if(items.size() == 1) {
-            final FilterItem item = items.get(0);
-            final Path field = root.get(getField());
-            switch (item.text) {
-                case "2+ Weeks Ago": {
-                    final Date from = getDaysFromNow(14);
-                    return cb.or(cb.lessThanOrEqualTo(field, cb.literal(from)), cb.isNull(field));
-                }
-                case "Last Week": {
-                    final Date from = getDaysFromNow(14);
-                    final Date to = getDaysFromNow(7);
-                    return cb.between(field, cb.literal(from), cb.literal(to));
-                }
-                case "This Week": {
-                    final Date from = getDaysFromNow(7);
-                    return cb.lessThanOrEqualTo(field, cb.literal(from));
-                }
-                case "Within 24 Hours": {
-                    final Date from = getDaysFromNow(1);
-                    return cb.lessThanOrEqualTo(field, cb.literal(from));
-                }
-                case "Just Now": {
-                    final Date from = Date.from(Instant.now().minusSeconds(SECONDS_IN_DAY / 100));
-                    return cb.lessThanOrEqualTo(field, cb.literal(from));
-                }
+        return items.size() == 1
+                ? createItemPredicate(items.get(0), cb, root)
+                : cb.or(items.stream().map(item -> createPredicate(items, cb, root)).toArray(Predicate[]::new));
+    }
+
+    @SuppressWarnings("unchecked")
+    private Predicate createItemPredicate(FilterItem item, CriteriaBuilder cb, Root root) {
+        final Path field = root.get(getField());
+        switch (item.text) {
+            case "2+ Weeks Ago": {
+                final Date from = getDaysFromNow(14);
+                return cb.or(cb.lessThanOrEqualTo(field, cb.literal(from)), cb.isNull(field));
             }
+            case "Last Week": {
+                final Date from = getDaysFromNow(14);
+                final Date to = getDaysFromNow(7);
+                return cb.between(field, cb.literal(from), cb.literal(to));
+            }
+            case "This Week": {
+                final Date from = getDaysFromNow(7);
+                return cb.lessThanOrEqualTo(field, cb.literal(from));
+            }
+            case "Within 24 Hours": {
+                final Date from = getDaysFromNow(1);
+                return cb.lessThanOrEqualTo(field, cb.literal(from));
+            }
+            case "Just Now": {
+                final Date from = Date.from(Instant.now().minusSeconds(SECONDS_IN_DAY / 100));
+                return cb.lessThanOrEqualTo(field, cb.literal(from));
+            }
+            default:
+                throw new IllegalArgumentException("unknown date facet value");
         }
-        return null;
+
     }
 
 
@@ -66,17 +71,14 @@ public abstract class AbstractDateColumnBasedFacetProvider extends AbstractColum
     @Override
     protected String getText(ResultSet resultSet) throws SQLException {
         final java.sql.Date date = resultSet.getDate(1);
-        if(date == null) {
-            return "2+ Weeks Ago";
-        }
-        double daysSinceCreated = (new Date().getTime() - date.getTime()) / 1000.0 / 60 / 60 / 24;
-        if (daysSinceCreated < .01) {
+        double days = date != null ? (new Date().getTime() - date.getTime()) / 1000.0 / 60 / 60 / 24 : 100500;
+        if (days < .01) {
             return "Just Now";
-        } else if (daysSinceCreated < 1) {
+        } else if (days < 1) {
             return "Within 24 Hours";
-        } else if (daysSinceCreated < 7) {
+        } else if (days < 7) {
             return "This Week";
-        } else if (daysSinceCreated < 14) {
+        } else if (days < 14) {
             return "Last Week";
         } else {
             return "2+ Weeks Ago";

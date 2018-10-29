3ab89d2723f2564ccd0e5858cba00091d49b3eba
VAntonov <valery.antonov@firstlinesoftware.com>
2018-10-29 15:47:09 +0300

    server-side facet values grouping

--- src/main/java/org/ohdsi/webapi/facets/AbstractColumnBasedFacetProvider.java
+++ src/main/java/org/ohdsi/webapi/facets/AbstractColumnBasedFacetProvider.java
@@ -6,7 +6,10 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public abstract class AbstractColumnBasedFacetProvider implements FacetProvider {
     private static final String[] GET_VALUES_PARAMS = { "values_column", "table"};
@@ -20,7 +23,14 @@ public abstract class AbstractColumnBasedFacetProvider implements FacetProvider 
     @Override
     public List<FacetItem> getValues(String entityName) {
         final String query = SqlRender.renderSql(getValuesQuery, GET_VALUES_PARAMS, new String[]{ getColumn(), entityName });
-        return jdbcTemplate.query(query, (resultSet, i) -> new FacetItem(getText(resultSet),getKey(resultSet),  resultSet.getInt(2)));
+        final Map<String, FacetItem> grouped = new HashMap<>();
+        jdbcTemplate.query(query, (resultSet) -> {
+            final String text = getText(resultSet);
+            final String key = getKey(resultSet);
+            final int count = resultSet.getInt(2);
+            grouped.merge(text, new FacetItem(text, key, count), (i1, i2) -> {i1.count += i2.count; return i1;});
+        });
+        return new ArrayList<>(grouped.values());
     }
 
     protected abstract String getKey(ResultSet resultSet) throws SQLException;

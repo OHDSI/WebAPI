032539cd600193f7be8e22f6613e981444d6920c
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2018-11-06 18:17:51 +0300

    review fixes

--- src/main/resources/resources/cohortcharacterizations/sql/prevalenceWithCriteria.sql
+++ src/main/resources/resources/cohortcharacterizations/sql/prevalenceWithCriteria.sql
@@ -1,3 +1,7 @@
+WITH qualified_events AS (
+    SELECT ROW_NUMBER() OVER () AS event_id, subject_id AS person_id, cohort_start_date AS OP_START_DATE, cohort_start_date AS start_date, cohort_end_date AS OP_END_DATE, cohort_end_date AS end_date
+    FROM @temp_database_schema.@targetTable where cohort_definition_id = @cohortId
+)
 insert into @results_database_schema.cc_results (type, fa_type, covariate_id, covariate_name, analysis_id, analysis_name, concept_id, count_value, avg_value, cohort_definition_id, cc_generation_id)
   select 'PREVALENCE' as type,
          'CRITERIA' as fa_type,
@@ -14,10 +18,6 @@ insert into @results_database_schema.cc_results (type, fa_type, covariate_id, co
     @cohortId as cohort_definition_id,
     @executionId as cc_generation_id
 from (select count(*) as sum_value from(
-         WITH qualified_events AS (
-             SELECT ROW_NUMBER() OVER () AS event_id, subject_id AS person_id, cohort_start_date AS OP_START_DATE, cohort_start_date AS start_date, cohort_end_date AS OP_END_DATE, cohort_end_date AS end_date
-             FROM @temp_database_schema.@targetTable where cohort_definition_id = @cohortId
-         )
-         select person_id from ( @groupQuery ) pi group by pi.person_id) pci) sum,
+   select person_id from ( @groupQuery ) pi group by pi.person_id) pci) sum,
   (select count(*) as total from  @temp_database_schema.@totalsTable where cohort_definition_id = @cohortId) totals
 ;

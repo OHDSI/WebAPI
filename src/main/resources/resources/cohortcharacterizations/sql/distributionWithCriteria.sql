a6ffbe9863b64613d140c605da3386bc0944ede4
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2018-11-12 19:05:33 +0300

    cc generation for distributed analysis fix

--- src/main/resources/resources/cohortcharacterizations/sql/distributionWithCriteria.sql
+++ src/main/resources/resources/cohortcharacterizations/sql/distributionWithCriteria.sql
@@ -1,17 +1,12 @@
 IF OBJECT_ID('tempdb..#events_count', 'U') IS NOT NULL
   DROP TABLE #events_count;
 
-IF OBJECT_ID('tempdb..#qualified_events', 'U') IS NOT NULL
-  DROP TABLE #qualified_events;
-
-SELECT
-  ROW_NUMBER() OVER () AS event_id,
-  subject_id AS person_id,
-  cohort_start_date AS start_date,
-  cohort_end_date AS end_date
-INTO #qualified_events
-FROM @temp_database_schema.@targetTable where cohort_definition_id = @cohortId;
-
+WITH qualified_events AS (
+  SELECT ROW_NUMBER() OVER () AS event_id, E.subject_id AS person_id, E.cohort_start_date AS start_date, E.cohort_end_date AS end_date, OP.observation_period_start_date AS op_start_date, OP.observation_period_end_date AS op_end_date
+  FROM @temp_database_schema.@targetTable E
+    JOIN @cdm_database_schema.observation_period OP ON E.subject_id = OP.person_id AND E.cohort_start_date >= OP.observation_period_start_date AND E.cohort_start_date <= OP.observation_period_end_date
+  WHERE cohort_definition_id = @cohortId
+)
 select
   v.person_id as person_id,
   count(*) as value_as_int
@@ -31,24 +26,33 @@ with
       count(*) as count_value,
       min(value_as_int) as min_value,
       max(value_as_int) as max_value,
-      avg(value_as_int) as avg_value,
-      stdev(value_as_int) as sdtev_value
-    from #events_count
+      sum(value_as_int) as sum_value,
+      stdev(value_as_int) as sdtev_value,
+      total_cohort_count.cnt - count(*) as count_no_value,
+      total_cohort_count.cnt as population_size
+    from #events_count, total_cohort_count
+    group by total_cohort_count.cnt
+  ),
+  event_prep as (select row_number() over (order by value_as_int) as rn, value_as_int, count(*) as people_count from #events_count group by value_as_int),
+  events_dist as (
+    select s.value_as_int, sum(p.people_count) as people_count
+    from event_prep s join event_prep p on p.rn <= s.rn group by s.value_as_int
   ),
   events_p10_value as (
-    select count(*) as p10 from #events_count, events_max_value where value_as_int < 0.1 * events_max_value.max_value
+    select min(value_as_int) as p10 from events_dist, event_stat_values where (people_count + count_no_value) >= 0.1 * population_size
   ),
   events_p25_value as (
-    select count(*) as p25 from #events_count, events_max_value where value_as_int < 0.25 * events_max_value.max_value
+    select min(value_as_int) as p25 from events_dist, event_stat_values where (people_count + count_no_value) >= 0.25 * population_size
   ),
   events_median_value as (
-    select count(*) as median_value from #events_count, events_max_value where value_as_int < 0.5 * events_max_value.max_value
+      select min(value_as_int) as median_value from events_dist, event_stat_values where (people_count + count_no_value) >= 0.5 * population_size
+--     select value_as_int as median_value from events_dist where people_count = (select max(people_count) from events_dist)
   ),
   events_p75_value as (
-    select count(*) as p75 from #events_count, events_max_value where value_as_int < 0.75 * events_max_value.max_value
+    select min(value_as_int) as p75 from events_dist, event_stat_values where people_count + count_no_value >= 0.75 * population_size
   ),
   events_p90_value as (
-    select count(*) as p90 from #events_count, events_max_value where value_as_int < 0.9 * events_max_value.max_value
+    select min(value_as_int) as p90 from events_dist, event_stat_values where people_count + count_no_value >= 0.9 * population_size
   )
 insert into @results_database_schema.cc_results(type, fa_type, covariate_id, covariate_name, analysis_id, analysis_name, concept_id,
   cohort_definition_id, cc_generation_id, count_value, min_value, max_value, avg_value, stdev_value, p10_value, p25_value, median_value, p75_value, p90_value)
@@ -63,19 +67,16 @@ select
   @cohortId as cohort_definition_id,
   @executionId as cc_generation_id,
   event_stat_values.count_value,
-  CASE WHEN event_stat_values.min_value = total_cohort_count.cnt THEN event_stat_values.min_value ELSE 0 END as min_value,
+  case when count_no_value = 0 then event_stat_values.min_value else 0 end as min_value,
   event_stat_values.max_value,
-  event_stat_values.avg_value,
+  cast(event_stat_values.sum_value / (1.0 * population_size) as float) as avg_value,
   event_stat_values.sdtev_value,
-  events_p10_value.p10 as p10_value,
-  events_p25_value.p25 as p25_value,
-  events_median_value.median_value,
-  events_p75_value.p75 as p75_value,
-  events_p90_value.p90 as p90_value
-from events_max_value, event_stat_values, events_p10_value, events_p25_value, events_median_value, events_p75_value, events_p90_value, total_cohort_count;
-
-truncate table #qualified_events;
-drop table #qualified_events;
+  case when population_size * .10 < count_no_value then 0 else events_p10_value.p10 end as p10_value,
+  case when population_size * .25 < count_no_value then 0 else events_p25_value.p25 end as p25_value,
+  case when population_size * .50 < count_no_value then 0 else events_median_value.median_value end as median_value,
+  case when population_size * .75 < count_no_value then 0 else events_p75_value.p75 end as p75_value,
+  case when population_size * .90 < count_no_value then 0 else events_p90_value.p90 end as p90_value
+from events_max_value, event_stat_values, events_p10_value, events_p25_value, events_median_value, events_p75_value, events_p90_value;
 
 truncate table #events_count;
 drop table #events_count;

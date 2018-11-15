4053e4b352864b3144184fe21453fe7b4e862af2
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2018-11-15 13:43:36 +0300

    Fixes Number of persons with continuous observation in each year

--- src/main/resources/resources/cohortanalysis/sql/runHeraclesAnalyses.sql
+++ src/main/resources/resources/cohortanalysis/sql/runHeraclesAnalyses.sql
@@ -756,21 +756,25 @@ DROP TABLE #raw_107;
   -- 109   Number of persons with continuous observation in each year
   -- Note: using temp table instead of nested query because this gives vastly improved performance in Oracle
    
-  SELECT DISTINCT 
-  YEAR(observation_period_start_date) AS obs_year,
-	DATEFROMPARTS(YEAR(observation_period_start_date),1,1) as obs_year_start,
-	DATEFROMPARTS(YEAR(observation_period_start_date),12,31) as obs_year_end
-  INTO
-  #temp_dates_1
-  from @CDM_schema.PERSON p1
-  inner join (select subject_id, cohort_definition_id as cohort_definition_id from @results_schema.COHORT where cohort_definition_id in (@cohort_definition_id)) c1
-  on p1.person_id = c1.subject_id
-  inner join
-  @CDM_schema.observation_period op1
-  on p1.person_id = op1.person_id
+  WITH x AS (SELECT n FROM (VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9)) v(n)),
+    years AS (SELECT ones.n + 10*tens.n + 100*hundreds.n + 1000*thousands.n as year
+              FROM x ones,     x tens,      x hundreds,       x thousands),
+    op_date_range AS (select
+                        min(observation_period_start_date) op_start_date,
+                        max(observation_period_end_date) op_end_date
+                      from @CDM_schema.PERSON p1
+                        inner join (select subject_id, cohort_definition_id as cohort_definition_id from @results_schema.COHORT where cohort_definition_id in (@cohort_definition_id)) c1
+                          on p1.person_id = c1.subject_id
+                        inner join
+                          @CDM_schema.observation_period op1
+                          on p1.person_id = op1.person_id)
+  select year as obs_year, DATEFROMPARTS(year, 1, 1) as obs_year_start, DATEFROMPARTS(year,12,31) as obs_year_end
+		into #temp_dates_1
+    from years, op_date_range where year between YEAR(op_date_range.op_start_date) AND YEAR(op_date_range.op_end_date)
   ;
-  
-  --INSERT INTO @results_schema.HERACLES_results (cohort_definition_id, analysis_id, stratum_1, count_value)
+
+
+--INSERT INTO @results_schema.HERACLES_results (cohort_definition_id, analysis_id, stratum_1, count_value)
   select c1.cohort_definition_id, 
   109 AS analysis_id,  
   obs_year AS stratum_1, 
@@ -801,19 +805,29 @@ DROP TABLE #raw_107;
   --{110 IN (@list_of_analysis_ids)}?{
   -- 110   Number of persons with continuous observation in each month
   -- Note: using temp table instead of nested query because this gives vastly improved performance in Oracle
-  
-  SELECT DISTINCT 
-  YEAR(observation_period_start_date)*100 + MONTH(observation_period_start_date) AS obs_month,
-  DATEFROMPARTS(YEAR(observation_period_start_date), MONTH(OBSERVATION_PERIOD_START_DATE), 1) AS obs_month_start,  
-  DATEADD(dd,-1,DATEADD(mm,1,DATEFROMPARTS(YEAR(observation_period_start_date), MONTH(OBSERVATION_PERIOD_START_DATE), 1))) AS obs_month_end
+
+	WITH x AS (SELECT n FROM (VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9)) v(n)),
+		years AS (SELECT ones.n + 10*tens.n + 100*hundreds.n + 1000*thousands.n as year
+							FROM x ones,     x tens,      x hundreds,       x thousands),
+		months AS (SELECT month FROM (VALUES (1), (2), (3), (4), (5), (6), (7), (8), (9), (10), (11), (12)) v(month)),
+		op_date_range AS (
+			SELECT MIN(observation_period_start_date) AS op_start_date,
+				MAX(observation_period_end_date) AS op_end_date
+			FROM @CDM_schema.PERSON p1
+			inner join (select subject_id, cohort_definition_id, cohort_start_date, cohort_end_date from #HERACLES_cohort) c1
+			 on p1.person_id = c1.subject_id
+			 inner join
+						 @CDM_schema.observation_period op1
+						 on p1.person_id = op1.person_id
+		)
+  SELECT DISTINCT
+  years.year*100 + months.month AS obs_month,
+  DATEFROMPARTS(years.year, months.month, 1) AS obs_month_start,
+  DATEADD(dd,-1,DATEADD(mm,1,DATEFROMPARTS(years.year, months.month, 1))) AS obs_month_end
   INTO
   #temp_dates_2
-  FROM @CDM_schema.PERSON p1
-  inner join (select subject_id, cohort_definition_id, cohort_start_date, cohort_end_date from #HERACLES_cohort) c1
-  on p1.person_id = c1.subject_id
-  inner join
-  @CDM_schema.observation_period op1
-  on p1.person_id = op1.person_id
+  FROM years, months, op_date_range WHERE year BETWEEN YEAR(op_date_range.op_start_date) AND YEAR(op_date_range.op_end_date)
+	ORDER BY years.year*100 + months.month
   ;
   
   
@@ -952,16 +966,23 @@ DROP TABLE #raw_107;
   -- 116   Number of persons with at least one day of observation in each year by gender and age decile
   -- Note: using temp table instead of nested query because this gives vastly improved performance in Oracle
 
-  select distinct 
-  YEAR(observation_period_start_date) as obs_year 
-  INTO
-  #temp_dates_3
-  FROM @CDM_schema.PERSON p1
-  inner join (select subject_id, cohort_definition_id, cohort_start_date, cohort_end_date from #HERACLES_cohort) c1
-  on p1.person_id = c1.subject_id
-  inner join
-  @CDM_schema.observation_period op1
-  on p1.person_id = op1.person_id
+	WITH x AS (SELECT n FROM (VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9)) v(n)),
+		years AS (SELECT ones.n + 10*tens.n + 100*hundreds.n + 1000*thousands.n as year
+							FROM x ones,     x tens,      x hundreds,       x thousands),
+		op_date_range AS (
+			select
+				MIN(observation_period_start_date) as op_start_date,
+				MAX(observation_period_end_date) as op_end_date
+			FROM @CDM_schema.PERSON p1
+			inner join (select subject_id, cohort_definition_id, cohort_start_date, cohort_end_date from #HERACLES_cohort) c1
+			on p1.person_id = c1.subject_id
+			inner join
+			@CDM_schema.observation_period op1
+			on p1.person_id = op1.person_id
+		)
+	SELECT year as obs_year
+		INTO #temp_dates_3
+	FROM years, op_date_range WHERE years.year BETWEEN YEAR(op_date_range.op_start_date) AND YEAR(op_date_range.op_end_date)
   ;
   
   --insert into @results_schema.HERACLES_results (cohort_definition_id, analysis_id, stratum_1, stratum_2, stratum_3, count_value)
@@ -998,17 +1019,26 @@ DROP TABLE #raw_107;
   --{117 IN (@list_of_analysis_ids)}?{
   -- 117   Number of persons with at least one day of observation in each year by gender and age decile
   -- Note: using temp table instead of nested query because this gives vastly improved performance in Oracle
-    
-  select distinct 
-  YEAR(observation_period_start_date)*100 + MONTH(observation_period_start_date)  as obs_month
-  into 
-  #temp_dates_4
-  FROM @CDM_schema.PERSON p1
-  inner join (select subject_id, cohort_definition_id, cohort_start_date, cohort_end_date from #HERACLES_cohort) c1
-  on p1.person_id = c1.subject_id
-  inner join
-  @CDM_schema.observation_period op1
-  on p1.person_id = op1.person_id
+
+	WITH x AS (SELECT n FROM (VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9)) v(n)),
+		years AS (SELECT ones.n + 10*tens.n + 100*hundreds.n + 1000*thousands.n as year
+							FROM x ones,     x tens,      x hundreds,       x thousands),
+		months AS (SELECT month FROM (VALUES (1), (2), (3), (4), (5), (6), (7), (8), (9), (10), (11), (12)) v(month)),
+		op_date_range AS (
+			select
+				MIN(observation_period_start_date) AS op_start_date,
+				MAX(observation_period_end_date) AS op_end_date
+			FROM @CDM_schema.PERSON p1
+			inner join (select subject_id, cohort_definition_id, cohort_start_date, cohort_end_date from #HERACLES_cohort) c1
+			on p1.person_id = c1.subject_id
+			inner join
+			@CDM_schema.observation_period op1
+			on p1.person_id = op1.person_id)
+	SELECT years.year * 100 + months.month AS obs_month
+	INTO
+		#temp_dates_4
+	FROM years, monsth, op_date_range
+	WHERE years.year BETWEEN YEAR(op_date_range.op_start_date) AND YEAR(op_date_range.op_end_date)
   ;
   
   --insert into @results_schema.HERACLES_results (cohort_definition_id, analysis_id, stratum_1, count_value)

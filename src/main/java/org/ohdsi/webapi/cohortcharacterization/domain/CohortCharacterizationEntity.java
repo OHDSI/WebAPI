cf859e43dbccfd83030701a5f399eeeccb2b4d66
Sander Bylemans <sbylemans@gmail.com>
2019-01-15 15:42:59 +0100

    Revert: Consequence of arachne.version upgrade.

--- src/main/java/org/ohdsi/webapi/cohortcharacterization/domain/CohortCharacterizationEntity.java
+++ src/main/java/org/ohdsi/webapi/cohortcharacterization/domain/CohortCharacterizationEntity.java
@@ -1,10 +1,12 @@
 package org.ohdsi.webapi.cohortcharacterization.domain;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import javax.persistence.*;
 
 import org.ohdsi.analysis.cohortcharacterization.design.CohortCharacterization;
-import org.ohdsi.analysis.cohortcharacterization.design.CohortCharacterizationStrata;
 import org.ohdsi.webapi.cohortdefinition.CohortDefinition;
 import org.ohdsi.webapi.feanalysis.domain.FeAnalysisEntity;
 import org.ohdsi.webapi.model.CommonEntity;
@@ -52,11 +54,6 @@ public class CohortCharacterizationEntity extends CommonEntity implements Cohort
     @Override
     public Set<CcParamEntity> getParameters() {
         return parameters;
-    }
-
-    @Override
-    public Collection<? extends CohortCharacterizationStrata> getStratas() {
-        return Collections.emptyList();
     }
 
     public Long getId() {

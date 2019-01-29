29f7e004f711d167ac4bcfb3bbb5cf6ba6eb34fb
Sergey Suvorov <sergey.suvorov@odysseusinc.com>
2019-01-29 17:08:07 +0300

    Created migration script with stored procedure to remove identity attribute from primary key column. Created hibernate sequence for entities with default sequence. Remove unnecessary statements(#753)

--- src/main/resources/db/migration/sqlserver/V2.7.0.20190129160657__exec_sp_removing_identities.sql
+++ src/main/resources/db/migration/sqlserver/V2.7.0.20190129160657__exec_sp_removing_identities.sql
@@ -1,19 +1,11 @@
 -- -- drop foreign keys. cannot be used inside stored proc
 ALTER TABLE ${ohdsiSchema}.output_files NOCHECK CONSTRAINT ALL;
 
--- ALTER TABLE ${ohdsiSchema}.output_files DROP CONSTRAINT fk_sif_cca_execution;
-
 GO
 
 exec ${ohdsiSchema}.remove_identity_from_tables;
 
 GO
-
--- --recreate foreign keys
--- ALTER TABLE ${ohdsiSchema}.output_files
---              ADD CONSTRAINT fk_sif_cca_execution FOREIGN KEY (cca_execution_id)
---              REFERENCES ${ohdsiSchema}.cca_execution (cca_execution_id)
---              ON UPDATE NO ACTION ON DELETE CASCADE;
 
 ALTER TABLE ${ohdsiSchema}.output_files CHECK CONSTRAINT ALL;
 

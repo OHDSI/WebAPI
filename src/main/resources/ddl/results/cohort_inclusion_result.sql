67d18e79c8faee15dc1439b478d6cd70ceb9d0bc
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-01-31 19:18:21 +0300

    removes DEFAULT values from results schema

--- src/main/resources/ddl/results/cohort_inclusion_result.sql
+++ src/main/resources/ddl/results/cohort_inclusion_result.sql
@@ -1,7 +1,7 @@
 IF OBJECT_ID('@results_schema.cohort_inclusion_result', 'U') IS NULL
 CREATE TABLE @results_schema.cohort_inclusion_result(
   cohort_definition_id int NOT NULL,
-  mode_id int NOT NULL DEFAULT 0,
+  mode_id int NOT NULL,
   inclusion_rule_mask bigint NOT NULL,
   person_count bigint NOT NULL
 );

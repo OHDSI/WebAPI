638bb936de1dca929e0c75b171f478b4d8346a0f
Anthony Sena <asena5@its.jnj.com>
2019-01-31 12:38:05 -0500

    Removed SQL Server patch script in favor of @wivern approach

--- src/main/resources/db/migration/sqlserver/V2.7.0.20190129073000__fe-analysis-drop-constraints.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE ${ohdsiSchema}.fe_analysis_criteria DROP CONSTRAINT FK_fac_su_cid;
-ALTER TABLE ${ohdsiSchema}.fe_analysis_criteria DROP CONSTRAINT FK_fac_su_mid;
-ALTER TABLE ${ohdsiSchema}.fe_analysis_criteria DROP CONSTRAINT DF_fe_analysis_criteria_cd;

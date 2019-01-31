33cfd5ef70c272b7116c8b1aff57582a6f64fb48
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-01-31 19:16:48 +0300

    removes DEFAULT values from results schema

--- src/main/resources/ddl/results/cohort_summary_stats.sql
+++ src/main/resources/ddl/results/cohort_summary_stats.sql
@@ -1,7 +1,7 @@
 IF OBJECT_ID('@results_schema.cohort_summary_stats', 'U') IS NULL
 CREATE TABLE @results_schema.cohort_summary_stats(
   cohort_definition_id int NOT NULL,
-  mode_id int NOT NULL DEFAULT 0,
+  mode_id int NOT NULL,
   base_count bigint NOT NULL,
   final_count bigint NOT NULL
 );

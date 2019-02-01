9cc5ae2bcec93391c0209c6b1874b72907f0d94e
N.K <clax1412@aumc.ac.kr>
2019-02-01 16:28:13 +0900

    Add sequence heracles and sources...

---
+++ src/main/resources/db/migration/sqlserver/V2.7.0.20190201140945__sources-created-modified-fix.sql
@@ -0,0 +1,53 @@
+-- RENAME source table
+ALTER TABLE ${ohdsiSchema}.source DROP CONSTRAINT DF_source_SOURCE_DIALECT;
+ALTER TABLE ${ohdsiSchema}.source DROP CONSTRAINT source_key_unique;
+ALTER TABLE ${ohdsiSchema}.source DROP CONSTRAINT PK_source;
+
+EXEC sp_rename '[${ohdsiSchema}].[source]', 'source_bak';
+
+CREATE SEQUENCE ${ohdsiSchema}.source_sequence
+  AS BIGINT
+  MINVALUE 0
+  NO CYCLE
+  CACHE 1;
+
+CREATE TABLE ${ohdsiSchema}.source
+(
+  SOURCE_ID         int default NEXT VALUE FOR ${ohdsiSchema}.source_sequence not null constraint PK_source primary key nonclustered,
+  SOURCE_NAME       varchar(255)                   not null,
+  SOURCE_KEY        varchar(50)                    not null constraint [source_key_unique] unique,
+  SOURCE_CONNECTION varchar(8000)                  not null,
+  SOURCE_DIALECT    varchar(255) CONSTRAINT [DF_source_SOURCE_DIALECT] DEFAULT ('sql server') not null,
+  username          nvarchar(255),
+  password          nvarchar(255),
+  krb_auth_method   varchar(10) default 'PASSWORD' not null,
+  keytab_name       varchar(50),
+  krb_keytab        varbinary(max),
+  krb_admin_server  varchar(50),
+  deleted_date      datetime
+)
+
+-- Copy previous source configuration and drop table
+INSERT INTO ${ohdsiSchema}.source SELECT * from ${ohdsiSchema}.source_bak;
+DROP TABLE ${ohdsiSchema}.source_bak;
+
+EXEC sp_rename '[${ohdsiSchema}].[source_daimon]', 'source_daimon_bak';
+
+CREATE SEQUENCE ${ohdsiSchema}.source_daimon_sequence
+  AS BIGINT
+  MINVALUE 0
+  NO CYCLE
+  CACHE 1;
+
+CREATE TABLE ${ohdsiSchema}.source_daimon
+(
+  source_daimon_id int default NEXT VALUE FOR ${ohdsiSchema}.source_sequence not null primary key,
+  source_id        int           not null,
+  daimon_type      int           not null,
+  table_qualifier  varchar(255)  not null,
+  priority         int default 0 not null
+)
+
+-- Copy previous source configuration and drop table
+INSERT INTO ${ohdsiSchema}.source_daimon SELECT * from ${ohdsiSchema}.source_daimon_bak;
+DROP TABLE ${ohdsiSchema}.source_daimon_bak;
---
+++ src/main/resources/db/migration/sqlserver/V2.7.0.20190201154512__heracles-visualization-data-fix.sql
@@ -0,0 +1,20 @@
+ALTER TABLE ${ohdsiSchema}.HERACLES_VISUALIZATION_DATA DROP CONSTRAINT PK_heracles_viz_data;
+
+EXEC sp_rename '[${ohdsiSchema}].[HERACLES_VISUALIZATION_DATA]', 'HERACLES_VISUALIZATION_DATA_bak';
+
+CREATE SEQUENCE ${ohdsiSchema}.HERACLES_VIZ_DATA_SEQUENCE
+  AS BIGINT
+  START WITH 0
+  NO CYCLE
+  CACHE 1;
+
+create table ${ohdsiSchema}.HERACLES_VISUALIZATION_DATA
+(
+  cohort_definition_id int          not null,
+  source_id            int          not null,
+  visualization_key    varchar(300) not null,
+  drilldown_id         int,
+  id                   int default NEXT VALUE FOR ${ohdsiSchema}.HERACLES_VIZ_DATA_SEQUENCE,
+  end_time             datetime,
+  data                 varchar(max)
+);

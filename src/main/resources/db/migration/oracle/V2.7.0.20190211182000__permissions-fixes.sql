6b63b840e6435eff574e1b114d882652f7e76149
Pavel Grafkin <pavgra@gmail.com>
2019-02-13 15:46:14 -0500

    Fixes for Oracle

--- src/main/resources/db/migration/oracle/V2.7.0.20190211182000__permissions-fixes.sql
+++ src/main/resources/db/migration/oracle/V2.7.0.20190211182000__permissions-fixes.sql
@@ -1,35 +1,35 @@
-ALTER TABLE ${ohdsiSchema}.sec_permission ADD COLUMN for_role_id INTEGER;
+ALTER TABLE ${ohdsiSchema}.sec_permission ADD for_role_id INTEGER;
 
 INSERT INTO ${ohdsiSchema}.sec_permission (id, value, for_role_id)
 SELECT ${ohdsiSchema}.sec_permission_id_seq.nextval, REPLACE(new_perms.val, '%s', REPLACE(REPLACE(value, 'source:', ''), ':access', '')), role_id
 FROM sec_permission sp
   JOIN sec_role_permission srp on sp.id = srp.permission_id
   CROSS JOIN (
-    SELECT 'vocabulary:%s:*:get' val
+    SELECT 'vocabulary:%s:*:get' val FROM DUAL
     UNION ALL
-    SELECT 'vocabulary:%s:included-concepts:count:post'
+    SELECT 'vocabulary:%s:included-concepts:count:post' FROM DUAL
     UNION ALL
-    SELECT 'vocabulary:%s:resolveConceptSetExpression:post'
+    SELECT 'vocabulary:%s:resolveConceptSetExpression:post' FROM DUAL
     UNION ALL
-    SELECT 'vocabulary:%s:lookup:identifiers:post'
+    SELECT 'vocabulary:%s:lookup:identifiers:post' FROM DUAL
     UNION ALL
-    SELECT 'vocabulary:%s:lookup:identifiers:ancestors:post'
+    SELECT 'vocabulary:%s:lookup:identifiers:ancestors:post' FROM DUAL
     UNION ALL
-    SELECT 'vocabulary:%s:lookup:mapped:post'
+    SELECT 'vocabulary:%s:lookup:mapped:post' FROM DUAL
     UNION ALL
-    SELECT 'vocabulary:%s:compare:post'
+    SELECT 'vocabulary:%s:compare:post' FROM DUAL
     UNION ALL
-    SELECT 'vocabulary:%s:optimize:post'
+    SELECT 'vocabulary:%s:optimize:post' FROM DUAL
     UNION ALL
-    SELECT 'cdmresults:%s:*:get'
+    SELECT 'cdmresults:%s:*:get' FROM DUAL
     UNION ALL
-    SELECT 'cdmresults:%s:*:*:get'
+    SELECT 'cdmresults:%s:*:*:get' FROM DUAL
     UNION ALL
-    SELECT 'cdmresults:%s:conceptRecordCount:post'
+    SELECT 'cdmresults:%s:conceptRecordCount:post' FROM DUAL
     UNION ALL
-    SELECT 'cohortresults:%s:*:*:get'
+    SELECT 'cohortresults:%s:*:*:get' FROM DUAL
     UNION ALL
-    SELECT 'cohortresults:%s:*:*:*:get'
+    SELECT 'cohortresults:%s:*:*:*:get' FROM DUAL
   ) new_perms
 WHERE sp.value LIKE 'source:%:access';
 
@@ -40,13 +40,17 @@ WHERE sp.for_role_id IS NOT NULL;
 
 ALTER TABLE ${ohdsiSchema}.sec_permission DROP COLUMN for_role_id;
 
-INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description)
-    VALUES
-    (${ohdsiSchema}.sec_permission_id_seq.nextval, 'conceptset:*:expression:get', 'Resolve concept set expression'),
-    (${ohdsiSchema}.sec_permission_id_seq.nextval, 'conceptset:*:generationinfo:get', 'Get generation info for concept set'),
-    (${ohdsiSchema}.sec_permission_id_seq.nextval, 'cohortdefinition:*:check:get', 'Get cohort definition design checks'),
-    (${ohdsiSchema}.sec_permission_id_seq.nextval, 'sqlrender:translate:post' , 'Translate SQL'),
-    (${ohdsiSchema}.sec_permission_id_seq.nextval, 'ir:*:info' , 'Get IR info'),
+INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description) VALUES
+    (${ohdsiSchema}.sec_permission_id_seq.nextval, 'conceptset:*:expression:get', 'Resolve concept set expression');
+INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description) VALUES
+    (${ohdsiSchema}.sec_permission_id_seq.nextval, 'conceptset:*:generationinfo:get', 'Get generation info for concept set');
+INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description) VALUES
+    (${ohdsiSchema}.sec_permission_id_seq.nextval, 'cohortdefinition:*:check:get', 'Get cohort definition design checks');
+INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description) VALUES
+    (${ohdsiSchema}.sec_permission_id_seq.nextval, 'sqlrender:translate:post' , 'Translate SQL');
+INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description) VALUES
+    (${ohdsiSchema}.sec_permission_id_seq.nextval, 'ir:*:info' , 'Get IR info');
+INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description) VALUES
     (${ohdsiSchema}.sec_permission_id_seq.nextval, 'job:type:*:name:*:get' , 'Get IR info');
 
 INSERT INTO ${ohdsiSchema}.sec_role_permission(id, role_id, permission_id)

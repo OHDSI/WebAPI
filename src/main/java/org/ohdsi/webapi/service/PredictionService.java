e32a07617a12189fd8ab57fbf5b6cc6cbfec439b
Anthony Sena <asena5@its.jnj.com>
2019-02-13 09:26:32 -0500

    Fixup compile errors after merge

--- src/main/java/org/ohdsi/webapi/service/PredictionService.java
+++ src/main/java/org/ohdsi/webapi/service/PredictionService.java
@@ -26,6 +26,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.ohdsi.analysis.Utils;
 import org.ohdsi.hydra.Hydra;
 import org.ohdsi.webapi.Constants;
 import org.ohdsi.webapi.cohortdefinition.CohortDefinitionRepository;

afd1b58c752535801a531dd836eb582b13783101
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-02-20 16:17:50 +0300

    WebAPI-805 Generation of new PLE and PLP via Execution Engine

--- src/main/resources/db/migration/oracle/V2.7.0.20190208164736__analysis_execution-add-job_id.sql
+++ src/main/resources/db/migration/oracle/V2.7.0.20190208164736__analysis_execution-add-job_id.sql
@@ -2,31 +2,34 @@ DECLARE v_col_exist NUMBER;
 BEGIN
   SELECT count(*) INTO v_col_exist FROM all_tab_cols WHERE
     UPPER(column_name) = 'JOB_EXECUTION_ID'
-    AND UPPER(table_name) = 'ANALYSIS_EXECUTION';
+    AND UPPER(table_name) = 'ANALYSIS_EXECUTION' AND owner = '${ohdsiSchema}';
   IF (v_col_exist = 0) THEN
-    execute immediate 'ALTER TABLE ${ohdsiSchema}.analysis_execution ADD job_execution_id NUMBER(19)';
-  end if;
+    EXECUTE IMMEDIATE 'ALTER TABLE ${ohdsiSchema}.analysis_execution ADD job_execution_id NUMBER(19)';
+  END IF;
 END;
+/
 
 DECLARE v_col_exist NUMBER;
 BEGIN
   SELECT count(*) INTO v_col_exist FROM all_tab_cols WHERE
     UPPER(column_name) = 'ANALYSIS_TYPE'
-    AND UPPER(table_name) = 'ANALYSIS_EXECUTION';
+    AND UPPER(table_name) = 'ANALYSIS_EXECUTION' AND owner = '${ohdsiSchema}';
   IF (v_col_exist > 0) THEN
     EXECUTE IMMEDIATE 'ALTER TABLE ${ohdsiSchema}.analysis_execution DROP COLUMN analysis_type';
   END IF;
 END;
+/
 
 DECLARE v_col_exist NUMBER;
 BEGIN
   SELECT count(*) INTO v_col_exist FROM all_tab_cols WHERE
     UPPER(column_name) = 'MEDIA_TYPE'
-    AND UPPER(table_name) = 'OUTPUT_FILES';
+    AND UPPER(table_name) = 'OUTPUT_FILES' AND owner = '${ohdsiSchema}';
   IF (v_col_exist = 0) THEN
     EXECUTE IMMEDIATE 'ALTER TABLE ${ohdsiSchema}.output_files ADD media_type VARCHAR2(255)';
   END IF;
 END;
+/
 
 ALTER TABLE ${ohdsiSchema}.analysis_execution MODIFY ANALYSIS_ID DEFAULT NULL NULL;
 
--- src/main/resources/db/migration/oracle/V2.7.0.20190212154939__analysis_execution_files.sql
+++ src/main/resources/db/migration/oracle/V2.7.0.20190212154939__analysis_execution_files.sql
@@ -1,11 +1,84 @@
-ALTER TABLE ${ohdsiSchema}.output_files DROP CONSTRAINT fk_sif_cca_execution;
-ALTER TABLE ${ohdsiSchema}.output_files DROP COLUMN cca_execution_id;
+DECLARE v_exists NUMBER;
+BEGIN
+  SELECT COUNT(*) INTO v_exists FROM all_constraints WHERE
+    UPPER(constraint_name) = 'FK_SIF_CCA_EXECUTION' AND UPPER(owner) = UPPER('${ohdsiSchema}');
+  IF (v_exists > 0) THEN
+    EXECUTE IMMEDIATE 'ALTER TABLE ${ohdsiSchema}.output_files DROP CONSTRAINT fk_sif_cca_execution';
+  END IF;
+END;
+/
 
-ALTER TABLE ${ohdsiSchema}.input_files DROP CONSTRAINT fk_sof_cca_execution;
-ALTER TABLE ${ohdsiSchema}.input_files DROP COLUMN cca_execution_id;
+DECLARE v_exists NUMBER;
+BEGIN
+  SELECT COUNT(*) INTO v_exists FROM ALL_TAB_COLS WHERE
+    UPPER(table_name) = 'OUTPUT_FILES' AND UPPER(column_name) = 'CCA_EXECUTION_ID'
+    AND UPPER(owner) = UPPER('${ohdsiSchema}');
+  IF (v_exists > 0) THEN
+    EXECUTE IMMEDIATE 'ALTER TABLE ${ohdsiSchema}.output_files DROP COLUMN cca_execution_id';
+  END IF;
+END;
+/
 
-ALTER TABLE ${ohdsiSchema}.output_files ADD execution_id NUMBER(10);
-ALTER TABLE ${ohdsiSchema}.input_files ADD execution_id NUMBER(10);
+DECLARE v_exists NUMBER;
+BEGIN
+  SELECT COUNT(*) INTO v_exists FROM all_constraints WHERE
+    UPPER(constraint_name) = 'FK_SOF_CCA_EXECUTION' AND UPPER(owner) = UPPER('${ohdsiSchema}');
+  IF (v_exists > 0) THEN
+    EXECUTE IMMEDIATE 'ALTER TABLE ${ohdsiSchema}.input_files DROP CONSTRAINT fk_sof_cca_execution';
+  END IF;
+END;
+/
 
-CREATE SEQUENCE ${ohdsiSchema}.output_file_seq;
-CREATE SEQUENCE ${ohdsiSchema}.input_file_seq;
+DECLARE v_exists NUMBER;
+BEGIN
+  SELECT COUNT(*) INTO v_exists FROM ALL_TAB_COLS WHERE
+    UPPER(table_name) = 'INPUT_FILES' AND UPPER(column_name) = 'CCA_EXECUTION_ID'
+    AND UPPER(owner) = UPPER('${ohdsiSchema}');
+  IF (v_exists > 0) THEN
+    EXECUTE IMMEDIATE 'ALTER TABLE ${ohdsiSchema}.input_files DROP COLUMN cca_execution_id';
+  END IF;
+END;
+/
+
+DECLARE v_exists NUMBER;
+BEGIN
+  SELECT COUNT(*) INTO v_exists FROM ALL_TAB_COLS WHERE
+    UPPER(table_name) = 'OUTPUT_FILES' AND UPPER(column_name) = 'EXECUTION_ID'
+    AND UPPER(owner) = UPPER('${ohdsiSchema}');
+  IF (v_exists = 0) THEN
+    EXECUTE IMMEDIATE 'ALTER TABLE ${ohdsiSchema}.output_files ADD execution_id NUMBER(10)';
+  END IF;
+END;
+/
+
+DECLARE v_exists NUMBER;
+BEGIN
+  SELECT COUNT(*) INTO v_exists FROM ALL_TAB_COLS WHERE
+    UPPER(table_name) = 'INPUT_FILES' AND UPPER(column_name) = 'EXECUTION_ID'
+    AND UPPER(owner) = UPPER('${ohdsiSchema}');
+  IF (v_exists = 0) THEN
+    EXECUTE IMMEDIATE 'ALTER TABLE ${ohdsiSchema}.input_files ADD execution_id NUMBER(10)';
+  END IF;
+END;
+/
+
+DECLARE v_exists NUMBER;
+BEGIN
+  SELECT COUNT(*) INTO v_exists FROM all_sequences WHERE
+    UPPER(sequence_name) = 'OUTPUT_FILE_SEQ' AND UPPER(sequence_owner) = UPPER('${ohdsiSchema}');
+  IF (v_exists = 0) THEN
+    EXECUTE IMMEDIATE 'CREATE SEQUENCE ${ohdsiSchema}.output_file_seq';
+  END IF;
+END;
+/
+
+
+DECLARE v_exists NUMBER;
+BEGIN
+  SELECT COUNT(*) INTO v_exists FROM all_sequences WHERE
+    UPPER(sequence_name) = 'INPUT_FILE_SEQ' AND UPPER(sequence_owner) = UPPER('${ohdsiSchema}');
+  IF (v_exists = 0) THEN
+    EXECUTE IMMEDIATE 'CREATE SEQUENCE ${ohdsiSchema}.input_file_seq';
+  END IF;
+END;
+/

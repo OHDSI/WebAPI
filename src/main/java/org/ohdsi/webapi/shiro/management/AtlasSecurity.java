72147ce5c29c6d2f86efab28d14b03d92971392a
Pavel Grafkin <pavgra@gmail.com>
2019-02-22 13:57:59 -0500

    Fixes permissions for Importing source codes (#858)
    * Fixes permissions for Importing source codes
    
    * Fixes AtlasSecurity
    
    * Fixes casts in migrations

--- src/main/java/org/ohdsi/webapi/shiro/management/AtlasSecurity.java
+++ src/main/java/org/ohdsi/webapi/shiro/management/AtlasSecurity.java
@@ -103,6 +103,7 @@ public abstract class AtlasSecurity extends Security {
     this.sourcePermissionTemplates.put("ir:*:execute:%s:get", "Generate IR on Source with SourceKey = %s");
     this.sourcePermissionTemplates.put("ir:*:execute:%s:delete", "Cancel IR generation on Source with SourceKey = %s");
     this.sourcePermissionTemplates.put("%s:person:*:get", "Get person's profile on Source with SourceKey = %s");
+    this.sourcePermissionTemplates.put("vocabulary:%s:lookup:sourcecodes:post", "Lookup source codes in Source with SourceKey = %s");
 
     this.incidenceRatePermissionTemplates.put("ir:%s:get", "Read Incidence Rate with ID=%s");
     this.incidenceRatePermissionTemplates.put("ir:%s:execution:*:get", "Execute Incidence Rate job with ID=%s");

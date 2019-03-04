bfca364db0012f9c2e8a699be6bc0d452a5fc825
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-03-04 17:56:18 +0300

    Characterization / Pathways access controls must be made consistent to other analysis

--- src/main/resources/db/migration/sqlserver/V2.7.0.20190304160255__fix-permissions-pathways.sql
+++ src/main/resources/db/migration/sqlserver/V2.7.0.20190304160255__fix-permissions-pathways.sql
@@ -1,11 +1,11 @@
 delete from ${ohdsiSchema}.sec_role_permission where
   permission_id in (select id from ${ohdsiSchema}.sec_permission where
-    value like 'cohort-characterization:%:generation:*:post' or
-      value like 'cohort-characterization:%:generation:*:delete'
+    value like 'pathway-analysis:%:generation:*:post' or
+    value like 'pathway-analysis:%:generation:*:delete'
   );
 delete from ${ohdsiSchema}.sec_permission where
-    value like 'cohort-characterization:%:generation:*:post' or
-    value like 'cohort-characterization:%:generation:*:delete';
+  value like 'pathway-analysis:%:generation:*:post' or
+  value like 'pathway-analysis:%:generation:*:delete';
 
 alter table ${ohdsiSchema}.sec_permission add for_role_id int;
 GO
@@ -15,14 +15,14 @@ INSERT INTO ${ohdsiSchema}.sec_permission (id, value, for_role_id)
 FROM ${ohdsiSchema}.sec_permission sp
 JOIN ${ohdsiSchema}.sec_role_permission srp on sp.id = srp.permission_id
 CROSS JOIN (
-  SELECT 'cohort-characterization:*:generation:%s:post' val UNION ALL
-  SELECT 'cohort-characterization:*:generation:%s:delete' val
+SELECT 'pathway-analysis:*:generation:%s:post' val UNION ALL
+SELECT 'pathway-analysis:*:generation:%s:delete' val
 ) new_perms
 WHERE sp.value LIKE 'source:%:access';
 
 INSERT INTO ${ohdsiSchema}.sec_role_permission (id, role_id, permission_id)
   SELECT NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, sp.for_role_id, sp.id
-  FROM ${ohdsiSchema}.sec_permission sp
-  WHERE sp.for_role_id IS NOT NULL;
+FROM ${ohdsiSchema}.sec_permission sp
+WHERE sp.for_role_id IS NOT NULL;
 
 alter table ${ohdsiSchema}.sec_permission drop column for_role_id;

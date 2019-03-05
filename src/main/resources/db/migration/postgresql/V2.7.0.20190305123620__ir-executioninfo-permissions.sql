6370eeebb2b984e3e3ad637f99cb99c99d3e5f23
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-03-05 15:51:11 +0300

    Fixes IR allows running even if previous execution is not completed yet

--- src/main/resources/db/migration/postgresql/V2.7.0.20190305123620__ir-executioninfo-permissions.sql
+++ src/main/resources/db/migration/postgresql/V2.7.0.20190305123620__ir-executioninfo-permissions.sql
@@ -1,8 +1,7 @@
 ALTER TABLE ${ohdsiSchema}.sec_permission ADD for_role_id INTEGER;
-GO
 
 INSERT INTO ${ohdsiSchema}.sec_permission (id, value, for_role_id)
-  SELECT NEXT VALUE FOR ${ohdsiSchema}.sec_permission_id_seq, REPLACE(CAST(new_perms.val AS VARCHAR(255)), '%s', REPLACE(REPLACE(value, 'source:', ''), ':access', '')), role_id
+  SELECT nextval('${ohdsiSchema}.sec_permission_id_seq'), REPLACE(CAST(new_perms.val AS VARCHAR), '%s', REPLACE(REPLACE(value, 'source:', ''), ':access', '')), role_id
 FROM ${ohdsiSchema}.sec_permission sp
 JOIN ${ohdsiSchema}.sec_role_permission srp on sp.id = srp.permission_id
 CROSS JOIN (
@@ -11,7 +10,7 @@ CROSS JOIN (
 WHERE sp.value LIKE 'source:%:access';
 
 INSERT INTO ${ohdsiSchema}.sec_role_permission (id, role_id, permission_id)
-  SELECT NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, sp.for_role_id, sp.id
+  SELECT nextval('${ohdsiSchema}.sec_role_permission_sequence'), sp.for_role_id, sp.id
 FROM ${ohdsiSchema}.sec_permission sp
 WHERE sp.for_role_id IS NOT NULL;
 

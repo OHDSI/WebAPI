43bbcdf21901c51634cd373243d44e9ddf14c13b
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-03-05 19:24:31 +0300

    Separated methods for IR results loading to provide smoother UX (#909)

--- src/main/java/org/ohdsi/webapi/ircalc/IRExecutionInfoRepository.java
+++ src/main/java/org/ohdsi/webapi/ircalc/IRExecutionInfoRepository.java
@@ -1,9 +1,11 @@
 package org.ohdsi.webapi.ircalc;
 
 import org.ohdsi.webapi.GenerationStatus;
+import org.ohdsi.webapi.source.Source;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IRExecutionInfoRepository extends CrudRepository<ExecutionInfo, ExecutionInfoId> {
     List<ExecutionInfo> findByStatus(GenerationStatus status);

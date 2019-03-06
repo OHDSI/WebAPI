eb4f204b8886f4ef954d0ac28cdc0d697ff3c414
Anastasiia Klochkova <anastasiia.klochkova@odysseusinc.com>
2019-03-06 15:51:07 +0300

    added cast for stratum

--- src/main/resources/resources/cdmresults/sql/report/drugera/drilldown/prevalenceByGenderAgeYear.sql
+++ src/main/resources/resources/cdmresults/sql/report/drugera/drilldown/prevalenceByGenderAgeYear.sql
@@ -1,11 +1,12 @@
 SELECT
   c1.concept_id                                                          AS concept_id,
-  CONCAT(cast(CAST(num_stratum_4 AS INT) * 10 AS VARCHAR(11)), '-', cast((CAST(num_stratum_4 AS INT) + 1) * 10 - 1 AS
+  CONCAT(cast(CAST(CASE WHEN isNumeric(num_stratum_4) = 1 THEN num_stratum_4 ELSE null END AS INT) * 10 AS VARCHAR(11)), 
+        '-', cast((CAST(CASE WHEN isNumeric(num_stratum_4) = 1 THEN num_stratum_4 ELSE null END AS INT) + 1) * 10 - 1 AS
                                                                 VARCHAR(11))) AS trellis_name,
   --age decile
   c2.concept_name                                                        AS series_name,
   --gender
-  CAST(num_stratum_2 AS INT)                                             AS x_calendar_year,
+  CAST(CASE WHEN isNumeric(num_stratum_2) = 1 THEN num_stratum_2 ELSE null END AS INT)                                             AS x_calendar_year,
   -- calendar year, note, there could be blanks
   ROUND(1000 * (1.0 * num_count_value / denom_count_value),
         5)                                                               AS y_prevalence_1000_pp --prevalence, per 1000 persons

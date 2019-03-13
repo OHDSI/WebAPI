16d265676b44a51c6d64706515c1603d39dbeaf7
N.K <clax1412@aumc.ac.kr>
2019-03-13 17:17:23 +0900

    Add permission for heracles baseline...

--- src/main/java/org/ohdsi/webapi/shiro/management/AtlasSecurity.java
+++ src/main/java/org/ohdsi/webapi/shiro/management/AtlasSecurity.java
@@ -158,6 +158,8 @@ public abstract class AtlasSecurity extends Security {
     this.sourcePermissionTemplates.put("cdmresults:%s:*:*:get", "Get Achilles reports details on Source with SourceKey = %s");
     this.sourcePermissionTemplates.put("cohortresults:%s:*:*:get", "Get cohort results on Source with SourceKey = %s");
     this.sourcePermissionTemplates.put("cohortresults:%s:*:*:*:get", "Get cohort results details on Source with SourceKey = %s");
+    this.sourcePermissionTemplates.put("cohortresults:%s:*:healthcareutilization:*:*:get", "Get cohort results baseline on period for Source with SourceKey = %s");
+    this.sourcePermissionTemplates.put("cohortresults:%s:*:healthcareutilization:*:*:*:get", "Get cohort results baseline on occurrence for Source with SourceKey = %s");
     this.sourcePermissionTemplates.put("ir:*:execute:%s:get", "Generate IR on Source with SourceKey = %s");
     this.sourcePermissionTemplates.put("ir:*:execute:%s:delete", "Cancel IR generation on Source with SourceKey = %s");
     this.sourcePermissionTemplates.put("ir:*:info:%s:get", "Get IR execution info on Source with SourceKey = %s");

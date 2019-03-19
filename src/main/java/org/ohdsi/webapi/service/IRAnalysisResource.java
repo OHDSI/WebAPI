780a0732da03062e75993d4ad2883035d50a7b0f
ssuvorov-fls <sergey.suvorov@firstlinesoftware.com>
2019-03-19 23:25:55 +0300

    Issue 655 export ir analysis sql (#947)
    * Created sql export services(#655)

--- src/main/java/org/ohdsi/webapi/service/IRAnalysisResource.java
+++ src/main/java/org/ohdsi/webapi/service/IRAnalysisResource.java
@@ -1,18 +1,11 @@
 package org.ohdsi.webapi.service;
 
+import org.ohdsi.webapi.common.generation.GenerateSqlResult;
 import org.ohdsi.webapi.ircalc.AnalysisReport;
 import org.ohdsi.webapi.job.JobExecutionResource;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -130,4 +123,10 @@ public interface IRAnalysisResource {
     @Transactional
     AnalysisReport getAnalysisReport(@PathParam("id") final int id, @PathParam("sourceKey") final String sourceKey, 
                                      @QueryParam("targetId") final int targetId, @QueryParam("outcomeId") final int outcomeId );
+
+    @Path("/sql")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public GenerateSqlResult generateSql(IRAnalysisService.GenerateSqlRequest request);
 }

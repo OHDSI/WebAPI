1df3110a3022fa12b63d8fa8f37374ccbb5b72b6
anastasiia klochkova <38942654+aklochkova@users.noreply.github.com>
2019-03-29 17:35:16 +0300

    Added permissions to watch IR report (#991)
    * added permissions to watch IR report
    
    * added migrations to cover newly created sources
    
    * simplify migration queries

--- src/main/java/org/ohdsi/webapi/shiro/management/AtlasSecurity.java
+++ src/main/java/org/ohdsi/webapi/shiro/management/AtlasSecurity.java
@@ -163,6 +163,7 @@ public abstract class AtlasSecurity extends Security {
     this.sourcePermissionTemplates.put("ir:*:execute:%s:get", "Generate IR on Source with SourceKey = %s");
     this.sourcePermissionTemplates.put("ir:*:execute:%s:delete", "Cancel IR generation on Source with SourceKey = %s");
     this.sourcePermissionTemplates.put("ir:*:info:%s:get", "Get IR execution info on Source with SourceKey = %s");
+    this.sourcePermissionTemplates.put("ir:*:report:%s:get","Get IR generation report with SourceKey = %s");
     this.sourcePermissionTemplates.put("%s:person:*:get", "Get person's profile on Source with SourceKey = %s");
     this.sourcePermissionTemplates.put("vocabulary:%s:lookup:sourcecodes:post", "Lookup source codes in Source with SourceKey = %s");
     this.sourcePermissionTemplates.put("cohort-characterization:*:generation:%s:post", "Generate Cohort Characterization on Source with SourceKey = %s");

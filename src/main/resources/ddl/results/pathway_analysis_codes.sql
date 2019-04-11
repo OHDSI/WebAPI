4c836d2dc0824a197d1d6d4162ef9835f29f6097
Chris Knoll <cknoll@ohdsi.org>
2019-04-11 17:38:57 -0400

    Added migration scripts for pathway_analysis_codes and pathway_analysis_stats.

--- src/main/resources/ddl/results/pathway_analysis_codes.sql
+++ src/main/resources/ddl/results/pathway_analysis_codes.sql
@@ -2,7 +2,7 @@ IF OBJECT_ID('@results_schema.pathway_analysis_codes', 'U') IS NULL
 CREATE TABLE @results_schema.pathway_analysis_codes
 (
 	pathway_analysis_generation_id BIGINT NOT NULL,
-	code INTEGER NOT NULL,
+	code BIGINT NOT NULL,
 	name VARCHAR(2000) NOT NULL,
 	is_combo int NOT NULL
 );

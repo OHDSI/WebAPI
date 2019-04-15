0b98a05c2e7698b8a5434458168392eb22f25a78
Anastasiia Klochkova <anastasiia.klochkova@odysseusinc.com>
2019-04-15 14:59:05 +0300

    fixed migrations

--- src/main/resources/db/migration/sqlserver/V2.8.0.20190414180601__add-entity-exists-permission.sql
+++ src/main/resources/db/migration/sqlserver/V2.8.0.20190414180601__add-entity-exists-permission.sql
@@ -1,26 +1,26 @@
 INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description)
 VALUES
-  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'conceptset:*:exists', 'Check name uniqueness of concept set'), 
-  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'cohortdefinition:*:exists', 'Check name uniqueness of cohort definition'),
-  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'pathway-analysis:*:exists', 'Check name uniqueness of pathway analysis'),
-  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'cohort-characterization:*:exists', 'Check name uniqueness of cohort characterization'),
-  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'feature-analysis:*:exists', 'Check name uniqueness of feature analysis'),
-  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'ir:*:exists', 'Check name uniqueness of incidence rate'),
-  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'prediction:*:exists', 'Check name uniqueness of prediction'),
-  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'estimation:*:exists', 'Check name uniqueness of estimation');
+  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'conceptset:*:exists:get', 'Check name uniqueness of concept set'), 
+  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'cohortdefinition:*:exists:get', 'Check name uniqueness of cohort definition'),
+  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'pathway-analysis:*:exists:get', 'Check name uniqueness of pathway analysis'),
+  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'cohort-characterization:*:exists:get', 'Check name uniqueness of cohort characterization'),
+  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'feature-analysis:*:exists:get', 'Check name uniqueness of feature analysis'),
+  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'ir:*:exists:get', 'Check name uniqueness of incidence rate'),
+  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'prediction:*:exists:get', 'Check name uniqueness of prediction'),
+  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'estimation:*:exists:get', 'Check name uniqueness of estimation');
 
 
 INSERT INTO ${ohdsiSchema}.sec_role_permission(id, role_id, permission_id)
 SELECT NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, sr.id, sp.id
 FROM ${ohdsiSchema}.sec_permission SP, ${ohdsiSchema}.sec_role sr
-WHERE sp.value = 'conceptset:*:exists'
+WHERE sp.value = 'conceptset:*:exists:get'
 AND sr.name IN ('concept set creator', 'Moderator');
 
 
 INSERT INTO ${ohdsiSchema}.sec_role_permission(id, role_id, permission_id)
 SELECT NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, sr.id, sp.id
 FROM ${ohdsiSchema}.sec_permission SP, ${ohdsiSchema}.sec_role sr
-WHERE sp.value = 'cohortdefinition:*:exists'
+WHERE sp.value = 'cohortdefinition:*:exists:get'
 AND sr.name IN ('cohort creator', 'Moderator');
 
 
@@ -29,11 +29,11 @@ INSERT INTO ${ohdsiSchema}.sec_role_permission(id, role_id, permission_id)
 SELECT NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, sr.id, sp.id
 FROM ${ohdsiSchema}.sec_permission SP, ${ohdsiSchema}.sec_role sr
 WHERE sp.value IN (
-  'pathway-analysis:*:exists',
-  'cohort-characterization:*:exists',
-  'feature-analysis:*:exists',
-  'ir:*:exists',
-  'prediction:*:exists',
-  'estimation:*:exists'
+  'pathway-analysis:*:exists:get',
+  'cohort-characterization:*:exists:get',
+  'feature-analysis:*:exists:get',
+  'ir:*:exists:get',
+  'prediction:*:exists:get',
+  'estimation:*:exists:get'
 )
 AND sr.name IN ('Atlas users', 'Moderator');

b49bc3d2fda62fe3af3f52257f70d113693e816f
Anastasiia Klochkova <anastasiia.klochkova@odysseusinc.com>
2019-04-15 13:40:17 +0300

    refactored converters

--- src/main/java/org/ohdsi/webapi/estimation/converter/EstimationToEstimationShortDTOConverter.java
+++ src/main/java/org/ohdsi/webapi/estimation/converter/EstimationToEstimationShortDTOConverter.java
@@ -3,24 +3,27 @@ package org.ohdsi.webapi.estimation.converter;
 import org.ohdsi.webapi.converter.BaseConversionServiceAwareConverter;
 import org.ohdsi.webapi.estimation.Estimation;
 import org.ohdsi.webapi.estimation.dto.EstimationShortDTO;
-import org.ohdsi.webapi.user.dto.UserDTO;
+import org.ohdsi.webapi.util.ConversionUtils;
 import org.springframework.stereotype.Component;
 
 @Component
-public class EstimationToEstimationShortDTOConverter extends BaseConversionServiceAwareConverter<Estimation, EstimationShortDTO> {
+public class EstimationToEstimationShortDTOConverter <T extends EstimationShortDTO> extends BaseConversionServiceAwareConverter<Estimation, T> {
 
     @Override
-    public EstimationShortDTO convert(Estimation source) {
+    protected T createResultObject() {
 
-        EstimationShortDTO result = new EstimationShortDTO();
+        return (T) new EstimationShortDTO();
+    }
+    
+    @Override
+    public T convert(Estimation source) {
+
+        T result = createResultObject(source);
+        ConversionUtils.convertMetadata(conversionService, source, result);
         result.setId(source.getId());
         result.setName(source.getName());
         result.setDescription(source.getDescription());
         result.setType(source.getType());
-        result.setCreatedBy(conversionService.convert(source.getCreatedBy(), UserDTO.class));
-        result.setCreatedDate(source.getCreatedDate());
-        result.setModifiedBy(conversionService.convert(source.getModifiedBy(), UserDTO.class));
-        result.setModifiedDate(source.getModifiedDate());
         return result;
     }
 }
--- src/main/java/org/ohdsi/webapi/prediction/converter/PredictionAnalysisToCommonAnalysisDTOConverter.java
+++ src/main/java/org/ohdsi/webapi/prediction/converter/PredictionAnalysisToCommonAnalysisDTOConverter.java
@@ -3,23 +3,26 @@ package org.ohdsi.webapi.prediction.converter;
 import org.ohdsi.webapi.common.analyses.CommonAnalysisDTO;
 import org.ohdsi.webapi.converter.BaseConversionServiceAwareConverter;
 import org.ohdsi.webapi.prediction.PredictionAnalysis;
-import org.ohdsi.webapi.user.dto.UserDTO;
+import org.ohdsi.webapi.util.ConversionUtils;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PredictionAnalysisToCommonAnalysisDTOConverter extends BaseConversionServiceAwareConverter<PredictionAnalysis, CommonAnalysisDTO> {
+public class PredictionAnalysisToCommonAnalysisDTOConverter <T extends CommonAnalysisDTO> extends BaseConversionServiceAwareConverter<PredictionAnalysis, T> {
 
     @Override
-    public CommonAnalysisDTO convert(PredictionAnalysis source) {
+    protected T createResultObject() {
 
-        CommonAnalysisDTO result = new CommonAnalysisDTO();
+        return (T) new CommonAnalysisDTO();
+    }
+
+    @Override
+    public T convert(PredictionAnalysis source) {
+
+        T result = createResultObject(source);
+        ConversionUtils.convertMetadata(conversionService, source, result);
         result.setId(source.getId());
         result.setName(source.getName());
         result.setDescription(source.getDescription());
-        result.setCreatedBy(conversionService.convert(source.getCreatedBy(), UserDTO.class));
-        result.setCreatedDate(source.getCreatedDate());
-        result.setModifiedBy(conversionService.convert(source.getModifiedBy(), UserDTO.class));
-        result.setModifiedDate(source.getModifiedDate());
         return result;
     }
 }

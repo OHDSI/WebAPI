a77bbb0506e00f37747cc76626be71ddccfda109
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-04-17 15:43:55 +0300

    Throws an error when Estimation import failed

--- src/main/java/org/ohdsi/webapi/estimation/EstimationServiceImpl.java
+++ src/main/java/org/ohdsi/webapi/estimation/EstimationServiceImpl.java
@@ -48,6 +48,7 @@ import org.springframework.stereotype.Service;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import javax.ws.rs.InternalServerErrorException;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -284,6 +285,10 @@ public class EstimationServiceImpl extends AnalysisExecutionSupport implements E
     @Override
     public Estimation importAnalysis(EstimationAnalysisImpl analysis) throws Exception {
         try {
+            if (Objects.isNull(analysis.getEstimationAnalysisSettings())) {
+                log.error("Failed to import Estimation. Invalid source JSON. EstimationAnalysisSettings is empty");
+                throw new InternalServerErrorException();
+            }
             if (analysis.getEstimationAnalysisSettings().getEstimationType() != EstimationTypeEnum.COMPARATIVE_COHORT_ANALYSIS) {
                 String estimationType = analysis.getEstimationAnalysisSettings().getEstimationType().name();
                 throw new UnsupportedOperationException("Cannot import " + estimationType);

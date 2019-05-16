cf4053e2b18143863b63150f401de04887008ad4
Pavel Grafkin <pavgra@gmail.com>
2019-05-16 09:39:01 -0400

    Improvements for PLE & PLP starter scripts (#1093)
    - Install packages into local lib
    - Install packages using `R CMD build` to avoid issues on Windows and forced packages `upgrade`
    - Do not build vignettes

--- src/main/resources/resources/prediction/r/runAnalysis.R
+++ src/main/resources/resources/prediction/r/runAnalysis.R
@@ -1,13 +1,20 @@
-library(devtools)
-options(devtools.install.args = "--no-multiarch")
-
 setwd("./")
+libs_local <- file.path(getwd(), "libs-local")
 tryCatch({
-    unzip('@packageFile', exdir = file.path(".", "@analysisDir"))
-    install_local(file.path(".", "@analysisDir"))
+  unzip('@packageFile', exdir = file.path(".", "@analysisDir"))
+  dir.create(libs_local)
+  callr::rcmd("build", c("@analysisDir", c("--no-build-vignettes")), echo = TRUE, show = TRUE)
+  pkg_file <- list.files(path = ".", pattern = "\\.tar\\.gz")[1]
+  tryCatch({
+    install.packages(pkg_file, lib = libs_local, repos = NULL, type="source", INSTALL_opts=c("--no-multiarch"))
+  }, finally = {
+    file.remove(pkg_file)
+  })
 }, finally = {
-    unlink('@analysisDir', recursive = TRUE, force = TRUE)
+  unlink('{{analysisDir}}', recursive = TRUE, force = TRUE)
 })
+
+.libPaths(c(.libPaths(), libs_local))
 
 library(DatabaseConnector)
 library(@packageName)
@@ -44,5 +51,6 @@ tryCatch({
                 minCellCount = 5,
                 cdmVersion = 5)
 }, finally = {
-        remove.packages('@packageName')
+        remove.packages('@packageName', lib = libs_local)
+        unlink(libs_local, recursive = TRUE, force = TRUE)
 })

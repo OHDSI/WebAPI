d416229fe4ce8345e685016c0675daaae0279c81
ssuvorov-fls <sergey.suvorov@firstlinesoftware.com>
2019-05-17 19:14:36 +0300

    Pull user's first and last names from auth providers (#1002)

--- src/main/java/org/ohdsi/webapi/shiro/management/AtlasRegularSecurity.java
+++ src/main/java/org/ohdsi/webapi/shiro/management/AtlasRegularSecurity.java
@@ -1,19 +1,8 @@
 package org.ohdsi.webapi.shiro.management;
 
-import static org.ohdsi.webapi.shiro.management.FilterTemplates.*;
-import static org.ohdsi.webapi.util.QuoteUtils.dequote;
-
 import io.buji.pac4j.filter.CallbackFilter;
 import io.buji.pac4j.filter.SecurityFilter;
 import io.buji.pac4j.realm.Pac4jRealm;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.Map;
-import java.util.Set;
-import javax.naming.Context;
-import javax.servlet.Filter;
-import javax.sql.DataSource;
 import org.apache.commons.lang.StringUtils;
 import org.apache.shiro.realm.Realm;
 import org.apache.shiro.realm.activedirectory.ActiveDirectoryRealm;
@@ -23,6 +12,8 @@ import org.jasig.cas.client.validation.Cas20ServiceTicketValidator;
 import org.ohdsi.webapi.Constants;
 import org.ohdsi.webapi.shiro.Entities.UserRepository;
 import org.ohdsi.webapi.shiro.filters.*;
+import org.ohdsi.webapi.shiro.mapper.ADUserMapper;
+import org.ohdsi.webapi.shiro.mapper.LdapUserMapper;
 import org.ohdsi.webapi.shiro.realms.ADRealm;
 import org.ohdsi.webapi.shiro.realms.JdbcAuthRealm;
 import org.ohdsi.webapi.shiro.realms.JwtAuthRealm;
@@ -49,6 +40,18 @@ import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.stereotype.Component;
 import waffle.shiro.negotiate.NegotiateAuthenticationFilter;
 import waffle.shiro.negotiate.NegotiateAuthenticationRealm;
+
+import javax.naming.Context;
+import javax.servlet.Filter;
+import javax.sql.DataSource;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.Set;
+
+import static org.ohdsi.webapi.shiro.management.FilterTemplates.*;
+import static org.ohdsi.webapi.util.QuoteUtils.dequote;
 
 @Component
 @ConditionalOnProperty(name = "security.provider", havingValue = Constants.SecurityProviders.REGULAR)
@@ -90,6 +93,12 @@ public class AtlasRegularSecurity extends AtlasSecurity {
     @Value("${security.ldap.url}")
     private String ldapUrl;
 
+    @Value("${security.ldap.searchString}")
+    private String ldapSearchString;
+
+    @Value("${security.ldap.searchBase}")
+    private String ldapSearchBase;
+
     @Value("${security.ad.url}")
     private String adUrl;
 
@@ -111,6 +120,9 @@ public class AtlasRegularSecurity extends AtlasSecurity {
     @Value("${security.ad.searchFilter}")
     private String adSearchFilter;
 
+    @Value("${security.ad.searchString}")
+    private String adSearchString;
+
     @Value("${security.ad.ignore.partial.result.exception}")
     private Boolean adIgnorePartialResultException;
 
@@ -127,6 +139,12 @@ public class AtlasRegularSecurity extends AtlasSecurity {
 
     @Autowired
     private ApplicationEventPublisher eventPublisher;
+
+    @Autowired
+    private ADUserMapper adUserMapper;
+
+    @Autowired
+    private LdapUserMapper ldapUserMapper;
 
     @Value("${security.oid.redirectUrl}")
     private String redirectUrl;
@@ -274,7 +292,7 @@ public class AtlasRegularSecurity extends AtlasSecurity {
     }
 
     private JndiLdapRealm ldapRealm() {
-        JndiLdapRealm realm = new LdapRealm();
+        JndiLdapRealm realm = new LdapRealm(ldapSearchString, ldapSearchBase, ldapUserMapper);
         realm.setUserDnTemplate(dequote(userDnTemplate));
         JndiLdapContextFactory contextFactory = new JndiLdapContextFactory();
         contextFactory.setUrl(dequote(ldapUrl));
@@ -285,7 +303,7 @@ public class AtlasRegularSecurity extends AtlasSecurity {
     }
 
     private ActiveDirectoryRealm activeDirectoryRealm() {
-        ActiveDirectoryRealm realm = new ADRealm(getLdapTemplate(), adSearchFilter);
+        ActiveDirectoryRealm realm = new ADRealm(getLdapTemplate(), adSearchFilter, adSearchString, adUserMapper);
         realm.setUrl(dequote(adUrl));
         realm.setSearchBase(dequote(adSearchBase));
         realm.setPrincipalSuffix(dequote(adPrincipalSuffix));

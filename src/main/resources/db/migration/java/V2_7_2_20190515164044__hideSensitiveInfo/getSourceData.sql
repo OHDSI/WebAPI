6f8f8863a07f0b2ca8eafb3902c5251e6c5becfc
Sergey Suvorov <sergey.suvorov@odysseusinc.com>
2019-05-20 14:21:45 +0300

    added sql scripts to vcs(#1046)

--- src/main/resources/db/migration/java/V2_7_2_20190515164044__hideSensitiveInfo/getSourceData.sql
+++ src/main/resources/db/migration/java/V2_7_2_20190515164044__hideSensitiveInfo/getSourceData.sql
@@ -2,10 +2,10 @@ select
 	s.source_name,
 	out.execution_id
 from
-	ohdsi.estimation_analysis_generation gen
-inner join ohdsi.output_files out on
+	@webapi_schema.estimation_analysis_generation gen
+inner join @webapi_schema.output_files out on
 	gen.analysis_execution_id = out.execution_id
-inner join ohdsi.source s on
+inner join @webapi_schema.source s on
 	s.source_id = gen.source_id
 group by
 	s.source_name,
@@ -15,10 +15,10 @@ select
 	s.source_name,
 	out.execution_id
 from
-	ohdsi.prediction_analysis_generation gen
-inner join ohdsi.output_files out on
+	@webapi_schema.prediction_analysis_generation gen
+inner join @webapi_schema.output_files out on
 	gen.analysis_execution_id = out.execution_id
-inner join ohdsi.source s on
+inner join @webapi_schema.source s on
 	s.source_id = gen.source_id
 group by
 	s.source_name,

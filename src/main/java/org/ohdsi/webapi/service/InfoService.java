260018c9dc54510e8de68c453b0eadecbf1ae53c
alexandr cumarav <https://localhost:8080/study-manager/studies/6>
2019-05-31 13:29:51 +0200

    webapi-1129: build version controller refresh

--- src/main/java/org/ohdsi/webapi/service/InfoService.java
+++ src/main/java/org/ohdsi/webapi/service/InfoService.java
@@ -16,49 +16,61 @@
 package org.ohdsi.webapi.service;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.ArrayList;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import org.ohdsi.webapi.shiro.Entities.UserEntity;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Component;
 
 /**
- *
  * @author Chris Knoll <cknoll@ohdsi.org>
  */
 @Path("/info")
 @Component
 public class InfoService {
-  
-  @Autowired
-  private Environment env;
-    
-  public static class Info {
-    @JsonProperty("version")
-    public String version;
-  }
-  
-  @Bean
-  public Info info()
-  {
-    Info newInfo = new Info();
-    newInfo.version = this.env.getRequiredProperty("webapi.version");
-    return newInfo;
-  }
-  
-  @Autowired
-  private Info info;
-  
-  @GET
-  @Path("/")
-  @Produces(MediaType.APPLICATION_JSON)
-  public Info getInfo() {
-    return info;
-  }  
+    private final Info info;
+
+    @Inject
+    public InfoService(BuildProperties buildProperties, @Value("${build.number}") final String buildNumber) {
+        String artifactVersion = String.format("%s %s", buildProperties.getArtifact(), buildProperties.getVersion());
+
+        this.info = new Info(buildNumber, artifactVersion, buildProperties.getTime().toString());
+    }
+
+    public static class Info {
+        private final String build;
+        private final String timestamp;
+        @JsonProperty("version")
+        private final String version;
+
+        public Info(String build, String artifactVersion, String timestamp) {
+            this.build = build;
+            this.version = artifactVersion;
+            this.timestamp = timestamp;
+        }
+
+        public String getVersion() {
+            return version;
+        }
+
+        public String getBuild() {
+            return build;
+        }
+
+        public String getTimestamp() {
+            return timestamp;
+        }
+    }
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Info getInfo() {
+        return info;
+    }
 }

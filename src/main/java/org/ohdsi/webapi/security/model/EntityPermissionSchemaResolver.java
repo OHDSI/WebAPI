93f0299f6313707b9f9c97c86215af8eb920c2cb
Pavel Grafkin <pavgra@gmail.com>
2019-08-01 12:02:04 -0400

    Adds ability for regular users to grant and revoke write permissions for their owned assets (#1273)
    * Added services for listing, granting and revoking write permissions for an entity
    
    * Revert pom change
    
    * Fixed Source EntityType
    
    * Revert warmCaches
    
    * Renamed package
    
    * Added role suggest
    
    * Added access control to Cohort Characterizations
    
    * Added note
    
    * Migrations
    
    * fixes query group by fields

---
+++ src/main/java/org/ohdsi/webapi/security/model/EntityPermissionSchemaResolver.java
@@ -0,0 +1,23 @@
+package org.ohdsi.webapi.security.model;
+
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class EntityPermissionSchemaResolver {
+
+    private static Map<EntityType, EntityPermissionSchema> entityPermissionSchemas = new HashMap<>();
+
+    public EntityPermissionSchemaResolver(List<EntityPermissionSchema> entityPermissionSchemaList) {
+
+        entityPermissionSchemaList.forEach(s -> entityPermissionSchemas.put(s.getEntityType(), s));
+    }
+
+    public EntityPermissionSchema getForType(EntityType entityType) {
+
+        return entityPermissionSchemas.get(entityType);
+    }
+}

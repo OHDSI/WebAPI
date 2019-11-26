13856b3c86325c2b1c6af537a8b9d0743e477913
Anastasiia Klochkova <anastasiia.klochkova@odysseusinc.com>
2019-11-26 19:01:52 +0100

    added embedded postgresql

--- src/test/java/org/ohdsi/webapi/test/TestEntityIT.java
+++ src/test/java/org/ohdsi/webapi/test/TestEntityIT.java
@@ -1,7 +1,17 @@
 package org.ohdsi.webapi.test;
 
+import com.opentable.db.postgres.embedded.EmbeddedPostgres;
+import java.io.IOException;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+import org.apache.shiro.subject.SimplePrincipalCollection;
+import org.apache.shiro.subject.Subject;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import org.mockito.Mockito;
+import org.ohdsi.webapi.test.entity.AbstractShiroTest;
 import org.ohdsi.webapi.test.entity.cohortcharacterization.copy.TestCCCopy;
 import org.ohdsi.webapi.test.entity.cohortcharacterization.create.TestCCCreate;
 import org.ohdsi.webapi.test.entity.cohortcharacterization.importing.TestCCImport;
@@ -47,5 +57,45 @@ import org.ohdsi.webapi.test.entity.prediction.importing.TestPredictionImport;
         TestPredictionCreate.class,
         TestPredictionImport.class,
         TestPredictionCopy.class})
-public class TestEntityIT {
+public class TestEntityIT extends AbstractShiroTest {
+
+    private static EmbeddedPostgres pg;
+    @BeforeClass
+    public static void before() {
+        try {
+            if (pg == null){
+                pg = EmbeddedPostgres.start();
+            }
+            Subject subjectUnderTest = Mockito.mock(Subject.class);
+            SimplePrincipalCollection principalCollection = Mockito.mock(SimplePrincipalCollection.class);
+            Mockito.when(subjectUnderTest.isAuthenticated()).thenReturn(true);
+            Mockito.when(subjectUnderTest.getPrincipals()).thenReturn(principalCollection);
+            Mockito.when(principalCollection.getPrimaryPrincipal()).thenReturn("admin@odysseusinc.com");
+
+            //bind the subject to the current thread
+            setSubject(subjectUnderTest);
+
+            System.setProperty("datasource.url", getDatabaseMetadata().getURL());
+            System.setProperty("flyway.datasource.url", System.getProperty("datasource.url"));
+            System.setProperty("flyway.datasource.driverClassName=", getDatabaseMetadata().getDriverName());
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @AfterClass
+    public static void tearDownSubject() {
+        //unbind the subject from the current thread
+        clearSubject();
+        try {
+            pg.close();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static DatabaseMetaData getDatabaseMetadata() throws SQLException {
+
+        return pg.getPostgresDatabase().getConnection().getMetaData();
+    }
 }
--- src/test/java/org/ohdsi/webapi/test/entity/BaseTestEntity.java
+++ src/test/java/org/ohdsi/webapi/test/entity/BaseTestEntity.java
@@ -1,73 +1,20 @@
 package org.ohdsi.webapi.test.entity;
 
-import org.apache.shiro.subject.SimplePrincipalCollection;
-import org.apache.shiro.subject.Subject;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.ohdsi.webapi.WebApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.convert.ConversionService;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.TestPropertySourceUtils;
-import org.testcontainers.containers.JdbcDatabaseContainer;
-import org.testcontainers.containers.MSSQLServerContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
-
-import static org.ohdsi.webapi.test.entity.BaseTestEntity.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = WebApi.class)
-@ContextConfiguration(initializers = TestInitializer.class)
-@TestPropertySource(locations = "classpath:application-test.properties")
-public abstract class BaseTestEntity extends AbstractShiroTest {    
+@TestPropertySource(locations = "/in-memory-webapi.properties")
+public class BaseTestEntity {    
     @Autowired
     protected ConversionService conversionService;
     protected final static String COPY_PREFIX = "COPY OF: ";
     protected final static String NEW_TEST_ENTITY = "New test entity";
     protected final static String SOME_UNIQUE_TEST_NAME = "Some unique test name";
-
-    @BeforeClass
-    public static void before() {
-
-        Subject subjectUnderTest = Mockito.mock(Subject.class);
-        SimplePrincipalCollection principalCollection = Mockito.mock(SimplePrincipalCollection.class);
-        Mockito.when(subjectUnderTest.isAuthenticated()).thenReturn(true);
-        Mockito.when(subjectUnderTest.getPrincipals()).thenReturn(principalCollection);
-        Mockito.when(principalCollection.getPrimaryPrincipal()).thenReturn("admin@odysseusinc.com");
-
-        //bind the subject to the current thread
-        setSubject(subjectUnderTest);
-    }
-
-    @AfterClass
-    public static void tearDownSubject() {
-        //unbind the subject from the current thread
-        clearSubject();
-    }
-
-    public static class TestInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-
-        @Override
-        public void initialize(ConfigurableApplicationContext context) {
-            JdbcDatabaseContainer container = new PostgreSQLContainer()
-                    .withDatabaseName("ohdsi_webapi")
-                    .withUsername("ohdsi")
-                    .withPassword("ohdsi");
-
-//            container = new MSSQLServerContainer();
-            container.start();           
-            
-            TestPropertySourceUtils.addInlinedPropertiesToEnvironment(context,
-                    "datasource.url=" + container.getJdbcUrl(),
-                    "flyway.datasource.url=" + container.getJdbcUrl(),
-                    "flyway.datasource.driverClassName=" + container.getDriverClassName());
-        }
-    }
 }

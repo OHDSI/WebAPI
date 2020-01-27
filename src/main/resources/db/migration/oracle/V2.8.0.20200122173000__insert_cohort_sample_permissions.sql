e192d4595d384c849bb6be1ad4250edbc52a7cd4
Joris Borgdorff <github@blootsvoets.net>
2020-01-27 11:51:54 +0100

    Fix Oracle and PostgreSQL migration syntax

--- src/main/resources/db/migration/oracle/V2.8.0.20200122173000__insert_cohort_sample_permissions.sql
+++ src/main/resources/db/migration/oracle/V2.8.0.20200122173000__insert_cohort_sample_permissions.sql
@@ -1,9 +1,9 @@
 INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description) VALUES
-(NEXT VALUE FOR ${ohdsiSchema}.sec_permission_id_seq, 'cohortsample:*:*:get', 'List cohort samples'),
-(NEXT VALUE FOR ${ohdsiSchema}.sec_permission_id_seq, 'cohortsample:*:*:*:get', 'Get single cohort samples'),
-(NEXT VALUE FOR ${ohdsiSchema}.sec_permission_id_seq, 'cohortsample:*:*:*:delete', 'Delete cohort sample'),
-(NEXT VALUE FOR ${ohdsiSchema}.sec_permission_id_seq, 'cohortsample:*:*:delete', 'Delete all cohort samples of a cohort.'),
-(NEXT VALUE FOR ${ohdsiSchema}.sec_permission_id_seq, 'cohortsample:*:*:post', 'Create cohort sample');
+(${ohdsiSchema}.sec_permission_id_seq.nextval, 'cohortsample:*:*:get', 'List cohort samples'),
+(${ohdsiSchema}.sec_permission_id_seq.nextval, 'cohortsample:*:*:*:get', 'Get single cohort samples'),
+(${ohdsiSchema}.sec_permission_id_seq.nextval, 'cohortsample:*:*:*:delete', 'Delete cohort sample'),
+(${ohdsiSchema}.sec_permission_id_seq.nextval, 'cohortsample:*:*:delete', 'Delete all cohort samples of a cohort.'),
+(${ohdsiSchema}.sec_permission_id_seq.nextval, 'cohortsample:*:*:post', 'Create cohort sample');
 
 INSERT INTO ${ohdsiSchema}.sec_role_permission(id, role_id, permission_id)
 SELECT NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, sr.id, sp.id

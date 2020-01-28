26d47bc727e073ac6fb83fe4e919cc67c7d59462
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2020-01-28 15:20:54 +0300

    Oracle migration fixed

--- src/main/resources/db/migration/oracle/V2.8.0.20200127101702__restore_rest_endpoint_perms.sql
+++ src/main/resources/db/migration/oracle/V2.8.0.20200127101702__restore_rest_endpoint_perms.sql
@@ -1,9 +1,9 @@
 INSERT INTO ${ohdsiSchema}.sec_permission (id, value, description)
-SELECT ${ohdsiSchema}.sec_permission_id_seq.nextval, 'source:priorityVocabulary:get', 'Get source with highest priority vocabulary daimon'
+SELECT ${ohdsiSchema}.sec_permission_id_seq.nextval, 'source:priorityVocabulary:get', 'Get source with highest priority vocabulary daimon' FROM DUAL
 ;
 
 INSERT INTO ${ohdsiSchema}.sec_permission (id, value, description)
-SELECT ${ohdsiSchema}.sec_permission_id_seq.nextval, 'cohort-characterization:generation:*:result:get', 'Get cohort characterization generation results - 2.7.x compatible'
+SELECT ${ohdsiSchema}.sec_permission_id_seq.nextval, 'cohort-characterization:generation:*:result:get', 'Get cohort characterization generation results - 2.7.x compatible' FROM DUAL
 ;
 
 INSERT INTO ${ohdsiSchema}.sec_role_permission(id, role_id, permission_id)

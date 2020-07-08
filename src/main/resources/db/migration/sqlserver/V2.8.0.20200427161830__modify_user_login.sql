be18719cb181a876cab77038eaaa2ea6a69bd50b
Chris Knoll <cknoll@ohdsi.org>
2020-07-08 17:39:53 -0400

    Update V2.8.0.20200427161830__modify_user_login.sql

--- src/main/resources/db/migration/sqlserver/V2.8.0.20200427161830__modify_user_login.sql
+++ src/main/resources/db/migration/sqlserver/V2.8.0.20200427161830__modify_user_login.sql
@@ -1,5 +1,5 @@
 ALTER TABLE ${ohdsiSchema}.sec_user DROP CONSTRAINT sec_user_login_unique;
 
-ALTER TABLE ${ohdsiSchema}.sec_user ALTER COLUMN login VARCHAR(MAX);
+ALTER TABLE ${ohdsiSchema}.sec_user ALTER COLUMN login VARCHAR(1024);
 
 ALTER TABLE ${ohdsiSchema}.sec_user ADD CONSTRAINT sec_user_login_unique UNIQUE (login);

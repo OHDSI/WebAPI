1c68b20d8d2c1f6d628e631ea86d6a7782750b83
Chris Knoll <cknoll@ohdsi.org>
2020-07-27 14:00:14 -0400

    Added javadoc and format validation.

--- src/main/java/org/ohdsi/webapi/util/SqlUtils.java
+++ src/main/java/org/ohdsi/webapi/util/SqlUtils.java
@@ -18,11 +18,20 @@ package org.ohdsi.webapi.util;
 import org.apache.commons.lang3.StringUtils;
 
 /**
- *
  * @author cknoll1
  */
 public final class SqlUtils {
+
+	/**
+		* Creates a DATEFROMPARTS() expression based on an input in form YYYY-MM-DD.
+		* @param date a string in YYYY-MM-DD format
+		* @return a String containing the DATEFROMPARTS expression
+		* @throws IllegalArgumentException
+		* @author cknoll1
+	*/
   public static String dateStringToSql(String date) {
+		if (!date.matches("^\\d{4}-\\d{2}-\\d{2}"))
+			throw new IllegalArgumentException(String.format("%s is not in valid YYYY-MM-DD format.", date));
     String[] dateParts = StringUtils.split(date, '-');
     return String.format("DATEFROMPARTS(%s, %s, %s)", Integer.valueOf(dateParts[0]), Integer.valueOf(dateParts[1]), Integer.valueOf(dateParts[2]));
   }  

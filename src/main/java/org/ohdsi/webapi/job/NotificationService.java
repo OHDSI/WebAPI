2ba1d3d7f64c021f329a10e6ccb483d54c6bb024
Sergey Suvorov <sergey.suvorov@firstlinesoftware.com>
2020-08-21 14:29:37 +0300

    fix getting list of caching jobs  (#1610)
    * fix getting list of caching jobs (https://github.com/OHDSI/Atlas/issues/2274)
    
    * fix getting list of caching jobs (https://github.com/OHDSI/Atlas/issues/2274)
    
    Co-authored-by: Sergey Suvorov <sergey.suvorov@odysseusinc.com>

--- src/main/java/org/ohdsi/webapi/job/NotificationService.java
+++ src/main/java/org/ohdsi/webapi/job/NotificationService.java
@@ -1,14 +1,14 @@
 package org.ohdsi.webapi.job;
 
-import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.JobExecution;
 
 import java.util.Date;
 import java.util.List;
 
 public interface NotificationService {
     List<JobExecutionInfo> findLastJobs(List<BatchStatus> hideStatuses);
+
+    List<JobExecutionInfo> findRefreshCacheLastJobs();
 
     Date getLastViewedTime() throws Exception;
 

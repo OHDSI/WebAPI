392a0e76a45392f1a8925f076ede73db5f768e5b
Wonjun Hong <hong.wonjun@gmail.com>
2020-09-06 00:23:44 +0900

    Add Measurement data over time in the Data Density Plot

--- src/main/resources/resources/cdmresults/sql/report/datadensity/recordsperperson.sql
+++ src/main/resources/resources/cdmresults/sql/report/datadensity/recordsperperson.sql
@@ -10,53 +10,59 @@ FROM
       count_value
     FROM @results_database_schema.achilles_results WHERE analysis_id = 220
     UNION ALL
-SELECT
-  'Condition occurrence' AS table_name,
-  stratum_1,
-  count_value
-FROM @results_database_schema.achilles_results WHERE analysis_id = 420
-UNION ALL
-SELECT
-  'Death' AS table_name,
-  stratum_1,
-  count_value
-FROM @results_database_schema.achilles_results WHERE analysis_id = 502
-UNION ALL
-SELECT
-  'Procedure occurrence' AS table_name,
-  stratum_1,
-  count_value
-FROM @results_database_schema.achilles_results WHERE analysis_id = 620
-UNION ALL
-SELECT
-  'Drug exposure' AS table_name,
-  stratum_1,
-  count_value
-FROM @results_database_schema.achilles_results WHERE analysis_id = 720
-UNION ALL
-SELECT
-  'Observation' AS table_name,
-  stratum_1,
-  count_value
-FROM @results_database_schema.achilles_results WHERE analysis_id = 820
-UNION ALL
-SELECT
-  'Drug era' AS table_name,
-  stratum_1,
-  count_value
-FROM @results_database_schema.achilles_results WHERE analysis_id = 920
-UNION ALL
-SELECT
-  'Condition era' AS table_name,
-  stratum_1,
-  count_value
-FROM @results_database_schema.achilles_results WHERE analysis_id = 1020
-UNION ALL
-SELECT
-  'Observation period' AS table_name,
-  stratum_1,
-  count_value
-FROM @results_database_schema.achilles_results WHERE analysis_id = 111
+    SELECT
+      'Condition occurrence' AS table_name,
+      stratum_1,
+      count_value
+    FROM @results_database_schema.achilles_results WHERE analysis_id = 420
+    UNION ALL
+    SELECT
+      'Death' AS table_name,
+      stratum_1,
+      count_value
+    FROM @results_database_schema.achilles_results WHERE analysis_id = 502
+    UNION ALL
+    SELECT
+      'Procedure occurrence' AS table_name,
+      stratum_1,
+      count_value
+    FROM @results_database_schema.achilles_results WHERE analysis_id = 620
+    UNION ALL
+    SELECT
+      'Drug exposure' AS table_name,
+      stratum_1,
+      count_value
+    FROM @results_database_schema.achilles_results WHERE analysis_id = 720
+    UNION ALL
+    SELECT
+      'Observation' AS table_name,
+      stratum_1,
+      count_value
+    FROM @results_database_schema.achilles_results WHERE analysis_id = 820
+    UNION ALL
+    SELECT
+      'Drug era' AS table_name,
+      stratum_1,
+      count_value
+    FROM @results_database_schema.achilles_results WHERE analysis_id = 920
+    UNION ALL
+    SELECT
+      'Condition era' AS table_name,
+      stratum_1,
+      count_value
+    FROM @results_database_schema.achilles_results WHERE analysis_id = 1020
+    UNION ALL
+    SELECT
+      'Observation period' AS table_name,
+      stratum_1,
+      count_value
+    FROM @results_database_schema.achilles_results WHERE analysis_id = 111
+    UNION ALL
+    SELECT
+      'Measurement' AS table_name,
+      stratum_1,
+      count_value
+    FROM @results_database_schema.achilles_results WHERE analysis_id = 1820
 ) t1
 INNER JOIN
 ( SELECT * FROM @results_database_schema.achilles_results WHERE analysis_id = 117) denom

241308a29c0df1ddde979ea365b41d17bea0bc29
molodkov yaroslav <yaroslav.molodkov@odysseusinc.com>
2020-09-28 12:50:33 +0200

    issue-1117 check permission against only jwt-auth-realm + upgrade shi… (#1483)
    * issue-1117 check permission against only jwt-auth-realm + upgrade shiro libraries
    
    * issue-1117 set realms for security manager to avoid any issue with caching
    
    * fixing npe foe disabled security (#1117)
    
    Co-authored-by: Sergey Suvorov <sergey.suvorov@odysseusinc.com>

--- src/main/java/org/ohdsi/webapi/shiro/tokens/JwtAuthToken.java
+++ src/main/java/org/ohdsi/webapi/shiro/tokens/JwtAuthToken.java
@@ -10,7 +10,7 @@ public class JwtAuthToken implements AuthenticationToken {
   
   private String subject;
   
-  public JwtAuthToken(String subject) {    
+  public JwtAuthToken(String subject) {
     this.subject = subject;
   }
   

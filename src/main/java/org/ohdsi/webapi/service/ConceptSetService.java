20cee90086e4252714c040e302e88521a33cf623
Chris Knoll <cknoll@ohdsi.org>
2020-10-05 17:00:14 -0400

    Properly load concept set expressions with duplicate concept IDs. Fixes #1646.

--- src/main/java/org/ohdsi/webapi/service/ConceptSetService.java
+++ src/main/java/org/ohdsi/webapi/service/ConceptSetService.java
@@ -124,18 +124,21 @@ public class ConceptSetService extends AbstractDaoService {
     }
 
     private ConceptSetExpression getConceptSetExpression(int id, SourceInfo sourceInfo) {
-        HashMap<Long, ConceptSetItem> map = new HashMap<>();
-
-        // collect the concept set items so we can lookup their properties later
-        for (ConceptSetItem csi : getConceptSetItems(id)) {
-            map.put(csi.getConceptId(), csi);
-        }
+        HashMap<Long, Concept> map = new HashMap<>();
 
         // create our expression to return
         ConceptSetExpression expression = new ConceptSetExpression();
-//        expression.items = new ConceptSetExpression.ConceptSetItem[map.size()];
+        // expression.items = new ConceptSetExpression.ConceptSetItem[map.size()];
         ArrayList<ConceptSetExpression.ConceptSetItem> expressionItems = new ArrayList<>();
-        
+
+        List<ConceptSetItem> repositoryItems = new ArrayList<>();
+        getConceptSetItems(id).forEach(repositoryItems::add);
+
+        // collect the unique concept IDs so we can load the concept object later.
+        for (ConceptSetItem csi : repositoryItems) {
+            map.put(csi.getConceptId(), null);
+        }
+
         // lookup the concepts we need information for
         long[] identifiers = new long[map.size()];
         int identifierIndex = 0;
@@ -147,15 +150,17 @@ public class ConceptSetService extends AbstractDaoService {
         // assume we want to resolve using the priority vocabulary provider
         Source vocabSourceInfo = sourceService.getPriorityVocabularySource();
         Collection<Concept> concepts = vocabService.executeIdentifierLookup(vocabSourceInfo.getSourceKey(), identifiers);
+        for(Concept concept : concepts) {
+          map.put(concept.conceptId, concept); // associate the concept object to the conceptID in the map
+        }
 
         // put the concept information into the expression along with the concept set item information 
-        for (Concept concept : concepts) {
+        for (ConceptSetItem repositoryItem : repositoryItems) {
           ConceptSetExpression.ConceptSetItem currentItem  = new ConceptSetExpression.ConceptSetItem();
-          currentItem.concept = concept;
-          ConceptSetItem csi = map.get(concept.conceptId);
-          currentItem.includeDescendants = (csi.getIncludeDescendants() == 1);
-          currentItem.includeMapped = (csi.getIncludeMapped() == 1);
-          currentItem.isExcluded = (csi.getIsExcluded() == 1);
+          currentItem.concept = map.get(repositoryItem.getConceptId());
+          currentItem.includeDescendants = (repositoryItem.getIncludeDescendants() == 1);
+          currentItem.includeMapped = (repositoryItem.getIncludeMapped() == 1);
+          currentItem.isExcluded = (repositoryItem.getIsExcluded() == 1);
           expressionItems.add(currentItem); 
         }
         expression.items = expressionItems.toArray(new ConceptSetExpression.ConceptSetItem[0]);

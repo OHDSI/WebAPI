568a8d39e0e4a88ec31dea174a2fd2a0b1f00980
Sergey Suvorov <sergey.suvorov@firstlinesoftware.com>
2020-10-29 13:40:12 +0300

    endpoint for concept ancestor and descendants was added(#1170) (#1664)
    Co-authored-by: Sergey Suvorov <sergey.suvorov@odysseusinc.com>

---
+++ src/main/resources/db/migration/postgresql/V2.8.0.20201022120031__concept_ancestor_and_descendants.sql
@@ -0,0 +1,16 @@
+ALTER TABLE ${ohdsiSchema}.sec_permission ADD COLUMN for_role_id INTEGER;
+
+INSERT INTO ${ohdsiSchema}.sec_permission (id, value, for_role_id)
+SELECT nextval('${ohdsiSchema}.sec_permission_id_seq'),
+  REPLACE('vocabulary:%s:concept:*:ancestorAndDescendant:get', '%s', REPLACE(REPLACE(value, 'source:', ''), ':access', '')),
+  role_id
+FROM ${ohdsiSchema}.sec_permission sp
+  JOIN ${ohdsiSchema}.sec_role_permission srp on sp.id = srp.permission_id
+WHERE sp.value LIKE 'source:%:access';
+
+INSERT INTO ${ohdsiSchema}.sec_role_permission (id, role_id, permission_id)
+SELECT nextval('${ohdsiSchema}.sec_role_permission_sequence'), sp.for_role_id, sp.id
+FROM ${ohdsiSchema}.sec_permission sp
+WHERE sp.for_role_id IS NOT NULL;
+
+ALTER TABLE ${ohdsiSchema}.sec_permission DROP COLUMN for_role_id;

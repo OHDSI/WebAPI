adac703e252f6cb1ef35dda070d3362f715a6ee5
anton-abushkevich <anton.abushkevich@gmail.com>
2021-02-09 13:43:23 +0300

    [Geospatial] Map or geospatial tab should not be shown for "non-geo" data sources #2412 (#1772)

---
+++ src/main/resources/db/migration/postgresql/V2.8.1.20210203163300__gis_service_add_check_source_permission.sql
@@ -0,0 +1,8 @@
+INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description) VALUES
+  (nextval('${ohdsiSchema}.sec_permission_id_seq'), 'gis:source:check:*:get', 'Check availability of geodata in source');
+
+
+INSERT INTO ${ohdsiSchema}.sec_role_permission(id, role_id, permission_id)
+  SELECT nextval('${ohdsiSchema}.sec_role_permission_sequence'), sr.id, sp.id
+  FROM ${ohdsiSchema}.sec_permission SP, ${ohdsiSchema}.sec_role sr
+  WHERE sp.value = 'gis:source:check:*:get' AND sr.name IN ('Atlas users');

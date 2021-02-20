47f4c838cbb84b47a8f4bfcef150cf05d98449d4
Sergey Suvorov <sergey.suvorov@odysseusinc.com>
2021-02-20 18:31:24 +0300

    Sequence used for sec_permission table was changed (#1796)
    (cherry picked from commit d042238)

--- src/main/resources/db/migration/sqlserver/V2.8.0.20190414180601__add-entity-exists-permission.sql
+++ src/main/resources/db/migration/sqlserver/V2.8.0.20190414180601__add-entity-exists-permission.sql
@@ -1,13 +1,13 @@
 INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description)
 VALUES
-  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'conceptset:*:exists:get', 'Check name uniqueness of concept set'), 
-  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'cohortdefinition:*:exists:get', 'Check name uniqueness of cohort definition'),
-  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'pathway-analysis:*:exists:get', 'Check name uniqueness of pathway analysis'),
-  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'cohort-characterization:*:exists:get', 'Check name uniqueness of cohort characterization'),
-  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'feature-analysis:*:exists:get', 'Check name uniqueness of feature analysis'),
-  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'ir:*:exists:get', 'Check name uniqueness of incidence rate'),
-  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'prediction:*:exists:get', 'Check name uniqueness of prediction'),
-  (NEXT VALUE FOR ${ohdsiSchema}.sec_role_permission_sequence, 'estimation:*:exists:get', 'Check name uniqueness of estimation');
+  (NEXT VALUE FOR ${ohdsiSchema}.sec_permission_id_seq, 'conceptset:*:exists:get', 'Check name uniqueness of concept set'),
+  (NEXT VALUE FOR ${ohdsiSchema}.sec_permission_id_seq, 'cohortdefinition:*:exists:get', 'Check name uniqueness of cohort definition'),
+  (NEXT VALUE FOR ${ohdsiSchema}.sec_permission_id_seq, 'pathway-analysis:*:exists:get', 'Check name uniqueness of pathway analysis'),
+  (NEXT VALUE FOR ${ohdsiSchema}.sec_permission_id_seq, 'cohort-characterization:*:exists:get', 'Check name uniqueness of cohort characterization'),
+  (NEXT VALUE FOR ${ohdsiSchema}.sec_permission_id_seq, 'feature-analysis:*:exists:get', 'Check name uniqueness of feature analysis'),
+  (NEXT VALUE FOR ${ohdsiSchema}.sec_permission_id_seq, 'ir:*:exists:get', 'Check name uniqueness of incidence rate'),
+  (NEXT VALUE FOR ${ohdsiSchema}.sec_permission_id_seq, 'prediction:*:exists:get', 'Check name uniqueness of prediction'),
+  (NEXT VALUE FOR ${ohdsiSchema}.sec_permission_id_seq, 'estimation:*:exists:get', 'Check name uniqueness of estimation');
 
 
 INSERT INTO ${ohdsiSchema}.sec_role_permission(id, role_id, permission_id)

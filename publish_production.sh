d3cd5f4c3ff69af899da1f05181129db036a74dd
Sus Verwimp <sus.verwimp@hotmail.com>
2021-04-30 12:20:35 +0200

    Update version to 1.9-SNAPSHOT for development

--- publish_production.sh
+++ publish_production.sh
@@ -3,6 +3,6 @@ set -e
 
 export AWS_DEFAULT_REGION='eu-west-1';
 aws ecr get-login --no-include-email | bash;
-docker pull 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.8-UAT;
-docker tag 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.8-UAT 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.8
-docker push 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.8
+docker pull 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.9-UAT;
+docker tag 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.9-UAT 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.9
+docker push 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.9

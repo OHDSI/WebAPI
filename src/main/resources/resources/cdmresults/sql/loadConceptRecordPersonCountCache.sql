91f86b02e871af6d8ab9b6cd4de2170d33604af5
Sergey Suvorov <sergey.suvorov@odysseusinc.com>
2021-05-28 23:10:27 +0300

    Person count and descendant person count are now taken from correct columns

--- src/main/resources/resources/cdmresults/sql/loadConceptRecordPersonCountCache.sql
+++ src/main/resources/resources/cdmresults/sql/loadConceptRecordPersonCountCache.sql
@@ -2,6 +2,6 @@ SELECT
     concept_id,
     record_count,
     descendant_record_count,
-    record_count as person_count,
-    descendant_record_count as descendant_person_count
+    person_count,
+    descendant_person_count
 FROM @resultTableQualifier.achilles_result_concept_count;

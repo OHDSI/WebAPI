52a91174e590b6f1c0d15bc410016b99d9d3677d
Sus Verwimp <sus.verwimp@hotmail.com>
2021-06-10 16:24:53 +0200

    build without version

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 set -eux
 
-VERSION=2.0.0
-TAG=2.9.0-$VERSION
-
-docker build --pull --rm -f "Dockerfile" -t feder8/webapi:$TAG "."
+docker build --pull --rm -f "Dockerfile" -t feder8/webapi:latest "."

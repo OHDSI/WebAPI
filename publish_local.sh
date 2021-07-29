24478874f62917094daddfaded97bcbca597091e
Sus Verwimp <sus.verwimp@hotmail.com>
2021-07-29 11:06:51 +0200

    copy paster error push webapi instead of atlas

--- publish_local.sh
+++ publish_local.sh
@@ -4,5 +4,5 @@ set -eux
 VERSION=2.0.0
 TAG=2.9.0-$VERSION
 
-docker tag feder8/atlas:latest $THERAPEUTIC_AREA_URL/$THERAPEUTIC_AREA/webapi:$TAG
+docker tag feder8/webapi:latest $THERAPEUTIC_AREA_URL/$THERAPEUTIC_AREA/webapi:$TAG
 docker push $THERAPEUTIC_AREA_URL/$THERAPEUTIC_AREA/webapi:$TAG

3a066683a34270ffc489c513cbdd1560ca23ae6f
Sus Verwimp <sus.verwimp@hotmail.com>
2021-08-12 17:44:23 +0200

    add default value to root and do not declare var again

--- cmd/healthcheck/main.go
+++ cmd/healthcheck/main.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-    contextPath := os.Getenv("SERVER_CONTEXT_PATH")
+    contextPath := os.Getenv("SERVER_CONTEXT_PATH", "/")
     if contextPath == "/" {
-        contextPath := ""
+        contextPath = ""
     }
 	_, err := http.Get("http://127.0.0.1:8080" + contextPath + "/info")
 	if err != nil {

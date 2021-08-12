39bd469466e18cc826470199dfcfb78a12fcc41c
Sus Verwimp <sus.verwimp@hotmail.com>
2021-08-12 17:28:44 +0200

    add context path to healtchcheck go script

--- cmd/healthcheck/main.go
+++ cmd/healthcheck/main.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
-	_, err := http.Get("http://127.0.0.1:8080/actuator/health/liveness")
+    contextPath := os.Getenv("SERVER_CONTEXT_PATH")
+    if contextPath == "/" {
+        contextPath := ""
+    }
+	_, err := http.Get("http://127.0.0.1:8080" + contextPath + "/info")
 	if err != nil {
 		os.Exit(1)
 	}

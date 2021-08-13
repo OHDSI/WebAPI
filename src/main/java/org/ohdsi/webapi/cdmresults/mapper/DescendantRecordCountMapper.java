b6be1048e334760f1066a503e2b989751628fcf7
Sergey Suvorov <sergey.suvorov@odysseusinc.com>
2021-08-13 16:38:49 +0300

    removed base class for record counts

--- src/main/java/org/ohdsi/webapi/cdmresults/mapper/DescendantRecordCountMapper.java
+++ src/main/java/org/ohdsi/webapi/cdmresults/mapper/DescendantRecordCountMapper.java
@@ -20,8 +20,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import org.ohdsi.webapi.cdmresults.DescendantRecordCount;
 
-public class DescendantRecordCountMapper extends BaseRecordCountMapper<DescendantRecordCount> {
-	protected DescendantRecordCount getResultObject() {
-		return new DescendantRecordCount();
+public class DescendantRecordCountMapper {
+
+	public DescendantRecordCount mapRow(ResultSet rs)
+			throws SQLException {
+		DescendantRecordCount descendantRecordCount = new DescendantRecordCount();
+		descendantRecordCount.setId(rs.getInt("concept_id"));
+		descendantRecordCount.setRecordCount(rs.getLong("record_count"));
+		descendantRecordCount.setDescendantRecordCount(rs.getLong("descendant_record_count"));
+		return descendantRecordCount;
 	}
 }

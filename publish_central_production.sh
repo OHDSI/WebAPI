cd066b864d410323cc424188ef8a0fa879ae00a6
Sus Verwimp <sus.verwimp@hotmail.com>
2021-09-30 14:58:26 +0200

    update versions for development

--- publish_central_production.sh
+++ publish_central_production.sh
@@ -3,6 +3,6 @@ set -e
 
 export AWS_DEFAULT_REGION='eu-west-1';
 aws ecr get-login --no-include-email | bash;
-docker pull 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.9-UAT;
-docker tag 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.9-UAT 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.9
-docker push 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.9
+docker pull 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.10-UAT;
+docker tag 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.10-UAT 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.10
+docker push 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.10

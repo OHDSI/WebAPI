bbfa1bad25ab860dc1cdff6d79a4350442a71208
Sus Verwimp <sus.verwimp@hotmail.com>
2022-02-04 16:05:18 +0100

    update to v1.10-UAT for UAT release

--- publish_central.sh
+++ publish_central.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-docker tag feder8/webapi:latest 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:latest
+docker tag feder8/webapi:latest 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.10-UAT
 export AWS_DEFAULT_REGION='eu-west-1'
 aws ecr get-login --no-include-email | bash
-docker push 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:latest
+docker push 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.10-UAT

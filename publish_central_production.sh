df8242d168d72e187e92c01a2a790429abd0eee7
Sus Verwimp <sus.verwimp@hotmail.com>
2022-02-24 14:08:51 +0100

    update for develop branch

--- publish_central_production.sh
+++ publish_central_production.sh
@@ -3,6 +3,6 @@ set -e
 
 export AWS_DEFAULT_REGION='eu-west-1';
 aws ecr get-login --no-include-email | bash;
-docker pull 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.10-UAT;
-docker tag 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.10-UAT 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.10
-docker push 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.10
+docker pull 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.11-UAT;
+docker tag 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.11-UAT 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.11
+docker push 973455288590.dkr.ecr.eu-west-1.amazonaws.com/honeur/webapi:1.11

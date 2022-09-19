1a79a3fd265d56bd914e3868a2739e37c7fdd162
Peter Moorthamer <moorthamer@gmail.com>
2022-09-19 08:38:28 +0200

    Update manifest for PRD (JIRA AEKR-1506)

--- build-manifest.sh
+++ build-manifest.sh
@@ -4,20 +4,32 @@
 VERSION=2.0.2
 TAG=2.9.0-$VERSION
 
-REGISTRY=harbor-uat.honeur.org
+REGISTRY=harbor.honeur.org
 REPOSITORY=honeur
 docker manifest rm $REGISTRY/$REPOSITORY/webapi:$TAG
 docker manifest create $REGISTRY/$REPOSITORY/webapi:$TAG $REGISTRY/$REPOSITORY/webapi:$TAG-amd64 $REGISTRY/$REPOSITORY/webapi:$TAG-arm64
 docker manifest push $REGISTRY/$REPOSITORY/webapi:$TAG
 
-export REGISTRY=harbor-uat.athenafederation.org
+export REGISTRY=harbor.athenafederation.org
 export REPOSITORY=athena
 docker manifest rm $REGISTRY/$REPOSITORY/webapi:$TAG
 docker manifest create $REGISTRY/$REPOSITORY/webapi:$TAG $REGISTRY/$REPOSITORY/webapi:$TAG-amd64 $REGISTRY/$REPOSITORY/webapi:$TAG-arm64
 docker manifest push $REGISTRY/$REPOSITORY/webapi:$TAG
 
-export REGISTRY=harbor-uat.lupusnet.org
+export REGISTRY=harbor.lupusnet.org
 export REPOSITORY=lupus
+docker manifest rm $REGISTRY/$REPOSITORY/webapi:$TAG
+docker manifest create $REGISTRY/$REPOSITORY/webapi:$TAG $REGISTRY/$REPOSITORY/webapi:$TAG-amd64 $REGISTRY/$REPOSITORY/webapi:$TAG-arm64
+docker manifest push $REGISTRY/$REPOSITORY/webapi:$TAG
+
+export REGISTRY=harbor.esfurn.org
+export REPOSITORY=esfurn
+docker manifest rm $REGISTRY/$REPOSITORY/webapi:$TAG
+docker manifest create $REGISTRY/$REPOSITORY/webapi:$TAG $REGISTRY/$REPOSITORY/webapi:$TAG-amd64 $REGISTRY/$REPOSITORY/webapi:$TAG-arm64
+docker manifest push $REGISTRY/$REPOSITORY/webapi:$TAG
+
+export REGISTRY=harbor.phederation.org
+export REPOSITORY=phederation
 docker manifest rm $REGISTRY/$REPOSITORY/webapi:$TAG
 docker manifest create $REGISTRY/$REPOSITORY/webapi:$TAG $REGISTRY/$REPOSITORY/webapi:$TAG-amd64 $REGISTRY/$REPOSITORY/webapi:$TAG-arm64
 docker manifest push $REGISTRY/$REPOSITORY/webapi:$TAG

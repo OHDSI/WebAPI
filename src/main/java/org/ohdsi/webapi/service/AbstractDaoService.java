b8f11bf689ccd28106aa6629580c506680fd1faf
alex-odysseus <75131044+alex-odysseus@users.noreply.github.com>
2022-10-03 20:25:22 +0200

    Missing import for Properties reverted (#2100)

--- src/main/java/org/ohdsi/webapi/service/AbstractDaoService.java
+++ src/main/java/org/ohdsi/webapi/service/AbstractDaoService.java
@@ -64,7 +64,9 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;import java.util.stream.Collectors;
+import java.util.Properties;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public abstract class AbstractDaoService extends AbstractAdminService {
 

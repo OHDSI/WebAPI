cae8553b7bf085052f832c9f26f863ce5c14f530
Chris Knoll <cknoll@ohdsi.org>
2022-10-25 10:25:15 -0400

    Use temp table for security permission migrations.
    Fixes #2141.

--- src/main/resources/db/migration/postgresql/V2.7.2.20190528153600__fix-ir-report-perms.sql
+++ src/main/resources/db/migration/postgresql/V2.7.2.20190528153600__fix-ir-report-perms.sql
@@ -1,17 +1,29 @@
-alter table ${ohdsiSchema}.sec_permission add for_role_id int;
+--alter table ${ohdsiSchema}.sec_permission add for_role_id int;
 
-INSERT INTO ${ohdsiSchema}.sec_permission (id, value, for_role_id)
-  SELECT nextval('${ohdsiSchema}.sec_permission_id_seq'), REPLACE(CAST(new_perms.val AS VARCHAR(255)), '%s', REPLACE(REPLACE(value, 'source:', ''), ':access', '')), role_id
-  FROM ${ohdsiSchema}.sec_permission sp
-    JOIN ${ohdsiSchema}.sec_role_permission srp on sp.id = srp.permission_id
-    CROSS JOIN (
-                 SELECT 'ir:%s:info:*:delete' val
-               ) new_perms
-  WHERE sp.value LIKE 'source:%:access';
+CREATE TEMP TABLE temp_migration (
+  from_perm_id int,
+  new_value character varying(255)
+);
 
-INSERT INTO ${ohdsiSchema}.sec_role_permission (id, role_id, permission_id)
-  SELECT nextval('${ohdsiSchema}.sec_role_permission_sequence'), sp.for_role_id, sp.id
-  FROM ${ohdsiSchema}.sec_permission sp
-  WHERE sp.for_role_id IS NOT NULL;
+INSERT INTO temp_migration (from_perm_id, new_value)
+SELECT sp.id as from_id,
+	REPLACE(CAST(new_perms.val AS VARCHAR(255)), '%s', REPLACE(REPLACE(value, 'source:', ''), ':access', '')) as new_value
+FROM ${ohdsiSchema}.sec_permission sp
+CROSS JOIN (
+	SELECT 'ir:%s:info:*:delete' val
+) new_perms
+WHERE sp.value LIKE 'source:%:access';
 
-alter table ${ohdsiSchema}.sec_permission drop column for_role_id;
+INSERT INTO ${ohdsiSchema}.sec_permission (id, value)
+SELECT nextval('${ohdsiSchema}.sec_permission_id_seq'), new_value
+FROM temp_migration;
+
+INSERT INTO ${ohdsiSchema}.sec_role_permission (id,role_id, permission_id)
+SELECT nextval('${ohdsiSchema}.sec_role_permission_sequence'),
+  srp.role_id,
+  sp.id as permission_id
+FROM temp_migration m
+JOIN ${ohdsiSchema}.sec_permission sp on m.new_value = sp.value
+JOIN ${ohdsiSchema}.sec_role_permission srp on m.from_perm_id = srp.permission_id;
+
+drop table temp_migration;

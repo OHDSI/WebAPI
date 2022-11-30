6de0796feaf3dc31070d3801adbbd3c5008a4707
ssuvorov-fls <sergey.suvorov@odysseusinc.com>
2022-11-30 14:58:42 +0300

    added permissions for reindex status endpoint

--- src/main/resources/db/migration/postgresql/V2.13.0.20221031170000__add_concept_set_search_permission.sql
+++ src/main/resources/db/migration/postgresql/V2.13.0.20221031170000__add_concept_set_search_permission.sql
@@ -4,6 +4,8 @@ INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description) VALUES
     (nextval('${ohdsiSchema}.sec_permission_id_seq'), 'conceptset:*:search', 'Concept sets search permission');
 INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description) VALUES
     (nextval('${ohdsiSchema}.sec_permission_id_seq'), 'conceptset:*:index', 'Concept sets search index permission');
+INSERT INTO ${ohdsiSchema}.sec_permission(id, value, description) VALUES
+    (nextval('${ohdsiSchema}.sec_permission_id_seq'), 'conceptset:*:index:*:status', 'Concept sets search index status permission');
 
 INSERT INTO ${ohdsiSchema}.sec_role_permission(id, role_id, permission_id)
 SELECT nextval('${ohdsiSchema}.sec_role_permission_sequence'), sr.id, sp.id
@@ -13,4 +15,4 @@ WHERE sp.value in ('conceptset:searchAvailable', 'conceptset:*:search') AND sr.n
 INSERT INTO ${ohdsiSchema}.sec_role_permission(id, role_id, permission_id)
 SELECT nextval('${ohdsiSchema}.sec_role_permission_sequence'), sr.id, sp.id
 FROM ${ohdsiSchema}.sec_permission SP, ${ohdsiSchema}.sec_role sr
-WHERE sp.value = 'conceptset:*:index' AND sr.name IN ('admin');
+WHERE sp.value in ('conceptset:*:index', 'conceptset:*:index:*:status') AND sr.name IN ('admin');

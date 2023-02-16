6b0fa0e605791f7fc0736aa5bba758cfb1beb37d
Frédéric De Smet <fredericdesmet@live.be>
2023-02-16 09:52:22 +0100

    Update schem var

--- src/main/resources/db/migration/sqlserver/V2.9.0.20210727101117__achilles_cache.sql
+++ src/main/resources/db/migration/sqlserver/V2.9.0.20210727101117__achilles_cache.sql
@@ -1,20 +1,21 @@
-CREATE TABLE webapi.achilles_cache_seq (
+
+CREATE TABLE ${ohdsiSchema}.achilles_cache_seq (
   id INT IDENTITY(1,1) PRIMARY KEY
 );
 
-CREATE TABLE webapi.achilles_cache
+CREATE TABLE ${ohdsiSchema}.achilles_cache
 (
     id         BIGINT NOT NULL,
     source_id  INT NOT NULL,
     cache_name VARCHAR NOT NULL,
     cache      TEXT,
     CONSTRAINT achilles_cache_pk PRIMARY KEY (id),
-    CONSTRAINT achilles_cache_fk FOREIGN KEY (source_id) REFERENCES webapi.source (source_id) ON DELETE CASCADE
+    CONSTRAINT achilles_cache_fk FOREIGN KEY (source_id) REFERENCES ${ohdsiSchema}.source (source_id) ON DELETE CASCADE
 );
 
-INSERT INTO webapi.achilles_cache (id, source_id, cache_name, cache)
-SELECT (SELECT MAX(id) + 1 FROM webapi.achilles_cache_seq), source_id, cache_name, cache
-FROM webapi.achilles_cache;
+INSERT INTO ${ohdsiSchema}.achilles_cache (id, source_id, cache_name, cache)
+SELECT (SELECT MAX(id) + 1 FROM ${ohdsiSchema}.achilles_cache_seq), source_id, cache_name, cache
+FROM ${ohdsiSchema}.achilles_cache;
 
 
-CREATE UNIQUE NONCLUSTERED INDEX achilles_cache_source_id_idx ON webapi.achilles_cache (source_id, cache_name);
+CREATE UNIQUE NONCLUSTERED INDEX achilles_cache_source_id_idx ON ${ohdsiSchema}.achilles_cache (source_id, cache_name);

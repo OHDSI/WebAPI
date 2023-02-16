f10690bc8f2444aaed236138ce2be66c2e065b47
Frédéric De Smet <fredericdesmet@live.be>
2023-02-16 20:58:47 +0100

    "Another one" DJ Khaled

--- src/main/resources/db/migration/sqlserver/V2.9.0.20210727101117__achilles_cache.sql
+++ src/main/resources/db/migration/sqlserver/V2.9.0.20210727101117__achilles_cache.sql
@@ -1,21 +1,13 @@
+CREATE SEQUENCE webapi.achilles_cache_seq START WITH 1;
 
-CREATE TABLE ${ohdsiSchema}.achilles_cache_seq (
-  id INT IDENTITY(1,1) PRIMARY KEY
-);
-
-CREATE TABLE ${ohdsiSchema}.achilles_cache
+CREATE TABLE webapi.achilles_cache
 (
-    id         BIGINT NOT NULL,
-    source_id  INT NOT NULL,
-    cache_name VARCHAR NOT NULL,
-    cache      TEXT,
+    id         bigint  NOT NULL DEFAULT NEXT VALUE FOR webapi.achilles_cache_seq,
+    source_id  INT    NOT NULL,
+    cache_name varchar NOT NULL,
+    cache      text,
     CONSTRAINT achilles_cache_pk PRIMARY KEY (id),
-    CONSTRAINT achilles_cache_fk FOREIGN KEY (source_id) REFERENCES ${ohdsiSchema}.source (source_id) ON DELETE CASCADE
+    CONSTRAINT achilles_cache_fk FOREIGN KEY (source_id) REFERENCES webapi."source" (source_id) ON DELETE CASCADE
 );
 
-INSERT INTO ${ohdsiSchema}.achilles_cache (id, source_id, cache_name, cache)
-SELECT (SELECT MAX(id) + 1 FROM ${ohdsiSchema}.achilles_cache_seq), source_id, cache_name, cache
-FROM ${ohdsiSchema}.achilles_cache;
-
-
-CREATE UNIQUE NONCLUSTERED INDEX achilles_cache_source_id_idx ON ${ohdsiSchema}.achilles_cache (source_id, cache_name);
+CREATE UNIQUE INDEX achilles_cache_source_id_idx ON webapi.achilles_cache (source_id, cache_name);

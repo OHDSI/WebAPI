ba5ded852eae8047dc9173fe0cd8409b4536260b
ssuvorov-fls <sergey.suvorov@odysseusinc.com>
2023-05-24 14:55:29 +0300

    #1790 Wildcard is not used now by default. User have to set it explicitly if needed

--- src/main/java/org/ohdsi/webapi/user/importer/providers/AbstractLdapProvider.java
+++ src/main/java/org/ohdsi/webapi/user/importer/providers/AbstractLdapProvider.java
@@ -8,11 +8,14 @@ import org.springframework.ldap.core.AttributesMapper;
 import org.springframework.ldap.core.CollectingNameClassPairCallbackHandler;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.filter.AndFilter;
-import org.springframework.ldap.filter.WhitespaceWildcardsFilter;
+import org.springframework.ldap.filter.EqualsFilter;
+import org.springframework.ldap.support.LdapEncoder;
 import org.springframework.ldap.support.LdapUtils;
 
 import java.util.List;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.ohdsi.webapi.user.importer.providers.OhdsiLdapUtils.getCriteria;
 import static org.ohdsi.webapi.user.importer.providers.OhdsiLdapUtils.valueAsString;
@@ -24,12 +27,49 @@ public abstract class AbstractLdapProvider implements LdapProvider {
 
   @Override
   public List<LdapGroup> findGroups(String searchStr) {
-
     LdapTemplate ldapTemplate = getLdapTemplate();
-    AndFilter filter = new AndFilter();
-//    filter.and(getCriteria(OBJECTCLASS_ATTR, getGroupClasses()));
-    filter.and(new WhitespaceWildcardsFilter(CN_ATTR, searchStr));
-    return ldapTemplate.search(LdapUtils.emptyLdapName(), filter.encode(), getAttributesMapper(LdapGroup::new));
+    return ldapTemplate.search(LdapUtils.emptyLdapName(), getFilterString(searchStr), getAttributesMapper(LdapGroup::new));
+  }
+
+  private String getFilterString(String searchString) {
+    StringBuffer buff = new StringBuffer();
+    buff.append('(');
+    buff.append(CN_ATTR).append("=").append(encodeSearchString(searchString));
+    buff.append(')');
+    return buff.toString();
+  }
+
+  private String encodeSearchString(String searchString) {
+    // encode search string without encoding wildcards
+    if (searchString == null) {
+      return null;
+    }
+    char wildcard = '*';
+    List<Integer> wildcardIndexes = IntStream.range(0, searchString.length())
+            .filter(i -> searchString.charAt(i) == wildcard).boxed()
+            .collect(Collectors.toList());
+    StringBuffer buff = new StringBuffer();
+    if (wildcardIndexes.isEmpty()) {
+      buff.append(LdapEncoder.filterEncode(searchString));
+    } else {
+      int previousWildcardIndex = -1;
+      for (int i = 0; i <= wildcardIndexes.size() - 1; i++) {
+        int wildcardIndex = wildcardIndexes.get(i);
+        if (wildcardIndex > previousWildcardIndex + 1 ) {
+          String substring = searchString.substring(previousWildcardIndex + 1, wildcardIndex);
+          buff.append(LdapEncoder.filterEncode(substring));
+        }
+        buff.append(wildcard);
+        // last wildcard in the string but not trailing
+        if (i == wildcardIndexes.size() - 1 && wildcardIndex < searchString.length() - 1) {
+          String substring = searchString.substring(wildcardIndex + 1, searchString.length());
+          buff.append(LdapEncoder.filterEncode(substring));
+        }
+        previousWildcardIndex = wildcardIndex;
+      }
+    }
+
+    return buff.toString();
   }
 
   @Override

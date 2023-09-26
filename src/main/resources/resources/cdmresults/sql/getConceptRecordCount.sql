1c5de2fec408fb548a9c7d5cbf3bd3527f288f9f
ww166 <85734872+ww166@users.noreply.github.com>
2023-09-26 11:22:27 +0800

    Fix: Sql syntax error on Update getConceptRecordCount.sql

--- src/main/resources/resources/cdmresults/sql/getConceptRecordCount.sql
+++ src/main/resources/resources/cdmresults/sql/getConceptRecordCount.sql
@@ -1,3 +1,3 @@
 select concept_id, record_count, descendant_record_count
-from @resultTableQualifier..achilles_result_concept_count
+from @resultTableQualifier.achilles_result_concept_count
 where concept_id IN (@conceptIdentifiers)

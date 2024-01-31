8e28b98f6900289f539d046e6ab98892eeed812d
Hernaldo Urbina <hernaldo.urbina@odysseusinc.com>
2024-01-30 23:18:45 -0600

    ATL-8: Atlas Usage Statistics

--- src/main/java/org/ohdsi/webapi/statistic/controller/StatisticController.java
+++ src/main/java/org/ohdsi/webapi/statistic/controller/StatisticController.java
@@ -2,7 +2,7 @@ package org.ohdsi.webapi.statistic.controller;
 
 import com.opencsv.CSVWriter;
 
-import org.apache.commons.lang3.tuple.Pair;
+import org.ohdsi.webapi.shiro.TokenManager;
 import org.ohdsi.webapi.statistic.dto.AccessTrendDto;
 import org.ohdsi.webapi.statistic.dto.AccessTrendsDto;
 import org.ohdsi.webapi.statistic.dto.EndpointDto;
@@ -11,9 +11,10 @@ import org.ohdsi.webapi.statistic.dto.SourceExecutionsDto;
 import org.ohdsi.webapi.statistic.service.StatisticService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -26,6 +27,8 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Controller
@@ -33,6 +36,10 @@ import java.util.stream.Collectors;
 @ConditionalOnProperty(value = "audit.trail.enabled", havingValue = "true")
 public class StatisticController {
     private StatisticService service;
+
+    public enum ResponseFormat {
+        CSV, JSON
+    }
 
     private static final List<String[]> EXECUTION_STATISTICS_CSV_RESULT_HEADER = new ArrayList<String[]>() {{
         add(new String[]{"Date", "Source", "Execution Type"});
@@ -56,11 +63,16 @@ public class StatisticController {
     @Consumes(MediaType.APPLICATION_JSON)
     public Response executionStatistics(ExecutionStatisticsRequest executionStatisticsRequest) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String userID = executionStatisticsRequest.isShowUserInformation() ? extractUserID() : null;
         
         SourceExecutionsDto sourceExecutions = service.getSourceExecutions(LocalDate.parse(executionStatisticsRequest.getStartDate(), formatter),
-                LocalDate.parse(executionStatisticsRequest.getEndDate(), formatter), executionStatisticsRequest.getSourceKey());
+                LocalDate.parse(executionStatisticsRequest.getEndDate(), formatter), executionStatisticsRequest.getSourceKey(), userID);
 
-        return prepareExecutionResultResponse(sourceExecutions.getExecutions(), "execution_statistics.zip");
+        if (ResponseFormat.CSV.equals(executionStatisticsRequest.getResponseFormat())) {
+            return prepareExecutionResultResponse(sourceExecutions.getExecutions(), "execution_statistics.zip");
+        } else {
+            return Response.ok(sourceExecutions).build();
+        }
     }
 
     /**
@@ -73,11 +85,16 @@ public class StatisticController {
     @Consumes(MediaType.APPLICATION_JSON)
     public Response accessStatistics(AccessTrendsStatisticsRequest accessTrendsStatisticsRequest) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        
-        AccessTrendsDto trends = service.getAccessTrends(LocalDate.parse(accessTrendsStatisticsRequest.getStartDate(), formatter),
-                LocalDate.parse(accessTrendsStatisticsRequest.getEndDate(), formatter), accessTrendsStatisticsRequest.getEndpoints());
+        String userID = accessTrendsStatisticsRequest.isShowUserInformation() ? extractUserID() : null;
 
-        return prepareAccessTrendsResponse(trends.getTrends(), "execution_trends.zip");
+        AccessTrendsDto trends = service.getAccessTrends(LocalDate.parse(accessTrendsStatisticsRequest.getStartDate(), formatter),
+                LocalDate.parse(accessTrendsStatisticsRequest.getEndDate(), formatter), accessTrendsStatisticsRequest.getEndpoints(), userID);
+
+        if (ResponseFormat.CSV.equals(accessTrendsStatisticsRequest.getResponseFormat())) {
+            return prepareAccessTrendsResponse(trends.getTrends(), "execution_trends.zip");
+        } else {
+            return Response.ok(trends).build();
+        }
     }
 
     private Response prepareExecutionResultResponse(List<SourceExecutionDto> executions, String filename) {
@@ -121,12 +138,23 @@ public class StatisticController {
         }
     }
 
+    private String extractUserID() {
+        return Optional.ofNullable(RequestContextHolder.getRequestAttributes())
+                .map(ServletRequestAttributes.class::cast)
+                .map(ServletRequestAttributes::getRequest)
+                .map(TokenManager::extractToken)
+                .map(TokenManager::getSubject)
+                .orElse(null);
+    }
+
     public static final class ExecutionStatisticsRequest {
         // Format - yyyy-MM-dd
         String startDate;
         // Format - yyyy-MM-dd
         String endDate;
         String sourceKey;
+        ResponseFormat responseFormat;
+        boolean showUserInformation;
 
         public String getStartDate() {
             return startDate;
@@ -151,6 +179,22 @@ public class StatisticController {
         public void setSourceKey(String sourceKey) {
             this.sourceKey = sourceKey;
         }
+
+        public ResponseFormat getResponseFormat() {
+            return responseFormat;
+        }
+
+        public void setResponseFormat(ResponseFormat responseFormat) {
+            this.responseFormat = responseFormat;
+        }
+
+        public boolean isShowUserInformation() {
+            return showUserInformation;
+        }
+
+        public void setShowUserInformation(boolean showUserInformation) {
+            this.showUserInformation = showUserInformation;
+        }
     }
 
     public static final class AccessTrendsStatisticsRequest {
@@ -161,6 +205,8 @@ public class StatisticController {
         // Key - method (POST, GET)
         // Value - endpoint ("{}" can be used as a placeholder, will be converted to ".*" in regular expression)
         List<EndpointDto> endpoints;
+        ResponseFormat responseFormat;
+        boolean showUserInformation;
         
         public String getStartDate() {
             return startDate;
@@ -184,6 +230,22 @@ public class StatisticController {
 
         public void setEndpoints(List<EndpointDto> endpoints) {
             this.endpoints = endpoints;
+        }
+
+        public ResponseFormat getResponseFormat() {
+            return responseFormat;
+        }
+
+        public void setResponseFormat(ResponseFormat responseFormat) {
+            this.responseFormat = responseFormat;
+        }
+
+        public boolean isShowUserInformation() {
+            return showUserInformation;
+        }
+
+        public void setShowUserInformation(boolean showUserInformation) {
+            this.showUserInformation = showUserInformation;
         }
     }
 }

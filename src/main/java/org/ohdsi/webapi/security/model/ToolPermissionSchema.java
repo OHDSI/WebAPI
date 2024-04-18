389fac5207be756881c204712b3bba94fb3da138
Juan Carlos Namendi Pineda <juan.namendi@odysseusinc.com>
2024-04-18 05:55:15 -0600

    Remove unused imports

--- src/main/java/org/ohdsi/webapi/security/model/ToolPermissionSchema.java
+++ src/main/java/org/ohdsi/webapi/security/model/ToolPermissionSchema.java
@@ -1,8 +1,5 @@
 package org.ohdsi.webapi.security.model;
 
-import org.ohdsi.webapi.model.CommonEntity;
-import org.ohdsi.webapi.shiro.Entities.RoleEntity;
-import org.ohdsi.webapi.tool.Tool;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -11,13 +8,13 @@ import java.util.Map;
 @Component
 public class ToolPermissionSchema extends EntityPermissionSchema {
     private static Map<String, String> writePermissions = new HashMap<String, String>() {{
-        put("tool:%s:delete", "Delete Tool with by id = %s");
-        put("tool:%s:put", "Update Tool with by id = %s");
-        put("tool:%s:post", "Create Tool with by id = %s");
+        put("tool:%s:delete", "Delete Tool with id = %s");
+        put("tool:%s:put", "Update Tool with id = %s");
+        put("tool:%s:post", "Create Tool with id = %s");
     }};
     private static Map<String, String> readPermissions = new HashMap<String, String>() {
         {
-            put("tool:%s:get", "View Tool with by id = %s");
+            put("tool:%s:get", "View Tool with id = %s");
         }
     };
 

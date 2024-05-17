6bb83362b5801e9cc2cf07d48327fd85735be4a6
Mike Peterson <mpeterson@spinsys.com>
2024-05-17 15:06:30 -0400

    changes for MDACA Migration to SB 3

--- src/main/java/org/ohdsi/webapi/I18nConfig.java
+++ src/main/java/org/ohdsi/webapi/I18nConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 

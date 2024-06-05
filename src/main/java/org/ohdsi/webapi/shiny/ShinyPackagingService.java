62e29d31cc45c529cdd75753e0dc8ae6ada03cf8
oleg-odysseus <oleg.himself@gmail.com>
2024-06-05 18:33:15 +0200

    [ATL-49] Implemented Cohort Characterizations Shiny App Publishing

--- src/main/java/org/ohdsi/webapi/shiny/ShinyPackagingService.java
+++ src/main/java/org/ohdsi/webapi/shiny/ShinyPackagingService.java
@@ -1,11 +1,6 @@
 package org.ohdsi.webapi.shiny;
 
 import com.odysseusinc.arachne.commons.api.v1.dto.CommonAnalysisType;
-import org.ohdsi.webapi.shiny.ApplicationBrief;
-import org.ohdsi.webapi.shiny.PackagingStrategy;
-import org.ohdsi.webapi.shiny.TemporaryFile;
-
-import java.nio.file.Path;
 
 public interface ShinyPackagingService {
     CommonAnalysisType getType();

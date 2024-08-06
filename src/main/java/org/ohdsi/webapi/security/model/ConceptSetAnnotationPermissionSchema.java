2b9c35a6193397b2247a8c0c7ee80c4115c7d486
oleg-odysseus <oleg.himself@gmail.com>
2024-08-06 16:53:40 +0200

    [ATL-58] Fixed permissions issues for annotations feature, transformed search data JSON to a human friendly format in annotations tab

--- src/main/java/org/ohdsi/webapi/security/model/ConceptSetAnnotationPermissionSchema.java
+++ src/main/java/org/ohdsi/webapi/security/model/ConceptSetAnnotationPermissionSchema.java
@@ -10,21 +10,16 @@ public class ConceptSetAnnotationPermissionSchema extends EntityPermissionSchema
 
     private static Map<String, String> writePermissions = new HashMap<String, String>() {
         {
-            put("conceptset:*:annotation:put", "Create Concept Set Annotation");
-            put("conceptset:update:*:annotation:put", "Update Concept Set Annotation");
             put("conceptset:annotation:%s:delete", "Delete Concept Set Annotation with ID %s");
         }
     };
 
     private static Map<String, String> readPermissions = new HashMap<String, String>() {
         {
-            put("conceptset:%s:annotation:get", "List Concept Set Annotation with id %s");
-            put("conceptset:*:annotation:get", "View Concept Set Annotation expression");
         }
     };
 
     public ConceptSetAnnotationPermissionSchema() {
-
         super(EntityType.CONCEPT_SET_ANNOTATION, readPermissions, writePermissions);
     }
 }

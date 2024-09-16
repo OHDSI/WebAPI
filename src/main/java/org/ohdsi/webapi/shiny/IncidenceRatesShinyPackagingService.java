e45af1e7e2fb6eb92cdb9e418852347920f2a219
oleg-odysseus <oleg.himself@gmail.com>
2024-09-16 17:53:58 +0200

    Added parameters required for the Shiny Apps introduction pages

--- src/main/java/org/ohdsi/webapi/shiny/IncidenceRatesShinyPackagingService.java
+++ src/main/java/org/ohdsi/webapi/shiny/IncidenceRatesShinyPackagingService.java
@@ -2,7 +2,9 @@ package org.ohdsi.webapi.shiny;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Iterables;
 import com.odysseusinc.arachne.commons.api.v1.dto.CommonAnalysisType;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.QuoteMode;
@@ -11,8 +13,11 @@ import org.ohdsi.webapi.ircalc.AnalysisReport;
 import org.ohdsi.webapi.ircalc.IncidenceRateAnalysis;
 import org.ohdsi.webapi.ircalc.IncidenceRateAnalysisExportExpression;
 import org.ohdsi.webapi.ircalc.IncidenceRateAnalysisRepository;
+import org.ohdsi.webapi.service.CDMResultsService;
 import org.ohdsi.webapi.service.IRAnalysisResource;
 import org.ohdsi.webapi.service.ShinyService;
+import org.ohdsi.webapi.service.dto.AnalysisInfoDTO;
+import org.ohdsi.webapi.shiny.summary.DataSourceSummaryConverter;
 import org.ohdsi.webapi.source.SourceRepository;
 import org.ohdsi.webapi.util.ExceptionUtils;
 import org.slf4j.Logger;
@@ -26,7 +31,9 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.ws.rs.InternalServerErrorException;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Stream;
 
 @Service
@@ -40,8 +47,6 @@ public class IncidenceRatesShinyPackagingService extends CommonShinyPackagingSer
     private final IncidenceRateAnalysisRepository incidenceRateAnalysisRepository;
     private final IRAnalysisResource irAnalysisResource;
 
-    private final SourceRepository sourceRepository;
-
     @Autowired
     public IncidenceRatesShinyPackagingService(
             @Value("${shiny.atlas.url}") String atlasUrl,
@@ -50,11 +55,13 @@ public class IncidenceRatesShinyPackagingService extends CommonShinyPackagingSer
             ManifestUtils manifestUtils,
             ObjectMapper objectMapper,
             IncidenceRateAnalysisRepository incidenceRateAnalysisRepository,
-            IRAnalysisResource irAnalysisResource, SourceRepository sourceRepository) {
-        super(atlasUrl, repoLink, fileWriter, manifestUtils, objectMapper);
+            IRAnalysisResource irAnalysisResource,
+            SourceRepository sourceRepository,
+            CDMResultsService cdmResultsService,
+            DataSourceSummaryConverter dataSourceSummaryConverter) {
+        super(atlasUrl, repoLink, fileWriter, manifestUtils, objectMapper, sourceRepository, cdmResultsService, dataSourceSummaryConverter);
         this.incidenceRateAnalysisRepository = incidenceRateAnalysisRepository;
         this.irAnalysisResource = irAnalysisResource;
-        this.sourceRepository = sourceRepository;
     }
 
     @Override
@@ -77,6 +84,21 @@ public class IncidenceRatesShinyPackagingService extends CommonShinyPackagingSer
             dataConsumers.getAppProperties().accept("analysis_name", analysis.getName());
 
             IncidenceRateAnalysisExportExpression expression = objectMapper.readValue(analysis.getDetails().getExpression(), IncidenceRateAnalysisExportExpression.class);
+            AnalysisInfoDTO analysisInfoDTO = irAnalysisResource.getAnalysisInfo(analysis.getId(), sourceKey);
+
+            Integer assetId = analysis.getId();
+            Integer sourceId = sourceRepository.findBySourceKey(sourceKey).getSourceId();
+
+            dataConsumers.getAppProperties().accept("author", getAuthor(analysis));
+            dataConsumers.getAppProperties().accept(ASSET_ID_KEY, analysis.getId().toString());
+            dataConsumers.getAppProperties().accept("generated_date", getGenerationStartTime(analysis));
+            dataConsumers.getAppProperties().accept("record_count", getRecordCount(analysisInfoDTO));
+            dataConsumers.getAppProperties().accept("person_count", getPersonCount(analysisInfoDTO));
+            dataConsumers.getAppProperties().accept("author_notes", getDescription(analysis));
+            dataConsumers.getAppProperties().accept("referenced_cohorts", prepareReferencedCohorts(expression));
+            dataConsumers.getAppProperties().accept("version_id", getGenerationId(assetId, sourceId));
+            dataConsumers.getAppProperties().accept("generation_id", getGenerationId(assetId, sourceId));
+
             String csvWithCohortDetails = prepareCsvWithCohorts(expression);
 
             dataConsumers.getTextFiles().accept("cohorts.csv", csvWithCohortDetails);
@@ -91,6 +113,61 @@ public class IncidenceRatesShinyPackagingService extends CommonShinyPackagingSer
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private String getAuthor(IncidenceRateAnalysis analysis) {
+        if (analysis.getCreatedBy() != null) {
+            return analysis.getCreatedBy().getLogin();
+        }
+        return VALUE_NOT_AVAILABLE;
+    }
+
+    private String getGenerationStartTime(IncidenceRateAnalysis analysis) {
+        if (analysis != null) {
+            if (CollectionUtils.isNotEmpty(analysis.getExecutionInfoList())) {
+                return dateToString(Iterables.getLast(analysis.getExecutionInfoList()).getStartTime());
+            }
+        }
+        return VALUE_NOT_AVAILABLE;
+    }
+
+    private String getDescription(IncidenceRateAnalysis analysis) {
+        if (analysis != null && analysis.getDescription() != null) {
+            return analysis.getDescription();
+        }
+        return VALUE_NOT_AVAILABLE;
+    }
+
+    private String getPersonCount(AnalysisInfoDTO analysisInfo) {
+        if (analysisInfo != null && CollectionUtils.isNotEmpty(analysisInfo.getSummaryList())) {
+            return Long.toString(Iterables.getLast(analysisInfo.getSummaryList()).cases);
+        }
+        return VALUE_NOT_AVAILABLE;
+    }
+
+    private String getRecordCount(AnalysisInfoDTO analysisInfo) {
+        if (analysisInfo != null && CollectionUtils.isNotEmpty(analysisInfo.getSummaryList())) {
+            return Long.toString(Iterables.getLast(analysisInfo.getSummaryList()).totalPersons);
+        }
+        return VALUE_NOT_AVAILABLE;
+    }
+
+    private String getGenerationId(Integer assetId, Integer sourceId) {
+        return assetId == null || sourceId == null ? "" : Integer.toString(assetId).concat("x").concat(Integer.toString(sourceId));
+    }
+
+    private String prepareReferencedCohorts(IncidenceRateAnalysisExportExpression expression) {
+        if (expression == null) {
+            return "";
+        }
+        Set<String> referencedCohortNames = new HashSet<>();
+        for (CohortDTO targetCohort : expression.targetCohorts) {
+            referencedCohortNames.add(targetCohort.getName());
+        }
+        for (CohortDTO outcomeCohort : expression.outcomeCohorts) {
+            referencedCohortNames.add(outcomeCohort.getName());
+        }
+        return String.join("; ", referencedCohortNames);
     }
 
     private Stream<AnalysisReport> streamAnalysisReportsForAllCohortCombinations(IncidenceRateAnalysisExportExpression expression, Integer analysisId, String sourceKey) {

1f6476c2091004c21d144e1490a22dad0cc63723
oleg-odysseus <oleg.himself@gmail.com>
2024-11-09 01:21:29 +0100

    Changed owner/permissions check for annotations delete to consider ownership of the concept set for annotations

--- src/main/java/org/ohdsi/webapi/security/model/ConceptSetPermissionSchema.java
+++ src/main/java/org/ohdsi/webapi/security/model/ConceptSetPermissionSchema.java
@@ -11,17 +11,20 @@ public class ConceptSetPermissionSchema extends EntityPermissionSchema {
     private static Map<String, String> writePermissions = new HashMap<String, String>() {{
         put("conceptset:%s:put", "Update Concept Set with ID = %s");
         put("conceptset:%s:items:put", "Update Items of Concept Set with ID = %s");
+        put("conceptset:%s:annotation:*:delete", "Delete Annotations of Concept Set with ID = %s");
+        put("conceptset:*:annotation:*:delete", "Delete Annotations of any Concept Set");
         put("conceptset:%s:delete", "Delete Concept Set with ID = %s");
     }};
 
     private static Map<String, String> readPermissions = new HashMap<String, String>() {{
         put("conceptset:%s:get", "view conceptset  definition with id %s");
         put("conceptset:%s:expression:get", "Resolve concept set %s expression");
+        put("conceptset:%s:annotation:get", "Resolve concept set annotations");
+        put("conceptset:*:annotation:get", "Resolve concept set annotations");
         put("conceptset:%s:version:*:expression:get", "Get expression for concept set %s items for default source");
     }};
 
     public ConceptSetPermissionSchema() {
-
         super(EntityType.CONCEPT_SET, readPermissions, writePermissions);
     }
 }

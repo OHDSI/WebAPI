9a0cc1fa76fac9b836b5a89117440bb2a3e5904b
alex-odysseus <alexey.manoylenko@odysseusinc.com>
2024-12-10 19:44:58 +0100

    Combining migration scripts

--- src/main/resources/shiny/migration/V2.15.0.20231222125707__add_shiny_published_and_permissions.sql
+++ src/main/resources/shiny/migration/V2.15.0.20231222125707__add_shiny_published_and_permissions.sql
@@ -1,3 +1,18 @@
+CREATE SEQUENCE ${ohdsiSchema}.shiny_published_sequence START WITH 1;
+
+CREATE TABLE ${ohdsiSchema}.shiny_published(
+    id BIGINT PRIMARY KEY default nextval('${ohdsiSchema}.shiny_published_sequence'),
+    type VARCHAR NOT NULL,
+    analysis_id BIGINT NOT NULL,
+    execution_id BIGINT,
+    source_key VARCHAR,
+    content_id UUID,
+    created_by_id BIGINT REFERENCES ${ohdsiSchema}.sec_user(id),
+    modified_by_id BIGINT REFERENCES ${ohdsiSchema}.sec_user(id),
+    created_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (now()),
+    modified_date TIMESTAMP WITH TIME ZONE
+);
+
 INSERT INTO ${ohdsiSchema}.sec_permission (id, value, description)
 SELECT nextval('${ohdsiSchema}.sec_permission_id_seq'),
        'shiny:download:*:*:*:get',

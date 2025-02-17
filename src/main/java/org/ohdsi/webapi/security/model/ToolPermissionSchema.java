OHDSI/WebAPI

commit fdf2d791f866a29ef8a56ea63a2a8f06de8b2a2b

Aligning with the permissions notation from the migration script

src/main/java/org/ohdsi/webapi/security/model/ToolPermissionSchema.java
@@ -9,8 +9,8 @@ import java.util.Map;
 public class ToolPermissionSchema extends EntityPermissionSchema {
     private static Map<String, String> writePermissions = new HashMap<String, String>() {{
         put("tool:%s:delete", "Delete Tool with id = %s");
-        put("tool:%s:put", "Update Tool with id = %s");
-        put("tool:%s:post", "Create Tool with id = %s");
+        put("tool:put", "Update a Tool");
+        put("tool:post", "Create a Tool");
     }};
     private static Map<String, String> readPermissions = new HashMap<String, String>() {
         {

commit ad014aba6d889f7d53f69d2f50775e4d9049b030

Removing malformed entries from the permission schema correcting a mistake being introduced by the previous commit fdf2d79

src/main/java/org/ohdsi/webapi/security/model/ToolPermissionSchema.java
@@ -9,8 +9,6 @@ import java.util.Map;
 public class ToolPermissionSchema extends EntityPermissionSchema {
     private static Map<String, String> writePermissions = new HashMap<String, String>() {{
         put("tool:%s:delete", "Delete Tool with id = %s");
-        put("tool:put", "Update a Tool");
-        put("tool:post", "Create a Tool");
     }};
     private static Map<String, String> readPermissions = new HashMap<String, String>() {
         {
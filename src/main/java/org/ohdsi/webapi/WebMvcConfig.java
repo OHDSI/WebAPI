dced00e3e35e3b2cbda420d8eb3d486349b1f697
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2026-01-25 13:32:32 +0800

    fix

--- src/main/java/org/ohdsi/webapi/WebMvcConfig.java
+++ src/main/java/org/ohdsi/webapi/WebMvcConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -18,6 +19,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Autowired(required = false)
     private LocaleInterceptor localeInterceptor;
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseTrailingSlashMatch(true);
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {

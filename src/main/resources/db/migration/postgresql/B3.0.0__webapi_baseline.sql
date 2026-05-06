519caecde6ce2254ac870f986874d18992cfce4f
Chris Knoll <cknoll@ohdsi.org>
2026-05-06 13:24:28 -0400

    Cascade deletes to sec_{entity} tables; done migration and baseline scripts.

--- src/main/resources/db/migration/postgresql/B3.0.0__webapi_baseline.sql
+++ src/main/resources/db/migration/postgresql/B3.0.0__webapi_baseline.sql
@@ -1130,6 +1130,74 @@ CREATE TABLE ${ohdsiSchema}.sec_source
 
 -- END sec_{entity} tables
 
+-- FK constraints for sec_{entity} tables: role and entity references.
+-- Both FKs use ON DELETE CASCADE: deleting a role or a parent entity automatically
+-- removes its permission grants in the corresponding sec_* table.
+
+ALTER TABLE ${ohdsiSchema}.sec_cohort_characterization
+    ADD CONSTRAINT fk_scc_sec_role_id
+        FOREIGN KEY (role_id) REFERENCES ${ohdsiSchema}.sec_role(id)
+        ON DELETE CASCADE,
+    ADD CONSTRAINT fk_scc_cohort_characterization_id
+        FOREIGN KEY (cohort_characterization_id) REFERENCES ${ohdsiSchema}.cohort_characterization(id)
+        ON DELETE CASCADE;
+
+ALTER TABLE ${ohdsiSchema}.sec_cohort_definition
+    ADD CONSTRAINT fk_scd_sec_role_id
+        FOREIGN KEY (role_id) REFERENCES ${ohdsiSchema}.sec_role(id)
+        ON DELETE CASCADE,
+    ADD CONSTRAINT fk_scd_cohort_definition_id
+        FOREIGN KEY (cohort_definition_id) REFERENCES ${ohdsiSchema}.cohort_definition(id)
+        ON DELETE CASCADE;
+
+ALTER TABLE ${ohdsiSchema}.sec_concept_set
+    ADD CONSTRAINT fk_scs_sec_role_id
+        FOREIGN KEY (role_id) REFERENCES ${ohdsiSchema}.sec_role(id)
+        ON DELETE CASCADE,
+    ADD CONSTRAINT fk_scs_concept_set_id
+        FOREIGN KEY (concept_set_id) REFERENCES ${ohdsiSchema}.concept_set(concept_set_id)
+        ON DELETE CASCADE;
+
+ALTER TABLE ${ohdsiSchema}.sec_fe_analysis
+    ADD CONSTRAINT fk_sfa_sec_role_id
+        FOREIGN KEY (role_id) REFERENCES ${ohdsiSchema}.sec_role(id)
+        ON DELETE CASCADE,
+    ADD CONSTRAINT fk_sfa_fe_analysis_id
+        FOREIGN KEY (fe_analysis_id) REFERENCES ${ohdsiSchema}.fe_analysis(id)
+        ON DELETE CASCADE;
+
+ALTER TABLE ${ohdsiSchema}.sec_ir_analysis
+    ADD CONSTRAINT fk_sia_sec_role_id
+        FOREIGN KEY (role_id) REFERENCES ${ohdsiSchema}.sec_role(id)
+        ON DELETE CASCADE,
+    ADD CONSTRAINT fk_sia_ir_analysis_id
+        FOREIGN KEY (ir_id) REFERENCES ${ohdsiSchema}.ir_analysis(id)
+        ON DELETE CASCADE;
+
+ALTER TABLE ${ohdsiSchema}.sec_pathway_analysis
+    ADD CONSTRAINT fk_spa_sec_role_id
+        FOREIGN KEY (role_id) REFERENCES ${ohdsiSchema}.sec_role(id)
+        ON DELETE CASCADE,
+    ADD CONSTRAINT fk_spa_pathway_analysis_id
+        FOREIGN KEY (pathway_analysis_id) REFERENCES ${ohdsiSchema}.pathway_analysis(id)
+        ON DELETE CASCADE;
+
+ALTER TABLE ${ohdsiSchema}.sec_reusable
+    ADD CONSTRAINT fk_sr_sec_role_id
+        FOREIGN KEY (role_id) REFERENCES ${ohdsiSchema}.sec_role(id)
+        ON DELETE CASCADE,
+    ADD CONSTRAINT fk_sr_reusable_id
+        FOREIGN KEY (reusable_id) REFERENCES ${ohdsiSchema}.reusable(id)
+        ON DELETE CASCADE;
+
+ALTER TABLE ${ohdsiSchema}.sec_source
+    ADD CONSTRAINT fk_ss_sec_role_id
+        FOREIGN KEY (role_id) REFERENCES ${ohdsiSchema}.sec_role(id)
+        ON DELETE CASCADE,
+    ADD CONSTRAINT fk_ss_source_id
+        FOREIGN KEY (source_id) REFERENCES ${ohdsiSchema}.source(source_id)
+        ON DELETE CASCADE;
+
 CREATE TABLE ${ohdsiSchema}.sec_session
 (
     session_id uuid NOT NULL,
@@ -1707,48 +1775,6 @@ ALTER TABLE ONLY ${ohdsiSchema}.reusable_version
 ALTER TABLE ONLY ${ohdsiSchema}.reusable_version
     ADD CONSTRAINT fk_reusable_version_sec_user_creator FOREIGN KEY (created_by_id) REFERENCES ${ohdsiSchema}.sec_user(id);
 
-ALTER TABLE ONLY ${ohdsiSchema}.sec_cohort_characterization
-    ADD CONSTRAINT fk_scc_cohort_characterization_id FOREIGN KEY (cohort_characterization_id) REFERENCES ${ohdsiSchema}.cohort_characterization (id);
-
-ALTER TABLE ONLY ${ohdsiSchema}.sec_cohort_characterization
-    ADD CONSTRAINT fk_scc_sec_role_id FOREIGN KEY (role_id) REFERENCES ${ohdsiSchema}.sec_role (id);
-
-ALTER TABLE ONLY ${ohdsiSchema}.sec_cohort_definition
-    ADD CONSTRAINT fk_scd_cohort_definition_id FOREIGN KEY (cohort_definition_id) REFERENCES ${ohdsiSchema}.cohort_definition (id);
-
-ALTER TABLE ONLY ${ohdsiSchema}.sec_cohort_definition
-    ADD CONSTRAINT fk_scd_sec_role_id FOREIGN KEY (role_id) REFERENCES ${ohdsiSchema}.sec_role (id);
-
-ALTER TABLE ONLY ${ohdsiSchema}.sec_concept_set
-    ADD CONSTRAINT fk_scs_concept_set_id FOREIGN KEY (concept_set_id) REFERENCES ${ohdsiSchema}.concept_set (concept_set_id);
-
-ALTER TABLE ONLY ${ohdsiSchema}.sec_concept_set
-    ADD CONSTRAINT fk_scs_sec_role_id FOREIGN KEY (role_id) REFERENCES ${ohdsiSchema}.sec_role (id);
-
-ALTER TABLE ONLY ${ohdsiSchema}.sec_fe_analysis
-    ADD CONSTRAINT fk_sfa_fe_analysis_id FOREIGN KEY (fe_analysis_id) REFERENCES ${ohdsiSchema}.fe_analysis (id);
-
-ALTER TABLE ONLY ${ohdsiSchema}.sec_fe_analysis
-    ADD CONSTRAINT fk_sfa_sec_role_id FOREIGN KEY (role_id) REFERENCES ${ohdsiSchema}.sec_role (id);
-
-ALTER TABLE ONLY ${ohdsiSchema}.sec_ir_analysis
-    ADD CONSTRAINT fk_sia_ir_analysis_id FOREIGN KEY (ir_id) REFERENCES ${ohdsiSchema}.ir_analysis (id);
-
-ALTER TABLE ONLY ${ohdsiSchema}.sec_ir_analysis
-    ADD CONSTRAINT fk_sia_sec_role_id FOREIGN KEY (role_id) REFERENCES ${ohdsiSchema}.sec_role (id);
-
-ALTER TABLE ONLY ${ohdsiSchema}.sec_pathway_analysis
-    ADD CONSTRAINT fk_spa_pathway_analysis_id FOREIGN KEY (pathway_analysis_id) REFERENCES ${ohdsiSchema}.pathway_analysis (id);
-
-ALTER TABLE ONLY ${ohdsiSchema}.sec_pathway_analysis
-    ADD CONSTRAINT fk_spa_sec_role_id FOREIGN KEY (role_id) REFERENCES ${ohdsiSchema}.sec_role (id);
-
-ALTER TABLE ONLY ${ohdsiSchema}.sec_reusable
-    ADD CONSTRAINT fk_sr_reusable_id FOREIGN KEY (reusable_id) REFERENCES ${ohdsiSchema}.reusable (id);
-
-ALTER TABLE ONLY ${ohdsiSchema}.sec_reusable
-    ADD CONSTRAINT fk_sr_sec_role_id FOREIGN KEY (role_id) REFERENCES ${ohdsiSchema}.sec_role (id);
-
 ALTER TABLE ONLY ${ohdsiSchema}.sec_role_group
     ADD CONSTRAINT fk_role_group_job FOREIGN KEY (job_id) REFERENCES ${ohdsiSchema}.user_import_job(id) ON DELETE CASCADE;
 
@@ -1769,12 +1795,6 @@ ALTER TABLE ONLY ${ohdsiSchema}.source
 
 ALTER TABLE ONLY ${ohdsiSchema}.source
     ADD CONSTRAINT source_modified_by_id_fkey FOREIGN KEY (modified_by_id) REFERENCES ${ohdsiSchema}.sec_user(id);
-
-ALTER TABLE ONLY ${ohdsiSchema}.sec_source
-    ADD CONSTRAINT fk_ss_source_id FOREIGN KEY (source_id) REFERENCES ${ohdsiSchema}.source(source_id);
-
-ALTER TABLE ONLY ${ohdsiSchema}.sec_source
-    ADD CONSTRAINT fk_ss_sec_role_id FOREIGN KEY (role_id) REFERENCES ${ohdsiSchema}.sec_role(id);
 
 ALTER TABLE ONLY ${ohdsiSchema}.source_daimon
     ADD CONSTRAINT fk_source_daimon_source_id FOREIGN KEY (source_id) REFERENCES ${ohdsiSchema}.source(source_id);

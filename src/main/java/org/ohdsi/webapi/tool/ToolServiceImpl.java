25a06cbf69db6952933ceffda46b905a7f7c1ee3
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2026-06-19 11:23:45 +0800

    fix(security): require admin:tools for tool mutations

--- src/main/java/org/ohdsi/webapi/tool/ToolServiceImpl.java
+++ src/main/java/org/ohdsi/webapi/tool/ToolServiceImpl.java
@@ -11,6 +11,7 @@ import org.ohdsi.webapi.security.authz.UserEntity;
 import org.ohdsi.webapi.service.AbstractDaoService;
 import org.ohdsi.webapi.tool.dto.ToolDTO;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,6 +41,7 @@ public class ToolServiceImpl extends AbstractDaoService implements ToolService {
     }
 
     @Override
+    @PreAuthorize("isPermitted('admin:tools')")
     @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ToolDTO saveTool(@RequestBody ToolDTO toolDTO) {
         Tool tool = saveToolFromDTO(toolDTO, getCurrentUser());
@@ -62,11 +64,13 @@ public class ToolServiceImpl extends AbstractDaoService implements ToolService {
     }
 
     @Override
+    @PreAuthorize("isPermitted('admin:tools')")
     @DeleteMapping(value = "/{id}")
     public void delete(@PathVariable("id") Integer id) {
         toolRepository.deleteById(id);
     }
 
+    @PreAuthorize("isPermitted('admin:tools')")
     @PutMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ToolDTO updateTool(@RequestBody ToolDTO toolDTO) {
         return saveTool(toolDTO);

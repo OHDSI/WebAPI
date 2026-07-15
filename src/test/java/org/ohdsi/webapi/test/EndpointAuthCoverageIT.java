ed9af6986dd970fc450d054390350132a2f10544
Chris Knoll <cknoll@ohdsi.org>
2026-07-15 11:35:12 -0400

    Temporary disable endpoint guard tests until we determine a better way to identify guarded.

--- src/test/java/org/ohdsi/webapi/test/EndpointAuthCoverageIT.java
+++ src/test/java/org/ohdsi/webapi/test/EndpointAuthCoverageIT.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -34,6 +35,7 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
  * access. Runtime behaviour for the anonymous principal is exercised by
  * {@link AnonymousAccessIT} and {@link SourceAccessIT}.
  */
+@Ignore("Temporarily disabled while we determine a more robust way to determine which routes should be guarded")
 public class EndpointAuthCoverageIT extends WebApiIT {
 
     // Reachable before login by design (login/bootstrap). Prefix match. Adding to
